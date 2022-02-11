aead9c7d904a9b5605980e115eece64ae65aa2e3
Lorenz Ras <lenzras@gmail.com>
2022-02-12 00:40:13 +0900

    tested router

--- src/pages/Router.tsx
+++ src/pages/Router.tsx
@@ -1,7 +1,7 @@
-
-import { Route, Routes } from 'react-router-dom';
+import { Route, Routes, RouteMatch, useParams } from 'react-router-dom';
 import CustomerRegistration from './customer/register/CustomerRegistration';
 import UnderConstruction from './UnderConstruction';
+
 
 
 // The Main component renders one of the provided
@@ -9,12 +9,19 @@ import UnderConstruction from './UnderConstruction';
 // and /schedule routes will match any pathname that starts
 // with /roster or /schedule. The / route will only match
 // when the pathname is exactly the string "/"
+
+const TestComponent = () => {
+  const params = useParams();
+  return <>Success {params.id} </>;
+};
+
 const Router = () => (
   <main>
     <Routes>
       <Route path="/" element={<UnderConstruction />} />
       <Route path="/customer/register" element={<CustomerRegistration />} />
-      <Route path="test" element={<>Users</>} />
+      
+      <Route path="/test/:id" element={<TestComponent />} />
     </Routes>
   </main>
 );

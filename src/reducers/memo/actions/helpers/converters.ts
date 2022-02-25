4765c70aa3b2742d26ec95f96cabb52388917a8b
Lorenz Ras <lenzras@gmail.com>
2022-02-25 13:37:30 +0900

    fixed notifications

--- src/reducers/memo/actions/helpers/converters.ts
+++ src/reducers/memo/actions/helpers/converters.ts
@@ -1,27 +1,28 @@
 import { getUserCodesByIds } from '../../../../api/kintone/users/GET';
 import { EmployeesToNotify, MemoFormState } from '../../../../types/form.memo';
 
-/* type DeepPartial<T> = T extends object ? {
-  [P in keyof T]?: DeepPartial<T[P]>;
-} : T; */
+
 
 export type ConvertedMemo = Partial<CustomerMemoTypes.SavedData>;
 
-export const convertMemo = async (state: MemoFormState) : Promise<ConvertedMemo> => {
-  const { groupId, memoType, memoContents, notifyTo, custId } = state;
-
-  const Ags = Object.keys(notifyTo)
+const convertAgs = (notifyTo: EmployeesToNotify ) => {
+  return Object.keys(notifyTo)
     .reduce((acc, curr: keyof EmployeesToNotify) : string[] => {
-      console.log(curr, notifyTo[curr]);
-      const removeEmpty = notifyTo[curr].ids.filter(val => val);
-      return acc.concat(removeEmpty);
+
+      const removedEmpty = notifyTo[curr].isNotify ? notifyTo[curr].ids.filter(val => val) : [];
+      return acc.concat(removedEmpty);
     }, [] as string[]);
+
+};
+
+export const convertMemo = async (state: MemoFormState) : Promise<ConvertedMemo> => {
+  const { groupId, memoType, memoContents, notifyTo, custId, isNotify } = state;
 
   return {
     contents: { value: memoContents.value },
     groupId: { value:  groupId },
     memoType: { value: memoType.value },
     custId: { value: custId ?? '' },
-    notifyTo: { value: await getUserCodesByIds(Ags) },
+    notifyTo: { value: isNotify ? await getUserCodesByIds(convertAgs(notifyTo)) : [] },
   };
 };

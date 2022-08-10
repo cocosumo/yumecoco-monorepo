a4e720a00495cd61a089eec9b475f1e9a0d9c540
RPA02coco <87287796+RPA02coco@users.noreply.github.com>
2022-08-10 14:33:57 +0900

    見積もり登録用フォームの調整

--- src/pages/projEstimate/FormProjEstimate.tsx
+++ src/pages/projEstimate/FormProjEstimate.tsx
@@ -1,26 +1,80 @@
 import { Divider, Grid } from '@mui/material';
-import { Form, useFormikContext } from 'formik';
+import { FieldArray, Form, useFormikContext } from 'formik';
 import { MainContainer } from '../../components/ui/containers';
 import { FabSave } from '../../components/ui/fabs/FabSave';
 import { PageTitle } from '../../components/ui/labels';
 import { FormikTextField } from '../../components/ui/textfield';
 import { ScrollToFieldError } from '../../components/utils/ScrollToFieldError';
-import { getFieldName } from './form';
+import { getFieldName, initialValues } from './form';
 
 export default function FormProjEstimate() {
   const { submitForm } = useFormikContext();
+
+  console.log('initialValues.items', initialValues.items);
 
   return (
     <Form noValidate>
       <ScrollToFieldError />
       <MainContainer>
-        <PageTitle label='見積もり登録'/>
+        <PageTitle label='見積もり登録' />
 
         <Grid container item xl={8} spacing={2} mb={12}>
-          
           <Grid item xs={12} md={4}>
-            <FormikTextField name={getFieldName('tax')} label="税率" />
+
+
+
+            {/* 工事情報の検索 */}
+
+
+
           </Grid>
+
+          <Grid item xs={12}>
+            <Divider />
+          </Grid>
+
+          <Grid item xs={12} md={3}>
+            <FormikTextField name={getFieldName('constructionType')} label="工事種別名" />
+          </Grid>
+          <Grid item xs={12} md={3}>
+            <FormikTextField name={getFieldName('profitRate')} label="利益率" />
+          </Grid>
+          <Grid item xs={12} md={3}>
+            <FormikTextField name={getFieldName('taxRate')} label="税率" />
+          </Grid>
+          <Grid item md={3} />
+
+
+          <Grid item xs={12} md={12}>
+            {/* 見積もり用のテーブル */}
+            {/* 大項目(ルックアップ)｜中項目(ルックアップ)｜部材名(手入力)｜原価｜数量｜利益率｜単位｜税(課税・非課税)｜単価｜金額 */}
+            <FieldArray
+              name={getFieldName('items')}
+              render={arrayHelpers => (
+                <div>
+                  {initialValues.items.map((item, index) => (
+                    <div key={index}>
+                      {/** both these conventions do the same */}
+                      <FormikTextField name={`items[${index}].majorItem`} label="大項目" />
+                      <FormikTextField name={`items.${index}.middleItem`} label="中項目" />
+
+                      <button type="button" onClick={() => arrayHelpers.remove(index)}>
+                        -
+                      </button>
+                    </div>
+                  ))}
+                  <button
+                    type="button"
+                    onClick={() => arrayHelpers.push({ majorItem: '', middleItem: '' })}
+                  >
+                    +
+                  </button>
+                </div>
+              )}
+            />
+          </Grid>
+
+
         </Grid>
         <FabSave onClick={submitForm} />
       </MainContainer>
--- src/pages/projEstimate/form.ts
+++ src/pages/projEstimate/form.ts
@@ -4,7 +4,35 @@ import * as Yup from 'yup';
 export const initialValues = {
   projId: '',
   costomerName: '',
-  tax: 10,
+  constructionType: '', /* 工事種別(ルックアップ) */
+  profitRate: 0.5, /* 利益率(自動計算) */
+  taxRate: 10, /* 税率 */
+  /* 見積もり用配列要素 */
+  items: [] as {
+    majorItem: '',  /* 大項目 */
+    middleItem: '', /* 中項目 */
+  }[],
+
+  element: '',    /* 部材 */
+  costPrice: '',  /* 原価 */
+  quantity: '',   /* 数量 */
+  elemProfRate: 0, /* 利益率(部材) */
+  unit: '', /* 単位 */
+  tax: '',  /* 税(課税/非課税) */
+  unitPrice: 0, /* 単価(原価*利益率(部材)) */
+  price: 0, /* 金額(課税：(単価*数量) * (1 + (税率/100)), 非課税：(単価*数量)) */
+
+
+  /* 合計 */
+  totalCost: 0, /* 原価合計 */
+  grossProfit: 0, /* 粗利 */
+  grossProfitMargin: 0, /* 粗利率 */
+  taxAmount: 0, /* 税(円) */
+  taxExcludedAmount: 0, /* 税抜金額 */
+  amountIncludingTax: 0, /* 税込金額 */
+  /* 大項目ごとの小計 */
+  majorItemName: '', /* 大項目 */
+  itemSubtotal: 0, /* 大項目小計 */
 };
 
 export type TypeOfForm =  typeof initialValues;
@@ -17,8 +45,27 @@ export const validationSchema =  Yup.object(
     'projId': Yup
       .string()
       .required('必須です。'),
-    'tax': Yup
+    'profitRate': Yup
+      .number(),
+    'taxRate': Yup
       .number()
-      .required('必須です。'),
+      .required('必須です。'),     
+    'items': Yup.array()
+      .of(
+        Yup.object().shape({
+          'majorItem': Yup.string().required('必須です。'),
+          'middleItem': Yup.string(), /* 中項目 */
+          'element': Yup.string(),    /* 部材 */
+          'costPrice': Yup.string().required('必須です。'), /* 原価 */
+          'quantity': Yup.string().required('必須です。'), /* 数量 */
+          'elemProfRate': Yup.number(), /* 利益率(部材) */
+          'unit': Yup.string(), /* 単位 */
+          'tax': Yup.string(),  /* 税(課税/非課税) */
+          'unitPrice': Yup.number(), /* 単価(原価*利益率(部材)) */
+          'price': Yup.number(), /* 金額(課税：(単価*数量) * (1 + (税率/100)), 非課税：(単価*数量)) */
+        }),
+      )
+      .required('Must have items')
+      .min(1, 'Minimum of 1 items'),
   } as Partial<Record<KeyOfForm, any>>,
 );

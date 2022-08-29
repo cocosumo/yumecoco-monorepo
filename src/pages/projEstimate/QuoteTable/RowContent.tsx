afa74898739a19a8d499ad1501ceaeacb53e9372
Lorenz Ras <lenzras@gmail.com>
2022-08-29 13:47:11 +0900

    Update RowContent.tsx

--- src/pages/projEstimate/QuoteTable/RowContent.tsx
+++ src/pages/projEstimate/QuoteTable/RowContent.tsx
@@ -2,8 +2,17 @@
 import { Button, TableCell, TableRow } from '@mui/material';
 import { useFormikContext } from 'formik';
 import { useEffect } from 'react';
-import { TKMaterials, TypeOfForm } from '../form';
+import { Display } from '../fieldComponents/Display';
+import { FormikInput } from '../fieldComponents/FormikInput';
+import { FormikPulldown } from '../fieldComponents/FormikPulldown';
+import { getFieldName, TKMaterials, TypeOfForm } from '../form';
 import InputCellContent from './InputCellContent';
+
+const itemsName = getFieldName('items');
+
+const getItemFieldName = (
+  rowIdx: number, fieldName: TKMaterials,
+) => `${itemsName}[${rowIdx}].${fieldName}`;
 
 export const RowContent = ({ taxRate, row, rowIdx, removeRow }: {
   taxRate: number,
@@ -14,6 +23,7 @@ export const RowContent = ({ taxRate, row, rowIdx, removeRow }: {
   const { setFieldValue } = useFormikContext<TypeOfForm>();
   const { costPrice, quantity, elemProfRate, tax } = row;
 
+
   // 各行の単価・金額の算出処理
   useEffect(() => {
     // 単価の算出処理 : IF(原価 <= 0, 0 , 原価  * ( 1 + (内訳利益率/100)))
@@ -21,7 +31,7 @@ export const RowContent = ({ taxRate, row, rowIdx, removeRow }: {
     if (!(isNaN(costPrice) || isNaN(elemProfRate)) && ((costPrice) > 0)) {
       newUnitPrice = Math.round(+costPrice * (1 + (+elemProfRate / 100)));
     }
-    setFieldValue(`items[${rowIdx}].unitPrice`, newUnitPrice);
+    setFieldValue(getItemFieldName(rowIdx, 'unitPrice'), newUnitPrice);
 
     // 金額の算出処理 : IF(原価 <= 0, 原価, IF ( 税="課税", (単価*数量) * (1 + (税率/100)), (単価*数量)))
     let newPrice = 0; // 入力値がエラー(数値でない)時は0にする
@@ -38,28 +48,68 @@ export const RowContent = ({ taxRate, row, rowIdx, removeRow }: {
 
   }, [costPrice, quantity, elemProfRate, tax, taxRate]);
 
-  return (<TableRow >
-    {(Object.keys(row) as TKMaterials[]).map((rowitem) => {
-      return (
-        <TableCell
-          key={`${rowitem}_header`}
-          sx={{
-            padding: 1,
-            verticalAlign: 'top',
-          }}
-        >
-          <InputCellContent fieldName={rowitem} rowIdx={rowIdx} />
-        </TableCell>
-      );
-    })}
-    <TableCell key={`${row}_delBtn`}>
-      <Button
+  return (
+    <TableRow >
+
+      <TableCell>
+        <Display name={getItemFieldName(rowIdx, 'number')} />
+      </TableCell>
+
+      <TableCell>
+        <FormikPulldown name={getItemFieldName(rowIdx, 'majorItem')} options={[]} />
+      </TableCell>
+
+      <TableCell>
+        <FormikPulldown name={getItemFieldName(rowIdx, 'middleItem')} options={[]} />
+      </TableCell>
+
+      <TableCell>
+        <FormikPulldown name={getItemFieldName(rowIdx, 'element')} options={[]} />
+      </TableCell>
+
+      <TableCell>
+        <FormikInput name={getItemFieldName(rowIdx, 'costPrice')} />
+      </TableCell>
+
+      <TableCell>
+        <FormikInput name={getItemFieldName(rowIdx, 'quantity')} />
+      </TableCell>
+
+      <TableCell>
+        <FormikInput name={getItemFieldName(rowIdx, 'elemProfRate')} />
+      </TableCell>
+
+      <TableCell>
+        <FormikPulldown
+        name={getItemFieldName(rowIdx, 'unit')}
+        options={['課税', '非課ｓ税'].map((c) => ({ label: c, value: c }))}
+        />
+      </TableCell>
+
+      <TableCell>
+        <FormikPulldown
+          name={getItemFieldName(rowIdx, 'tax')}
+          options={['課税', '非課税']}
+          />
+      </TableCell>
+
+      <TableCell>
+        <Display name={getItemFieldName(rowIdx, 'unitPrice')} suffix={'円'}/>
+      </TableCell>
+
+      <TableCell>
+        <Display name={getItemFieldName(rowIdx, 'price')} suffix={'円'}/>
+      </TableCell>
+
+
+      <TableCell key={`${row}_delBtn`}>
+        <Button
         variant="outlined"
         onClick={() => removeRow(rowIdx)}
       >
-        -
-      </Button>
-    </TableCell>
-  </TableRow>
+          -
+        </Button>
+      </TableCell>
+    </TableRow>
   );
 };

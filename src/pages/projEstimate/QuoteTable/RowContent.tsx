046ac3f655490381cc47f7e166a28e133d534d92
Lorenz Ras <lenzras@gmail.com>
2022-08-29 14:14:09 +0900

    Separate row calculation logic to another hook

--- src/pages/projEstimate/QuoteTable/RowContent.tsx
+++ src/pages/projEstimate/QuoteTable/RowContent.tsx
@@ -1,11 +1,10 @@
 
 import { Button, TableCell, TableRow } from '@mui/material';
-import { useFormikContext } from 'formik';
-import { useEffect } from 'react';
 import { Display } from '../fieldComponents/Display';
 import { FormikInput } from '../fieldComponents/FormikInput';
 import { FormikPulldown } from '../fieldComponents/FormikPulldown';
-import { getFieldName, taxChoices, TKMaterials, TypeOfForm, unitChoices } from '../form';
+import { getFieldName, taxChoices, TKMaterials, unitChoices } from '../form';
+import { useCalculateRow } from '../hooks/useCalculateRow';
 
 const itemsName = getFieldName('items');
 
@@ -13,39 +12,15 @@ const getItemFieldName = (
   rowIdx: number, fieldName: TKMaterials,
 ) => `${itemsName}[${rowIdx}].${fieldName}`;
 
-export const RowContent = ({ taxRate, row, rowIdx, removeRow }: {
-  taxRate: number,
-  row: TypeOfForm['items'][number],
-  rowIdx: number,
-  removeRow: (rowIdx: number) => void
-}) => {
-  const { setFieldValue } = useFormikContext<TypeOfForm>();
-  const { costPrice, quantity, elemProfRate, tax } = row;
+export const RowContent = (
+  { rowIdx,
+    removeRow,
+  }: {
+    rowIdx: number,
+    removeRow: (rowIdx: number) => void
+  }) => {
 
-
-  // 各行の単価・金額の算出処理
-  useEffect(() => {
-    // 単価の算出処理 : IF(原価 <= 0, 0 , 原価  * ( 1 + (内訳利益率/100)))
-    let newUnitPrice = 0; // 入力値がエラー(数値でない)時は0にする
-    if (!(isNaN(costPrice) || isNaN(elemProfRate)) && ((costPrice) > 0)) {
-      newUnitPrice = Math.round(+costPrice * (1 + (+elemProfRate / 100)));
-    }
-    setFieldValue(getItemFieldName(rowIdx, 'unitPrice'), newUnitPrice);
-
-    // 金額の算出処理 : IF(原価 <= 0, 原価, IF ( 税="課税", (単価*数量) * (1 + (税率/100)), (単価*数量)))
-    let newPrice = 0; // 入力値がエラー(数値でない)時は0にする
-    if (+costPrice <= 0 ) {
-      newPrice = costPrice;
-    } else if ((newUnitPrice !== 0) && !(isNaN(quantity))) {
-      if (tax === '課税') {
-        newPrice = Math.round((newUnitPrice * +quantity) * (1 + (+taxRate / 100)));
-      } else { /* 非課税 */
-        newPrice = Math.round(newUnitPrice * +quantity);
-      }
-    }
-    setFieldValue(`items[${rowIdx}].price`, newPrice);
-
-  }, [costPrice, quantity, elemProfRate, tax, taxRate]);
+  useCalculateRow(rowIdx);
 
   return (
     <TableRow >
@@ -101,7 +76,7 @@ export const RowContent = ({ taxRate, row, rowIdx, removeRow }: {
       </TableCell>
 
 
-      <TableCell key={`${row}_delBtn`}>
+      <TableCell >
         <Button
         variant="outlined"
         onClick={() => removeRow(rowIdx)}

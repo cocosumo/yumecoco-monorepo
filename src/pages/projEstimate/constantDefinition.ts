af6367da31cf94142355ef05d851c040d6a3aba1
RPA02coco <87287796+RPA02coco@users.noreply.github.com>
2022-08-29 10:11:38 +0900

    バリデーションの修正

--- src/pages/projEstimate/constantDefinition.ts
+++ src/pages/projEstimate/constantDefinition.ts
@@ -38,7 +38,7 @@ export const materialsLabelList: Record<TKMaterials, 'display' | 'pulldown' | 'p
  * 見積もりアイテムのラベル定義
  */
 export const materialsNameList = [
-  'No.', '大項目*', '中項目', '部材', '原価*', '数量*', '利益率', '単位', '税(課税 / 非課税)', '単価', '金額',
+  'No.', '大項目*', '中項目', '部材', '原価*', '数量*', '利益率(%)', '単位', '税(課税 / 非課税)', '単価', '金額',
 ] as const;
 
 /**

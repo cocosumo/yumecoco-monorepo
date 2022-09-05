d53707bcc2af5c5fa2ececdeeacd5b85767ad5c5
Lorenz Ras <lenzras@gmail.com>
2022-09-05 13:53:27 +0900

    add convert method

--- src/pages/projEstimate/api/saveForm.ts
+++ src/pages/projEstimate/api/saveForm.ts
@@ -1,8 +1,29 @@
 import { TypeOfForm } from '../form';
 
-export const saveForm = (form: TypeOfForm) => {
+const convertToKintone = ({
+  customerName, projName, projType,
+  profitRate, taxRate,
 
-  const kintoneRecord: Partial<ConstructionTypes.SavedData> = {
+}: TypeOfForm) => {
+
+  /* 変換処理 */
+  const kintoneRecord: Partial<Estimates.main.SavedData> = {
+
+    顧客名: { value: customerName },
+    工事名称: { value: projName },
+    工事種別名: { value: projType },
+    利益率: { value: profitRate.toString() },
+    税率: { value: taxRate.toString() },
 
   };
+
+  return kintoneRecord;
+};
+
+export const saveForm = (form: TypeOfForm) => {
+
+  const newKintoneRecord =  convertToKintone(form);
+
+
+  /* 194(ココアス：工事内容)を更新 */
 };

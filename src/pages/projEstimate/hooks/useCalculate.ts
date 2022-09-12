f75f9c00ca8e60261d22fd5b22883e5a2e43443f
RPA02coco <87287796+RPA02coco@users.noreply.github.com>
2022-09-12 14:27:49 +0900

    金額の算出方法を修正

--- src/pages/projEstimate/hooks/useCalculate.ts
+++ src/pages/projEstimate/hooks/useCalculate.ts
@@ -35,17 +35,15 @@ export const useCalculate = (rowIdx: number) => {
     const grossProfitMarginVal = isNaN(provVal) ? 0 : parseFloat(provVal.toFixed(2));
 
 
-    // 単価の算出処理 : IF(原価 <= 0, 0 , 原価  * ( 1 + (内訳利益率/100)))
+    // 単価の算出処理 : (原価  * ( 1 + (内訳利益率/100)))
     let newUnitPrice = 0; // 入力値がエラー(数値でない)時は0にする
     if (!(isNaN(costPrice) || isNaN(elemProfRate))) {
       newUnitPrice = Math.round(+costPrice * (1 + (+elemProfRate / 100)));
     }
 
-    // 金額の算出処理 : IF(原価 <= 0, 原価, IF ( 税="課税", (単価*数量) * (1 + (税率/100)), (単価*数量)))
+    // 金額の算出処理 : IF(税="課税", (単価*数量) * (1 + (税率/100)), (単価*数量))
     let newPrice = 0; // 入力値がエラー(数値でない)時は0にする
-    if (+costPrice <= 0) {
-      newPrice = costPrice;
-    } else if ((newUnitPrice !== 0) && !(isNaN(quantity))) {
+    if ((newUnitPrice !== 0) && !(isNaN(quantity))) {
       if (tax === '課税') {
         newPrice = Math.round((newUnitPrice * +quantity) * (1 + (+taxRate / 100)));
       } else { /* 非課税 */

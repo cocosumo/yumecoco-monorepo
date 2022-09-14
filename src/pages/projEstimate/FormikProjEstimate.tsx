279d5ae86763af3676a7677ad443e26c8ab23f2d
Lorenz Ras <lenzras@gmail.com>
2022-09-14 16:24:07 +0900

    add save process

--- src/pages/projEstimate/FormikProjEstimate.tsx
+++ src/pages/projEstimate/FormikProjEstimate.tsx
@@ -1,5 +1,6 @@
 import { Formik } from 'formik';
 import { useConfirmDialog, useSnackBar } from '../../hooks';
+import { saveForm } from './api/saveForm';
 import { BtnSaveChoices } from './fieldComponents/formActions/BtnSaveChoices';
 import { initialValues, validationSchema } from './form';
 import FormProjEstimate from './FormProjEstimate';
@@ -15,30 +16,45 @@ export const FormikProjEstimate = () => {
       initialStatus={((s: TFormStatus)=>s)('busy')}
       enableReinitialize
       validationSchema={validationSchema}
-      onSubmit={(values, { setSubmitting }) => {
+      onSubmit={(values, { setSubmitting, setValues }) => {
         const { saveMode } = values;
-        /* kintoneに保存する処理を追加する */
 
-        if (saveMode === 'temporary') {
-          setTimeout(()=>{
-            setSnackState({
-              open: true,
-              message: '保存しました。',
+        const handleSave = (actionAfterSave?: () => void) => setTimeout(() => {
+          saveForm(values)
+            .then(({ id: estimateId })=>{
+              setValues((prev) => ({ ...prev, estimateId }));
+              setSnackState({
+                open: true,
+                severity: 'success',
+                message: '保存しました。',
+                handleClose: actionAfterSave,
+              });
+            })
+            .catch((err)=>{
+              setSnackState({
+                open: true,
+                severity: 'error',
+                message: `保存が失敗しました。${err.message}`,
+              });
+            })
+            .finally(()=>{
+              setSubmitting(false);
             });
-            setSubmitting(false);
-          }, 1500);
+        }, 1500); // Throttle to limit API calls
 
+        /** 一時保存 */
+        if (saveMode === 'temporary') {
+          handleSave();
         }
 
-
+        /** 保存 */
         if (saveMode === 'normal') {
           setDialogState({
             title: '編集した内容で保存します',
             content: (
               <BtnSaveChoices
                 handleClose={handleClose}
-                setSubmitting={setSubmitting}
-                values={values}
+                handleSave={handleSave}
               />),
             withNo: false, withYes: false,
           });

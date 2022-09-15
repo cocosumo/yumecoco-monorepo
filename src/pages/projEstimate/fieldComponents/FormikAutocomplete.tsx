6d946a5167a22564eba82bdbda1e1281f6c4b770
Lorenz Ras <lenzras@gmail.com>
2022-09-16 01:00:52 +0900

    fix eslint exhaustive-deps

--- src/pages/projEstimate/fieldComponents/FormikAutocomplete.tsx
+++ src/pages/projEstimate/fieldComponents/FormikAutocomplete.tsx
@@ -1,7 +1,18 @@
 import { debounce, FormControl, FormHelperText, TextField } from '@mui/material';
-import { useField } from 'formik';
+import { useField, useFormikContext } from 'formik';
 import Autocomplete from '@mui/material/Autocomplete';
-import { useCallback } from 'react';
+import { useMemo } from 'react';
+import { TypeOfForm } from '../form';
+
+
+/* 
+  useFieldはレンダリングたび、異なるリファレンスになるので、
+  依存している値の配列に入れると不安定です。
+  変わりに、setFieldValueを利用します。
+  https://github.com/jaredpalmer/formik/issues/2268 
+
+  Formik V3で修正したようです。リリースしたら、更新しましょう。
+*/
 
 export const FormikAutocomplete = (
   { name, options, handleChange }:
@@ -11,14 +22,16 @@ export const FormikAutocomplete = (
     handleChange?: (newVal?: string) => void
   },
 ) => {
-  const [field, meta, helpers] = useField(name);
+
+  const { setFieldValue } = useFormikContext<TypeOfForm>();
+  const [field, meta] = useField(name);
   const { touched, error } = meta;
 
-  const handleInputChange = useCallback(
-    debounce((event, value) => {
+  const handleInputChange = useMemo(()=>
+    debounce((_, value) => {
       if (handleChange) handleChange(value);
-      helpers.setValue(value);
-    }, 1000), [JSON.stringify(options)]);
+      setFieldValue(name, value);
+    }, 1000), [setFieldValue, name, handleChange]);
 
   return (
     <FormControl variant="standard" sx={{ minWidth: 100 }} size='small'>

942c9215969fca02f7e6d92be89431bd80b61de7
RPA02coco <87287796+RPA02coco@users.noreply.github.com>
2022-09-16 11:51:29 +0900

    Update calcUnitPrice.ts

--- src/pages/projEstimate/helpers/calcUnitPrice.ts
+++ src/pages/projEstimate/helpers/calcUnitPrice.ts
@@ -9,9 +9,5 @@ export const calcUnitPrice = (
   elemProfRate: number,
 ) => {
 
-  let newUnitPrice = 0; // 入力値がエラー(数値でない)時は0にする
-  if (!(isNaN(costPrice) || isNaN(elemProfRate))) {
-    newUnitPrice = Math.round(+(costPrice) * (1 + (+(elemProfRate) / 100)));
-  }
-  return +newUnitPrice;
+  return Math.round(+(costPrice) * (1 + (+(elemProfRate) / 100)));
 };

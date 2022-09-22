84fe06e1ff7d9435a67e563369e6117c8745bc75
Lorenz Ras <lenzras@gmail.com>
2022-09-22 11:35:20 +0900

    WIP added electronic send hook.

---
+++ src/pages/projContracts/hooks/useSendElectronicContract.ts
@@ -0,0 +1,45 @@
+import { useFormikContext } from 'formik';
+import { useBackdrop, useSnackBar } from '../../../hooks';
+import { sendContract } from '../api/docusign/sendContract';
+import { TypeOfForm } from '../form';
+
+export const useSendElectronicContract = () => {
+  const {
+    setValues,
+    values: {
+      projEstimateId,
+    },
+  } = useFormikContext<TypeOfForm>();
+  const { setBackdropState } = useBackdrop();
+  const { setSnackState } = useSnackBar();
+
+  const handleSendContract = async () => {
+    try {
+      /* 操作を無効化するため */
+      setBackdropState({ open: true });
+
+      const { envelopeId, envelopeStatus } = await sendContract({ projEstimateId });
+
+      setValues( (prev) => ({
+        ...prev,
+        envelopeId,
+        envelopeStatus,
+      }));
+
+      setSnackState({
+        open: true,
+        autoHideDuration: 20000,
+        severity: 'success',
+        message: '送信が成功しました。',
+      });
+
+      /* 操作を有効化するため */
+      setBackdropState({ open: true });
+    } catch (err: any) {
+
+    }
+
+
+
+  };
+};

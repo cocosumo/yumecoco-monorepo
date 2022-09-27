356fca8413d855beb2cedf2bc8e0e31118fe3b8e
Lorenz Ras <lenzras@gmail.com>
2022-09-27 16:18:07 +0900

    added components

---
+++ src/pages/projContracts/parts/paymentSchedule/PaymentField.tsx
@@ -0,0 +1,10 @@
+import { Box } from '@mui/material';
+
+export const PaymentField = () => {
+
+  return (
+    <Box>
+      Hello
+    </Box>
+  );
+};
--- src/pages/projContracts/parts/paymentSchedule/PaymentFields.tsx
+++ src/pages/projContracts/parts/paymentSchedule/PaymentFields.tsx
@@ -1,7 +1,22 @@
+import { Grid } from '@mui/material';
+import { PaymentField } from './PaymentField';
+
+
 export const PaymentFields = () => {
   return (
-    <>
-      SS
-    </>
+    <Grid container justifyContent={'center'} spacing={2}>
+      <Grid item xs={8}>
+        <PaymentField />
+      </Grid>
+      <Grid item xs={8}>
+        <PaymentField />
+      </Grid>
+      <Grid item xs={8}>
+        <PaymentField />
+      </Grid>
+      <Grid item xs={8}>
+        <PaymentField />
+      </Grid>
+    </Grid>
   );
 };

0c5a8a1090960c5f496099f30ab7a0bf350ff3be
Lorenz Ras <lenzras@gmail.com>
2022-09-27 16:29:00 +0900

    fixed postal

--- src/helpers/yupValidator.ts
+++ src/helpers/yupValidator.ts
@@ -1,3 +1,3 @@
 
 export const phoneRegExp = /^((\\+[1-9]{1,4}[ \\-]*)|(\\([0-9]{2,3}\\)[ \\-]*)|([0-9]{2,4})[ \\-]*)*?[0-9]{3,4}?[ \\-]*[0-9]{3,4}?$/;
-export const postalRegExp = /^\d{3,4}-\d{4}$/; 
+export const postalRegExp = /^\d{7,8}$/;
--- src/pages/customer/register/parts/Customers/Address.tsx
+++ src/pages/customer/register/parts/Customers/Address.tsx
@@ -1,7 +1,7 @@
 
 import { Grid, Collapse,  IconButton } from '@mui/material';
 import SearchIcon from '@mui/icons-material/Search';
-import {  FormikTextField, TextMaskPostal } from '../../../../../components/ui/textfield';
+import {  FormikTextField } from '../../../../../components/ui/textfield';
 import { Contacts } from './Contacts';
 import { FormikLabeledCheckBox } from '../../../../../components/ui/checkboxes';
 import { CustomerForm, getCustFieldName } from '../../form';
@@ -19,29 +19,33 @@ interface AddressProps {
 }
 
 const AddressFields = (namePrefix: string, postal: string, handleAddressSearch: ()=>void) => (
-  <Grid container item xs={12} spacing={2} mt={1}>
+  <Grid container item xs={12}
+    spacing={2} mt={1}
+  >
     <Grid item xs={8} md={4} >
       <FormikTextField
-      name={`${namePrefix}${getCustFieldName('postal')}`}
-      label="郵便番号" placeholder='471-0041'
-      inputComponent={TextMaskPostal}
-      shrink={!!postal}/>
+        name={`${namePrefix}${getCustFieldName('postal')}`}
+        label="郵便番号" placeholder='471-0041'
+      />
     </Grid>
     <Grid item xs={4} md={2} >
       <IconButton color={'primary'} size={'small'} onClick={handleAddressSearch}>
-        〒<SearchIcon sx={{ ml: '-6px', mt: '8px' }} fontSize="large" color={'primary'}/>
+        〒
+        <SearchIcon sx={{ ml: '-6px', mt: '8px' }} fontSize="large" color={'primary'} />
       </IconButton>
     </Grid>
 
-    <Grid item xs={12} md={6} /> {/* Offset */}
+    <Grid item xs={12} md={6} />
+    {' '}
+    {/* Offset */}
 
     <Grid item xs={12} >
-      <FormikTextField name={`${namePrefix}${getCustFieldName('address1')}`} label="住所" placeholder='愛知県豊田市汐見町8丁目87-8'/>
+      <FormikTextField name={`${namePrefix}${getCustFieldName('address1')}`} label="住所" placeholder='愛知県豊田市汐見町8丁目87-8' />
     </Grid>
     <Grid item xs={12} mb={2}>
-      <FormikTextField name={`${namePrefix}${getCustFieldName('address2')}`} label="住所（建物名）" placeholder='マンション山豊101'/>
+      <FormikTextField name={`${namePrefix}${getCustFieldName('address2')}`} label="住所（建物名）" placeholder='マンション山豊101' />
     </Grid>
-    <Contacts namePrefix={namePrefix}/>
+    <Contacts namePrefix={namePrefix} />
   </Grid>
 );
 
@@ -89,9 +93,8 @@ export const Address = (props: AddressProps) => {
     <>
       { !isFirstCustomer &&
       <Grid item xs={12}>
-        <FormikLabeledCheckBox name={`${namePrefix}${getCustFieldName('isSameAddress')}`} label="住所と連絡先は【契約者１】と同じ" defaultVal={isSameAddress}/>
-      </Grid>
-      }
+        <FormikLabeledCheckBox name={`${namePrefix}${getCustFieldName('isSameAddress')}`} label="住所と連絡先は【契約者１】と同じ" defaultVal={isSameAddress} />
+      </Grid>}
 
       <Grid item xs={12} ref={divRef}>
         <Collapse appear={!isFirstCustomer} in={(!isSameAddress || isFirstCustomer)} >

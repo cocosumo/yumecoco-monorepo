be6934328a28b108d76085aadac9e056b61d04ed
Lorenz Ras <lenzras@gmail.com>
2022-09-30 13:45:45 +0900

    Update index.ts

--- src/pages/projContracts/parts/index.ts
+++ src/pages/projContracts/parts/index.ts
@@ -1,5 +1,6 @@
 export * from './ContractPageShortcuts';
 export * from './FabStartProcess';
+export * from './GridNextDivider';
 export * from './PreviewToolBar';
 export * from './SearchProjField';
 export * from './SenderViewButton';

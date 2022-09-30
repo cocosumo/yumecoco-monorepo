505b9be9c2064e99d2f22fb7a14b3ed7aeb5ff57
Lorenz Ras <lenzras@gmail.com>
2022-09-30 13:44:27 +0900

    updated barrels

--- src/components/ui/selects/index.ts
+++ src/components/ui/selects/index.ts
@@ -1,5 +1,5 @@
 export * from './AgentFormSelect';
 export * from './BasicSelect';
 export * from './FormikSelect';
-export * from './projEstimates';
 export * from './FormikSelectAdvanced';
+export * from './projEstimates';

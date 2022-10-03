1b5d8991985f2028fadedbc41a96354cee1701fe
Lorenz Ras <lenzras@gmail.com>
2022-10-03 12:31:31 +0900

    added save process

--- src/pages/projContracts/hooks/useSubmitContractInfo.ts
+++ src/pages/projContracts/hooks/useSubmitContractInfo.ts
@@ -1,16 +1,48 @@
 import { Formik } from 'formik';
 import { ComponentProps } from 'react';
+import { useSnackBar } from '../../../hooks';
+import { saveContractDetails } from '../api/saveContractDetails';
 import { TypeOfForm } from '../form';
 
 export const useSubmitContractInfo = () => {
+  const { setSnackState } = useSnackBar();
 
   const onSubmit: ComponentProps<typeof Formik<TypeOfForm>>['onSubmit'] = async (
     values,
-    { setSubmitting },
+    {
+      setSubmitting,
+      resetForm,
+    },
   ) => {
-    alert('保存処理は開発中');
+    try {
+      const { revision } = await saveContractDetails(values);
 
-    setSubmitting(false);
+
+      resetForm({ values: {
+        ...values,
+        projEstimateRevision: revision,
+      } });
+
+      setSnackState({
+        open: true,
+        severity:  'success',
+        message: '保存が出来ました。',
+      });
+
+
+    } catch (err) {
+      setSnackState({
+        open: true,
+        severity:  'error',
+        message: `エラーが発生しました。ブラウザーをリフレッシュをして直良かったら、管理者にお知らせください。${err.message}。`,
+      });
+    } finally {
+
+      /* Throttle */
+      setTimeout(()=>setSubmitting(false), 1000);
+
+    }
+
   };
 
 

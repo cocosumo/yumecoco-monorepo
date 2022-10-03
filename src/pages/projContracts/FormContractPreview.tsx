60d7c376f961bd411110a3986ed476a7f024029b
Lorenz Ras <lenzras@gmail.com>
2022-10-03 10:41:51 +0900

    Update FormContractPreview.tsx

--- src/pages/projContracts/FormContractPreview.tsx
+++ src/pages/projContracts/FormContractPreview.tsx
@@ -16,7 +16,6 @@ import {
 import { SelectProjEstimates } from '../../components/ui/selects';
 import { PaymentSchedule } from './parts/paymentSchedule/PaymentSchedule';
 import { GridNextDivider } from './parts/GridNextDivider';
-import { ScrollToFieldError } from '../../components/utils/ScrollToFieldError';
 
 export const FormContractPreview = () => {
 
@@ -41,7 +40,6 @@ export const FormContractPreview = () => {
 
   return (
     <Form noValidate>
-      <ScrollToFieldError />
       <MainContainer justifyContent={'space-between'}>
         <PageTitle label='契約' />
 

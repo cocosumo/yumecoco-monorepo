6356d05716e0e66b60c71224f79801ea962144e6
Lorenz Ras <lenzras@gmail.com>
2022-10-04 11:55:34 +0900

    Update RelatedProjects.tsx

--- src/pages/customer/register/parts/Related/RelatedProjects.tsx
+++ src/pages/customer/register/parts/Related/RelatedProjects.tsx
@@ -21,13 +21,15 @@ export const RelatedProjects = () => {
       projects.map(({ projName, projId, envelopeStatus })=>{
 
         return (
-          <Grid item xs={12} md={6} lg={4} key={projName}>
+          <Grid item xs={12} md={6}
+            lg={4} key={projName}
+          >
             <Card>
               <CardContent>
                 <Stack spacing={2}>
                   <LabeledDetail label='工事番号' value={projId} direction={'column'} />
                   <LabeledDetail label='工事名' value={projName} direction={'column'} />
-                  <LabeledDetail label='契約状態' value={jaEnvelopeStatus(envelopeStatus).ja} direction={'column'} />
+                  <LabeledDetail label='契約状態' value={jaEnvelopeStatus(envelopeStatus)?.ja} direction={'column'} />
                 </Stack>
               </CardContent>
               <CardActions>

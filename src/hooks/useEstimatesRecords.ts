10d4b171badda18466584479b0202449bd52b820
Lorenz Ras <lenzras@gmail.com>
2022-10-06 12:50:32 +0900

    re-engineered useEstimateRecords to be more flexible

--- src/hooks/useEstimatesRecords.ts
+++ src/hooks/useEstimatesRecords.ts
@@ -1,22 +1,46 @@
-import { useMemo } from 'react';
+import { useSnackBar } from './useSnackBar';
+import { useCallback, useEffect, useState } from 'react';
 import { fetchEstimatesByProjId } from './../api/kintone/estimates/GET';
-import { usePromiseWithNotify } from './usePromiseWithNotify';
 
-
+/**
+ * 渡された工事番号に関連する見積もを取得する。
+ * 
+ * @param projId 
+ * @returns {Object} {
+ *   projEstimateRecords - 見積もり配列
+ *   handleFetchEstimates - 手動で更新したい時、この関数を呼ぶ
+ *   loading - 取得中
+ * }
+ */
 export const useEstimateRecords = (projId: string) => {
-  const memoPromiseFunc = useMemo(() => {
-    return projId ? () => fetchEstimatesByProjId(projId) : null;
-  }, [projId]);
+  const { setSnackState } = useSnackBar();
+  const [records, setRecords] = useState<Awaited<ReturnType<typeof fetchEstimatesByProjId>>>([]);
+  const [loading, setLoading] = useState(false);
 
-  const { data, loading } = usePromiseWithNotify<Estimates.main.SavedData[]>(
-    {
-      promiseFunc: memoPromiseFunc,
-      initialValue: [],
-    },
-  );
+  const handleFetchEstimates = useCallback(() => {
+    setLoading(true);
+    fetchEstimatesByProjId(projId)
+      .then(res => {
+        setRecords(res);
+        setLoading(false);
+      })
+      .catch((e) => {
+        setSnackState({
+          open: true,
+          severity: 'error',
+          message: `見積リストの取得にエラーが発生しました。${e.message}`,
+        });
+      });   
+  }, [projId, setSnackState]);
+
+  useEffect(() => {
+    handleFetchEstimates();
+  }, [projId, handleFetchEstimates]);
+
 
   return {
-    projEstimateRecords: projId ? data : [],
+    projEstimateRecords: records,
+    handleFetchEstimates,
     loading,
   };
 };

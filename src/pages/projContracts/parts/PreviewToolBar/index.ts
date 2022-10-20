b68d16f629d11d3fd051b72d5ec82a49379a552a
Lorenz Ras <lenzras@gmail.com>
2022-10-20 09:23:40 +0900

    refactor

--- src/pages/projContracts/parts/PreviewToolBar/index.ts
+++
@@ -1,5 +0,0 @@
-export * from './DownloadContract';
-export * from './EnvelopeStatus';
-export * from './PreviewToolBar';
-export * from './RefreshButton';
-export * from '../Preview/PreviewMenu/startContract';

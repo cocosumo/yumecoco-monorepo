9927ef8963b946b1494c4923a7c9d2337886381e
Lorenz Ras <lenzras@gmail.com>
2022-10-21 12:49:14 +0900

    Update ProjScheduleDate.tsx

--- src/pages/projContracts/parts/projSchedules/ProjScheduleDate.tsx
+++ src/pages/projContracts/parts/projSchedules/ProjScheduleDate.tsx
@@ -4,20 +4,24 @@ import { ComponentProps } from 'react';
 import { JADatePicker } from '../../../../components/ui/datetimepickers/JADatePicker';
 import { KeyOfForm } from '../../form';
 
-
-export const ProjScheduleDate = ({
-  fieldName,
-  variant = 'standard',
-  label,
-  isEmphasized = false,
-  helperText = ' ',
-} : {
+interface ProjScheduleDateProps extends Partial<ComponentProps<typeof JADatePicker>> {
   fieldName: KeyOfForm,
   variant?: ComponentProps<typeof TextField>['variant'],
   label?: string
   isEmphasized?: boolean,
   helperText?: string,
-}) => {
+}
+
+export const ProjScheduleDate = (props : ProjScheduleDateProps) => {
+  const {
+    fieldName,
+    variant = 'standard',
+    label,
+    isEmphasized = false,
+    helperText = ' ',
+    disablePast = true,
+    ...jaDateProps
+  } = props;
   const [field, meta, helpers] = useField(fieldName);
   const { value, name } = field;
   const { error, touched } = meta;
@@ -27,11 +31,12 @@ export const ProjScheduleDate = ({
 
   return (
     <JADatePicker
+      {...jaDateProps}
       /* Need to use null as empty string wont work when clearing the field.
       This is different with other fields where they
       become uncontrolled component when value becomes null. ~ ras 2022.10.03 */
       value={value || ''}
-      disablePast
+      disablePast={disablePast}
       views={['year', 'month', 'day' ]}
       onAccept={(v)=>{
         setValue(v ?? '', true);

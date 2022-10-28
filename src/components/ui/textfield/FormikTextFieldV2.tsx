9e3fdf14b9d7ddc952b1e4866d9908d12f73f685
Lorenz Ras <lenzras@gmail.com>
2022-10-28 09:44:27 +0900

    added useField Fast

--- src/components/ui/textfield/FormikTextFieldV2.tsx
+++ src/components/ui/textfield/FormikTextFieldV2.tsx
@@ -1,5 +1,7 @@
 import { TextField, TextFieldProps } from '@mui/material';
 import { useField } from 'formik';
+import { useEffect, useState } from 'react';
+import { useDebounce } from 'usehooks-ts';
 
 export const FormikTextFieldV2 = (
   props: TextFieldProps & {
@@ -14,18 +16,33 @@ export const FormikTextFieldV2 = (
     helperText,
     ...others
   } = props;
-  const [field, meta] = useField(name);
+  const [inputValue, setInputValue] = useState<string>('');
+  const debouncedValue = useDebounce<string>(inputValue, 500);
+  const [field, meta, helpers] = useField(name);
+
   const { error, touched } = meta;
+  const { setValue, setTouched } = helpers;
   const isShowError = touched && !!error;
+
+
+  const handleChange: TextFieldProps['onChange'] = (e) => {
+    setInputValue(e.target.value);
+    onChange?.(e);
+  };
+
+  useEffect(() => {
+    console.log('Fired!');
+    setValue(debouncedValue);
+    setTouched(true);
+  }, [debouncedValue, setValue, setTouched]);
+
 
   return (
     <TextField
       {...others}
       {...field}
-      onChange={((e)=>{
-        field.onChange(e);
-        onChange?.(e);
-      })}
+      value={inputValue}
+      onChange={handleChange}
       onBlur={((e)=>{
         field.onBlur(e);
         onBlur?.(e);

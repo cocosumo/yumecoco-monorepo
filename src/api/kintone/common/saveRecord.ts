705618cc8e559bff7987a26ded94a29644e2ef96
Lorenz Ras <lenzras@gmail.com>
2022-10-30 11:44:52 +0900

    added save

--- src/api/kintone/common/saveRecord.ts
+++ src/api/kintone/common/saveRecord.ts
@@ -1,6 +1,20 @@
 import { APPIDS, KintoneRecord } from './../config';
 
 
+/**
+ * 顧客グループのレコードを保存する。
+ * 
+ * @param obj 
+ * @param obj.appId App's id.
+ * @param obj.record Kintone's record object.
+ * @param obj.recordId Optional. If provided, it will update the specified recordId, if not add a new record. 
+ * @param obj.revision Optional. If record lock is needed, use the record's revision number to define this.
+ * @param obj.updateRelatedFn Optional. A function that is called when updating a record.
+ * 
+ * @see レコードの更新（PUT） https://developer.cybozu.io/hc/ja/articles/201941784
+ * @see レコードの登録（POST） https://developer.cybozu.io/hc/ja/articles/202166160
+ * @returns Object containing id and revision.
+ */
 export const saveRecord = async (
   {
     appId,
@@ -36,7 +50,7 @@ export const saveRecord = async (
       revision,
     });
 
-    /** IMPORTANT: This updates related apps to this record */
+    /** IMPORTANT: This updates related apps to this record.  */
     const relatedUpdates =  updateRelatedFn ?  await updateRelatedFn() : undefined;
 
     return {
--- src/api/kintone/custgroups/saveCustGroup.ts
+++ src/api/kintone/custgroups/saveCustGroup.ts
@@ -3,20 +3,6 @@ import { APPIDS } from './../config';
 import { getAgentNames } from './getAgentNames';
 import { updateRelatedToCustGroup } from './updateRelatedToCustGroup';
 
-
-
-
-/**
- * 顧客グループのレコードを保存する。
- * 
- * @param record 
- * @param custGroupId Optional. If provided, it will update the record instead. 
- * @param revision Optional. If record lock is needed, use the record's revision number to define this.
- * 
- * @see レコードの更新（PUT） https://developer.cybozu.io/hc/ja/articles/201941784-%E3%83%AC%E3%82%B3%E3%83%BC%E3%83%89%E3%81%AE%E6%9B%B4%E6%96%B0-PUT-
- * 
- * @returns Object containing id and revision.
- */
 export const saveCustGroup = async (
   {
     record,

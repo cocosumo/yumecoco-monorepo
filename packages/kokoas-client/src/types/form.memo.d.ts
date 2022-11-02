91254e74a5be1b3938b793f37ec169efdeebc9e3
Lorenz Ras <lenzras@gmail.com>
2022-11-02 11:06:19 +0900

    deleted dead code

--- packages/kokoas-client/src/types/form.memo.d.ts
+++ packages/kokoas-client/src/types/form.memo.d.ts
@@ -1,7 +1,7 @@
 
 
 import { ICustmemos } from 'types';
-import { InputField, ElementTarget, SubmitStatus } from './forms';
+
 
 export type NotifyAgent = {
   isNotify: boolean, ids: string[]
@@ -13,34 +13,8 @@ export interface EmployeesToNotify {
   'ゆめてつAG': NotifyAgent,
 }
 
-export interface MemoFormState {
-  isSubmitted: boolean,
-  groupId: string,
-  $id?:string,
-  custId?: string,
-  custName?: string,
-  memoType: InputField,
-  memoContents: InputField,
-  createdTime?: Date,
-  createdBy?: string,
-  createdByName?: string,
-  isNotify: boolean,
-  notifyTo: EmployeesToNotify,
-  submitState: SubmitStatus
-  hasError: boolean
-}
 
 export type InitialMemoPayload = { groupId: string, custId: string, custName: string, cocoAg: string[], yumeAg: string[] };
 
-export type SubmitPayload = { submitState: SubmitStatus, fetchResponse?: any };
-
 export type KeyPayload = { key: keyof EmployeesToNotify };
 
-export type FieldActionType =
-| { type: 'CHANGE_MEMO_VALUE', payload: ElementTarget  }
-| { type: 'CHANGE_CHECKED_AGENT', payload: KeyPayload  }
-| { type: 'CHANGE_SUBMITSTATE', payload: SubmitPayload  }
-| { type: 'SET_EDIT', payload: ICustmemos }
-| { type: 'SET_INITIAL', payload: InitialMemoPayload  }
-| { type: 'RESET' }
-| { type: 'CHANGE_ISNOTIFY' };

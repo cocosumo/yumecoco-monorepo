e09b940af7a5f84016045dfb49a78e57f7d96fc6
Lorenz Ras <lenzras@gmail.com>
2022-11-02 13:21:41 +0900

    deleted deprecated files

--- packages/kokoas-server/src/api/docusign/contracts/processUkeoi.test.ts
+++
@@ -1,8 +0,0 @@
-import {processUkeoi} from './processUkeoi';
-
-describe('sendUkeoi', ()=>{
-  it('should send ukeoi', async ()=>{
-    const result = await processUkeoi('111');
-    expect(result).toMatchSnapshot();
-  }, 30000);
-});

c913fdcc330f783039e907e4a0ab80ea5b74986d
Lorenz Ras <lenzras@gmail.com>
2022-11-04 13:45:19 +0900

    removed babeljest

--- packages/api-kintone/src/record/client.ts
+++ packages/api-kintone/src/record/client.ts
@@ -3,10 +3,10 @@ import { RecordClient } from '@kintone/rest-api-client/lib/client/RecordClient';
 
 const oAuth = {
   accessToken: '',
-
   /** Time when accesstoken was generated, valid for 1 hr */
   at: null as null | Date,
-
   refreshToken: process.env.KT_REFRESH_TOKEN,
 };
+
+
 

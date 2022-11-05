bac2a6829ea23bf21d6871f49d8dfdaf42feb6ad
Lorenz Ras <lenzras@gmail.com>
2022-11-05 16:16:05 +0900

    Update useUpdateEstimateId.ts

--- packages/kokoas-client/src/pages/projEstimate/hooks/useUpdateEstimateId.ts
+++ packages/kokoas-client/src/pages/projEstimate/hooks/useUpdateEstimateId.ts
@@ -1,3 +1,4 @@
+import { useEstimateById } from './../../../hooksQuery/useEstimateById';
 import { KintoneRecord, APPIDS } from './../../../api/kintone/config';
 import { useFormikContext } from 'formik';
 import { useEffect } from 'react';
@@ -34,66 +35,67 @@ export const useUpdateEstimateId = () => {
     setValues,
   } = useFormikContext<TypeOfForm>();
 
+  const { data } = useEstimateById(estimateId);
+
   useEffect(() => {
+    if (data) {
 
-    if (estimateId) {
-      getEstimateById(estimateId)
-        .then(({
-          projId,
-          内訳: { value: estimateTable },
-          顧客名 : custName,
-          工事種別利益: projTypeProfit,
-          税: tax,
-          estimateStatus,
-          工事名称: projName,
-          projTypeId,
-          工事種別名: projTypeName,
-          作成日時,
-          envStatus,
-        }) => {
+      const { record } = data;
+      const {
+        projId,
+        内訳: { value: estimateTable },
+        顧客名 : custName,
+        工事種別利益: projTypeProfit,
+        税: tax,
+        estimateStatus,
+        工事名称: projName,
+        projTypeId,
+        工事種別名: projTypeName,
+        作成日時,
+        envStatus,
+      } = record;
 
-          if (estimateId) {
-            setValues(prev => {
-              return produce(prev, draft => {
-                draft.customerName = custName.value;
-                draft.projId = projId.value;
-                draft.projTypeProfit = +projTypeProfit.value;
-                draft.projTypeId = projTypeId.value;
-                draft.tax = +tax.value;
-                draft.status = estimateStatus.value as TypeOfForm['status'];
-                draft.projName = projName.value;
-                draft.projTypeName = projTypeName.value;
-                draft.createdDate = format(parseISO(作成日時.value), 'yyyy/MM/dd');
-                draft.envStatus = envStatus.value;
-                draft.items = estimateTable.map(({ id, value: {
-                  原価,
-                  大項目,
-                  中項目,
-                  部材名,
-                  数量,
-                  単位,
-                  部材利益率,
-                  taxType,
-                } }) => {
-                  return {
-                    key: id,
-                    majorItem: 大項目.value,
-                    middleItem: 中項目.value,
-                    element: 部材名.value,
-                    quantity: +数量.value,
-                    unit: 単位.value as TypeOfForm['items'][number]['unit'],
-                    costPrice: +原価.value,
-                    elemProfRate: +部材利益率.value,
-                    taxType: taxType.value as TypeOfForm['items'][number]['taxType'],
-                  };
-                });
-              });
-            });
-          }
 
+      setValues(prev => {
+        return produce(prev, draft => {
+          draft.customerName = custName.value;
+          draft.projId = projId.value;
+          draft.projTypeProfit = +projTypeProfit.value;
+          draft.projTypeId = projTypeId.value;
+          draft.tax = +tax.value;
+          draft.status = estimateStatus.value as TypeOfForm['status'];
+          draft.projName = projName.value;
+          draft.projTypeName = projTypeName.value;
+          draft.createdDate = format(parseISO(作成日時.value), 'yyyy/MM/dd');
+          draft.envStatus = envStatus.value;
+          draft.items = estimateTable.map(({ id, value: {
+            原価,
+            大項目,
+            中項目,
+            部材名,
+            数量,
+            単位,
+            部材利益率,
+            taxType,
+          } }) => {
+            return {
+              key: id,
+              majorItem: 大項目.value,
+              middleItem: 中項目.value,
+              element: 部材名.value,
+              quantity: +数量.value,
+              unit: 単位.value as TypeOfForm['items'][number]['unit'],
+              costPrice: +原価.value,
+              elemProfRate: +部材利益率.value,
+              taxType: taxType.value as TypeOfForm['items'][number]['taxType'],
+            };
+          });
         });
+      });
+      
+      
     }
 
+  }, [data, estimateId, setValues]);
 
-  }, [estimateId, setValues]);
 };

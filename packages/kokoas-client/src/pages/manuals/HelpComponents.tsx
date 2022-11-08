a26cedcf03bc0e65eb48581db1790d722c9daca1
Lorenz Ras <lenzras@gmail.com>
2022-11-08 18:46:41 +0900

    Update HelpComponents.tsx

--- packages/kokoas-client/src/pages/manuals/HelpComponents.tsx
+++ packages/kokoas-client/src/pages/manuals/HelpComponents.tsx
@@ -1,7 +1,6 @@
-import { CssBaseline, Divider, styled } from '@mui/material';
+import { Box, CssBaseline, Divider, styled } from '@mui/material';
 import ManualMenus from './ManualMenus';
 import ManualRouter from './ManualRouter';
-import { Box } from '@mui/system';
 
 export const manualMenusWidth = '240';
 
@@ -16,10 +15,11 @@ const ExplanationMain = styled('div')`
 export default function HelpComponents() {
   return (
     <Box 
-    sx={{
-      display: 'flex',
-      height: '100vh',
-    }}>
+      sx={{
+        display: 'flex',
+        height: '100vh',
+      }}
+    >
       <CssBaseline />
       <ManualMenus />
       <Divider orientation="vertical" flexItem />

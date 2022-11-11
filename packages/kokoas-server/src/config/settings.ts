bff8bd049547de6e5a1a5ccd5e6f71c556e7c0f1
Lorenz Ras <lenzras@gmail.com>
2022-11-11 11:07:09 +0900

    added prod

--- packages/kokoas-server/src/config/settings.ts
+++ packages/kokoas-server/src/config/settings.ts
@@ -5,15 +5,15 @@ import docusign from 'docusign-esign';
 import { loadEnv } from 'libs';
 loadEnv();
 
-console.log('PROCESS', process.env.DS_INTEGRATOR_KEY);
+
 
 export const isProd = process.env.NODE_ENV === 'prod';
 export const ds = docusign;
 export const oAuth = ds.ApiClient.OAuth;
 export const restApi = ds.ApiClient.RestApi;
 
-export const basePath = restApi.BasePath.DEMO;
-export const oAuthBasePath = oAuth.BasePath.DEMO;
+export const basePath = restApi.BasePath[isProd ? 'PRODUCTION' : 'DEMO'];
+export const oAuthBasePath = oAuth.BasePath[isProd ? 'PRODUCTION' : 'DEMO'];
 export const redirectURI = 'http://localhost';
 export const expiresIn = 60 * 60 * 8;
 export const tokenReplaceMin = 10;
@@ -27,7 +27,7 @@ export const getPrivateKey = async () => {
 };
 
 export const integratorKey = <string>process.env.DS_INTEGRATOR_KEY;
-export const userId = <string>process.env.DS_USER_ID;
+export const userId = <string>process.env[isProd ? 'DS_USER_ID_PROD' : 'DS_USER_ID'];
 export const scopes = [
   oAuth.Scope.IMPERSONATION,
   oAuth.Scope.SIGNATURE,
@@ -40,5 +40,8 @@ export const apiClient = new ds.ApiClient({
 
 export const getEnvelopesApi = () => new docusign.EnvelopesApi(apiClient);
 
+console.log('Starting with integrator key', process.env.DS_INTEGRATOR_KEY);
 console.log('Integrator key ' + integratorKey);
-
+console.log('userId ' + userId);
+console.log('ENV: ', process.env.NODE_ENV);
+console.log('BasePath: ', basePath);

77de2f2c75686df0b4cca29dc0ecab91e12f5b46
Lorenz Ras <lenzras@gmail.com>
2022-11-14 14:17:06 +0900

    fix

--- packages/kokoas-client/src/pages/customer/register/parts/Memo/memoForm/api/deleteMemo.ts
+++ packages/kokoas-client/src/pages/customer/register/parts/Memo/memoForm/api/deleteMemo.ts
@@ -2,7 +2,7 @@ import { MemoFormType } from '../form';
 import { APPIDS, KintoneRecord } from '../../../../../../../api/kintone';
 
 export const deleteMemo = (params: MemoFormType) => {
-  console.log('deletememo props', params);
+
   if (!params.memoId) throw new Error('Please provide id to delete!');
 
   return KintoneRecord.deleteRecords({

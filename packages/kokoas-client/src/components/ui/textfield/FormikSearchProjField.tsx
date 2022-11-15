d7c6b58a8d65a01ca489806841ccafbabff6a16a
Lorenz Ras <lenzras@gmail.com>
2022-11-15 18:13:15 +0900

    Update FormikSearchProjField.tsx

--- packages/kokoas-client/src/components/ui/textfield/FormikSearchProjField.tsx
+++ packages/kokoas-client/src/components/ui/textfield/FormikSearchProjField.tsx
@@ -1,7 +1,12 @@
 import { useField } from 'formik';
 import { SearchProjects } from './SearchProjects';
 
-/* TODO: SearchProjectsのラッパーとして変える */
+/**
+ * Higher level formik wrapper for searching projects.
+ * 
+ * nameを渡すことだけで、Formikのフォームにバインド出来ます。
+ * Formikは使用しないなら、より低レベルのSearchProjectsをご利用ください。
+ */
 export const FormikSearchProjField = (props: {
   name: string,
   label: string,

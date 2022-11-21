3a500c20f47ef3f30e5d90cb199dcd4a2f8c965b
Lorenz Ras <lenzras@gmail.com>
2022-11-21 11:48:39 +0900

    Update useCustomersByCustGroupId.ts

--- packages/kokoas-client/src/hooksQuery/useCustomersByCustGroupId.ts
+++ packages/kokoas-client/src/hooksQuery/useCustomersByCustGroupId.ts
@@ -2,6 +2,11 @@ import { useMemo } from 'react';
 import { useCustGroupById } from './useCustGroupById';
 import { useCustomersByIds } from './useCustomersByIds';
 
+/**
+ * 顧客グループ番号で顧客データを取得する。
+ *
+ * custGroup n-n customers
+ */
 export const useCustomersByCustGroupId = (custGroupId?: string) => {
   const { data: custGroupRecord } = useCustGroupById(custGroupId ?? '');
 

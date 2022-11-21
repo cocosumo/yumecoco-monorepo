5ea10397ab7d0708e6100f55e2965a4af8ae07c5
Lorenz Ras <lenzras@gmail.com>
2022-11-21 12:25:54 +0900

    Update useSaveCustGroup.ts

--- packages/kokoas-client/src/hooksQuery/useSaveCustGroup.ts
+++ packages/kokoas-client/src/hooksQuery/useSaveCustGroup.ts
@@ -1,13 +1,23 @@
-import { useMutation } from '@tanstack/react-query';
+import { useMutation, useQueryClient } from '@tanstack/react-query';
 import { saveCustGroup } from 'api-kintone';
+import { AppIds } from 'config';
 import { useCommonOptions } from './useCommonOptions';
 
 export const useSaveCustGroup = () => {
   const commonOptions = useCommonOptions();
+  const queryClient = useQueryClient();
+
+
   return useMutation(
     saveCustGroup,
     {
       ...commonOptions,
+      onSuccess: (data) => {
+        commonOptions.onSuccess();
+        queryClient.invalidateQueries({ queryKey: [AppIds.custGroups, { custGroupId: data.id }] });
+        queryClient.invalidateQueries({ queryKey: [AppIds.customers, { custGroupId: data.id }] });
+
+      },
     },
-  ); 
+  );
 };

d00a9dc469a758b4bf98739edfdab9b78e609ce2
Lorenz Ras <lenzras@gmail.com>
2022-12-05 20:22:36 +0900

    removed more files

--- packages/api-kintone/src/estimates/index.ts
+++ packages/api-kintone/src/estimates/index.ts
@@ -3,7 +3,6 @@ export * from './generateEstimateDataIdSeqNum';
 export * from './getAllEstimates';
 export * from './getEstimateByEnvId';
 export * from './getEstimateById';
-export * from './getEstimateByIdV2';
 export * from './getEstimatesByProjId';
 export * from './saveEstimate';
 export * from './updateEstimateById';

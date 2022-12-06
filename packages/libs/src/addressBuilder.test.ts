8fbb062b676e17e5c855f1248251505fa16daf40
Lorenz Ras <lenzras@gmail.com>
2022-12-06 15:49:52 +0900

    Update addressBuilder.test.ts

--- packages/libs/src/addressBuilder.test.ts
+++ packages/libs/src/addressBuilder.test.ts
@@ -1,18 +1,26 @@
 import { addressBuilder } from './addressBuilder';
 
 it('should build address in correct format', () => {
+
+  /* 〒←マークは番号の前に。ハイフンも入れる。 */
   expect(addressBuilder({
     postal: '4418124',
     address1: '愛知県豊橋市野依町字山中１０－１０',
     address2: 'ABC',
-  })).toEqual('〒441-8124 愛知県豊橋市野依町字山中１０－１０ABC');
+  }))
+    .toEqual('〒441-8124 愛知県豊橋市野依町字山中１０－１０ABC');
 
+
+  /* 郵便番号が入っていない場合は、マークを外す */
   expect(addressBuilder({
     address1: '愛知県豊橋市野依町字山中１０－１０',
     address2: 'ABC',
-  })).toEqual('愛知県豊橋市野依町字山中１０－１０ABC');
+  }))
+    .toEqual('愛知県豊橋市野依町字山中１０－１０ABC');
 
+  /* address1だけ */
   expect(addressBuilder({
     address1: '愛知県豊橋市野依町字山中１０－１０',
-  })).toEqual('愛知県豊橋市野依町字山中１０－１０');
+  }))
+    .toEqual('愛知県豊橋市野依町字山中１０－１０');
 });

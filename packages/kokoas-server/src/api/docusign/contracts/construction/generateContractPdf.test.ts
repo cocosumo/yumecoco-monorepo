443055c1418652d23e1e9dcd2d61d990622a6bbd
Lorenz Ras <lenzras@gmail.com>
2022-12-06 13:13:05 +0900

    fixing test

--- packages/kokoas-server/src/api/docusign/contracts/construction/generateContractPdf.test.ts
+++ packages/kokoas-server/src/api/docusign/contracts/construction/generateContractPdf.test.ts
@@ -8,7 +8,7 @@ import { assetsDir } from '../config/file';
 describe('Contract', () => {
   it('should generate contract in pdf', async () =>{
     const contractData = await getContractData({
-      projEstimateId: '74',
+      projEstimateId: 'dummy01',
       userCode: 'RPA03',
     });
     const pdf = await generateContractPdf(contractData, 'Uint8Array ');

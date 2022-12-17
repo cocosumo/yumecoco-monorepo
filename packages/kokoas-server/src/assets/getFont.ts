f5ce43b82e7ec9eb7c0251acd1ab6fb3ed525f95
Lorenz Ras <lenzras@gmail.com>
2022-12-17 11:55:08 +0900

    updated document

--- packages/kokoas-server/src/assets/getFont.ts
+++ packages/kokoas-server/src/assets/getFont.ts
@@ -1,5 +1,5 @@
 import path from 'path';
 
-export const getFont = (font = 'MSMINCHO.TTF') => {
+export const getFont = (font = 'MSMINCHO.ttf') => {
   return path.join(__dirname, 'fonts', font);
 };

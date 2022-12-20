20475c4f3f9baf7b28ee64b8a8e77879541802f6
Lorenz Ras <lenzras@gmail.com>
2022-12-20 10:31:14 +0900

    Update FormikAutocomplete.tsx

--- packages/kokoas-client/src/pages/projEstimate/fieldComponents/FormikAutocomplete.tsx
+++ packages/kokoas-client/src/pages/projEstimate/fieldComponents/FormikAutocomplete.tsx
@@ -7,7 +7,8 @@ import { useDebounce } from 'usehooks-ts';
 /*
   useFieldはレンダリングたび、異なるリファレンスになるので、
   依存している値の配列に入れると不安定です。
-  変わりに、setFieldValueを利用します。
+  変わりに、独自作ったuseFieldFastを利用します。
+  useFieldと同じ仕様ですが、リファレンスを安定させました。
   https://github.com/jaredpalmer/formik/issues/2268
 
   Formik V3で修正したようです。リリースしたら、更新しましょう。

7a408439277a6f3de3ff8290676e57600cba1d92
Lorenz Ras <lenzras@gmail.com>
2022-12-21 09:35:04 +0900

    fixed position of summary

--- packages/kokoas-client/src/pages/projEstimate/SummaryTable/SummaryTableContainer.tsx
+++ packages/kokoas-client/src/pages/projEstimate/SummaryTable/SummaryTableContainer.tsx
@@ -1,4 +1,5 @@
 import { Box, Paper, Table, TableContainer } from '@mui/material';
+import { grey } from '@mui/material/colors';
 import { useMenuContext } from 'kokoas-client/src/hooks/useMenuContext';
 import { ReactNode } from 'react';
 
@@ -21,9 +22,21 @@ export const SummaryTableContainer = ({
         transform: 'translateX(-50%)',
         left: '50%',
         ml: menuOpen ? `${drawWidth / 2}px` : '0px',
+        transition: 'all 0.5s',
       }}
     >
-      <TableContainer component={Paper}>
+      <TableContainer
+        variant='elevation'
+        component={Paper}
+        sx={{
+          background: grey[200],
+          opacity: '0.7',
+          transition: 'opacity 0.5s',
+          '&:hover': {
+            opacity: '1',
+          },
+        }}
+      >
         <Table size="small" >
           {children}
         </Table>

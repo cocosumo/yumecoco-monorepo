7373e503862244af967bbfda4ec63f639740c393
Lorenz Ras <lenzras@gmail.com>
2023-01-07 15:23:51 +0900

    Update SystemUpdate.tsx

--- packages/kokoas-client/src/pages/@home/sections/systemUpdate/SystemUpdate.tsx
+++ packages/kokoas-client/src/pages/@home/sections/systemUpdate/SystemUpdate.tsx
@@ -4,25 +4,27 @@ import { SystemUpdateItem } from './SystemUpdateItem';
 import Carousel from 'react-spring-3d-carousel';
 import { Box } from '@mui/material';
 import { useState } from 'react';
-import { config } from 'react-spring';
 
 export const SystemUpdate = () => {
   const [slide, setSlide] = useState({
     goToSlide: 0,
-    offsetRadius: 2,
-    config: config.gentle,
   });
   const { data, isFetching } = useSystemUpdate();
   const { records } = data || {};
 
   return (
-    <Box width={'100%'}
-      height={'200px'}
+    <Box
+      sx={{
+        width: '100%',
+        height: '200px',
+        userSelect: 'none',
+        cursor: 'pointer',
+      }}
     >
       {!isFetching && records && (
         <Carousel
           showNavigation={false}
-          offsetRadius={4}
+          offsetRadius={1}
           goToSlide={slide.goToSlide}
           slides={records.map((record, index) => {
             return (

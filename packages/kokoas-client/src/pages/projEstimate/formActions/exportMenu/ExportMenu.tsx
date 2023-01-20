347517cfbeb5d2d9cd853965741704caf7b7e51e
Lorenz Ras <lenzras@gmail.com>
2023-01-20 11:25:06 +0900

    add menu

---
+++ packages/kokoas-client/src/pages/projEstimate/formActions/exportMenu/ExportMenu.tsx
@@ -0,0 +1,47 @@
+import { Box, SpeedDial, SpeedDialAction, Tooltip } from '@mui/material';
+import { RiFileExcel2Fill } from '@react-icons/all-files/ri/RiFileExcel2Fill';
+import { TiExport } from '@react-icons/all-files/ti/TiExport';
+import PrintIcon from '@mui/icons-material/Print';
+import SpeedDialIcon from '@mui/material/SpeedDialIcon';
+import { useConfirmDialog, useSnackBar } from 'kokoas-client/src/hooks';
+
+
+
+export const ExportMenu = () => {
+  const { setSnackState } = useSnackBar();
+
+  const handleClick = () => {
+    setSnackState({
+      open: true,
+      message: '申し訳ございません。機能は開発中です。',
+      severity: 'warning',
+    });
+  };
+
+  return (
+    <Tooltip title={'出力'} placement={'top'}>
+      <Box>
+        <SpeedDial
+          ariaLabel='出力'
+          icon={<SpeedDialIcon icon={<TiExport fontSize={'22px'} />} />}
+          direction={'down'}
+          FabProps={{
+            size: 'small',
+          }}
+        >
+          <SpeedDialAction
+            icon={<RiFileExcel2Fill fontSize={'22px'} color={'green'} />}
+            tooltipTitle={'エクセル'}
+            onClick={handleClick}
+          />
+          <SpeedDialAction
+            icon={<PrintIcon />}
+            tooltipTitle={'印刷'}
+            onClick={handleClick}
+          />
+          {/* 出力形式はここに追加。多くなったら、配列化してリファクタリングする。*/}
+        </SpeedDial>
+      </Box>
+    </Tooltip>
+  );
+};

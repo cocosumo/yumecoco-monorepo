41b6a3f333e3868e0e3b9887461566fcd90f7b19
Lorenz Ras <lenzras@gmail.com>
2023-01-24 10:59:16 +0900

    WIP

---
+++ packages/kokoas-server/src/api/andpad/@auth/generateURI.tsx
@@ -0,0 +1,3 @@
+export const generateURI = () => {
+  return `https://api.andpad.jp/v3/auth/oauth/authorize?response_type=code&client_id=ESwajxWkZNNJIWfoS4Nl-ryK57w8HE7XhY61G3GzQQQ&redirect_uri=https://example.com&scope=openid`;
+};

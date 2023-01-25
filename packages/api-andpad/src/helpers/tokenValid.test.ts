7592d528f03e6ecb12bc53f89512ed8df4c994f6
Lorenz Ras <lenzras@gmail.com>
2023-01-25 11:58:04 +0900

    token

---
+++ packages/api-andpad/src/helpers/tokenValid.test.ts
@@ -0,0 +1,12 @@
+
+
+describe('tokenValid', () => {
+  it('should return false if token is expired', () => {
+
+
+  });
+
+  it('should return true if token is not expired', () => {
+
+  });
+});
--- packages/api-andpad/src/helpers/tokenValid.ts
+++ packages/api-andpad/src/helpers/tokenValid.ts
@@ -1,7 +1,12 @@
-import { AuthToken } from '../types';
 
-export default function tokenValid(token: AuthToken) {
+type TokenDates = {
+  created_at: number,
+  expires_in: number
+};
+
+export default function tokenValid(token: TokenDates) {
   const now = Date.now() / 1000;
   const expiry = token.created_at + token.expires_in;
+
   return now < expiry;
 }

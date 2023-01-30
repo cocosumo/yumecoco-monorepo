f9904b56a11ef5d6364b31cb9a15647df2cf4899
RPA02coco <87287796+RPA02coco@users.noreply.github.com>
2023-01-30 10:09:11 +0900

    Update EstimatesTableBody.tsx

--- packages/kokoas-client/src/pages/projInvoice/fieldComponents/EstimatesTableBody.tsx
+++ packages/kokoas-client/src/pages/projInvoice/fieldComponents/EstimatesTableBody.tsx
@@ -1,7 +1,24 @@
-import { TableCell, TableRow } from '@mui/material';
+import { TableCell, TableRow, Typography } from '@mui/material';
 import { FormikLabeledCheckBox } from 'kokoas-client/src/components';
 import { roundTo } from 'libs';
 import { getEstimatesFieldName, TMaterials } from '../form';
+
+const CellContent = ({
+  content,
+  disabled,
+}: {
+  content: string | undefined
+  disabled: boolean
+}) => {
+  return (
+    <Typography
+      variant='body2'
+      color={!disabled ? 'text.primary' : 'gray'}
+    >
+      {content}
+    </Typography>
+  );
+};
 
 export const EstimateTableBody = ({
   estimateRow,
@@ -9,34 +26,51 @@ export const EstimateTableBody = ({
   estimateRow: TMaterials
 }) => {
 
+  const {
+    contractAmount,
+    billedAmount,
+  } = estimateRow;
+
+  const isRefund = contractAmount < 0;
+  const disabled = !isRefund ? contractAmount <= billedAmount : contractAmount >= billedAmount;
 
 
   return (
     <TableRow>
-      <TableCell>
+      <TableCell align="right">
         {/* 工事種別 */}
+        {disabled &&
+          <CellContent
+            content={'※全額請求済み'}
+            disabled={disabled}
+          />}
       </TableCell>
       <TableCell align="right">
-        {/* 枝番号 */
-          estimateRow.dataId.split('-').at(-1)
-        }
+        {/* 枝番号 */}
+        <CellContent
+          content={estimateRow.dataId.split('-').at(-1)}
+          disabled={disabled}
+        />
       </TableCell>
       <TableCell align="right">
-        {/* 契約金額 */
-          roundTo(+estimateRow.contractAmount).toLocaleString()
-        }
+        {/* 契約金額 */}
+        <CellContent
+          content={roundTo(+estimateRow.contractAmount).toLocaleString()}
+          disabled={disabled}
+        />
       </TableCell>
       <TableCell align="right">
-        {/* 請求済み金額 */
-          roundTo(+estimateRow.billedAmount).toLocaleString()
-        }
+        {/* 請求済み金額 */}
+        <CellContent
+          content={roundTo(+estimateRow.billedAmount).toLocaleString()}
+          disabled={disabled}
+        />
       </TableCell>
       <TableCell>
-        {/* 請求に使用する */
-
-        }
+        {/* 請求に使用する */}
         <FormikLabeledCheckBox
           name={getEstimatesFieldName(+estimateRow.estimateIndex, 'isForPayment')}
+          disabled={disabled}
         />
       </TableCell>
     </TableRow>

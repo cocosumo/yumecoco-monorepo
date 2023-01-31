f4bb41cdfa40c8f6988d891324fa8c6a952f75d4
Lorenz Ras <lenzras@gmail.com>
2023-01-31 15:49:08 +0900

    Update formValidation.ts

--- packages/kokoas-client/src/pages/projContractSearch/formValidation.ts
+++ packages/kokoas-client/src/pages/projContractSearch/formValidation.ts
@@ -12,11 +12,23 @@ const {
 
 /* MAIN VALIDATION SCHEMA */
 
+const amountErrorMessage = '最初の金額は後になるものより小さい必要があります';
+const yupDateNullable = yupJA.date().nullable();
+
 export const validationSchema = yupJA
   .object({
-    contractDateFrom: yupJA.date().nullable(),
-    contractDateTo: yupJA.date().nullable(),
     mainSearch : yupJA.string(),
-    amountTo :  yupNumber,
-    amountFrom : yupNumber.lessThan(yupJA.ref('amountTo'), '最初の金額は後になるものより小さい必要があります。'),
+    contractDateFrom: yupJA
+      .date()
+      .when('contractDateTo', {
+        is: Boolean,
+        then: yupJA.date().max(yupJA.ref('contractDateTo'), '契約日（から）は契約日（まで）より前である必要があります'),
+      })
+      .nullable(),
+    contractDateTo: yupDateNullable,
+    amountFrom : yupNumber.when('amountTo', {
+      is: (value: number) => value !== undefined,
+      then: yupNumber.lessThan(yupJA.ref('amountTo'), amountErrorMessage),
+    }),
+    amountTo : yupNumber,
   });

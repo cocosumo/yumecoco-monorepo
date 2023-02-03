0525d7b080913ba34367d3c79757dc54c984d08b
Lorenz Ras <lenzras@gmail.com>
2023-02-03 12:23:40 +0900

    Update latestInvoiceReducer.ts

--- packages/kokoas-client/src/pages/projContractSearch/hooks/util/latestInvoiceReducer.ts
+++ packages/kokoas-client/src/pages/projContractSearch/hooks/util/latestInvoiceReducer.ts
@@ -1,14 +1,20 @@
 import { IInvoices } from 'types';
 
-/** Reducer fn to get latest issued invoice
- * 直近発行した請求書を取得するreducer関数
- *
+const isInvoiceOfEstimateId = (invoice: IInvoices, id: string) => (
+  invoice.estimateLists.value.some(({ value: inv }) => inv.estimateId.value === id)
+);
+
+const isMoreRecent = (acc: IInvoices, cur: IInvoices) => (
+  !acc?.issuedDateTime?.value || acc.issuedDateTime.value < cur.issuedDateTime.value
+);
+
+/**
  * 直近の判定は仮実装です。確認お願いします。~ras
 */
-export const latestInvoiceReducer = (estimateId: string) => (acc: IInvoices, cur: IInvoices) => {
-  const isInvoiceOfEstimateId = cur.estimateLists.value.some(({ value: inv }) => inv.estimateId.value === estimateId);
+export const latestInvoiceReducer = (estimateId: string) =>
+  (acc: IInvoices, cur: IInvoices) => (
+    isInvoiceOfEstimateId(cur, estimateId) && isMoreRecent(acc, cur)
+      ? cur
+      : acc
+  );
 
-  return (isInvoiceOfEstimateId && (!acc?.issuedDateTime?.value || acc.issuedDateTime.value < cur.issuedDateTime.value))
-    ? cur
-    : acc;
-};

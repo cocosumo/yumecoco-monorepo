a6089eae85b0a1e8662ca96129b62e8df36f84be
Lorenz Ras <lenzras@gmail.com>
2023-02-13 12:10:42 +0900

    add development warning

--- packages/kokoas-client/src/pages/projRegister/sections/RecordSelect/SaveToAndpadButton.tsx
+++ packages/kokoas-client/src/pages/projRegister/sections/RecordSelect/SaveToAndpadButton.tsx
@@ -1,6 +1,7 @@
 import { AndpadButton } from 'kokoas-client/src/components/ui/buttons/AndpadButton';
 import SaveIcon from '@mui/icons-material/Save';
 import { Tooltip, Zoom } from '@mui/material';
+import { useSnackBar } from 'kokoas-client/src/hooks';
 
 export const SaveToAndpadButton = (
   {
@@ -9,12 +10,17 @@ export const SaveToAndpadButton = (
     show : boolean
   },
 ) => {
+  const { setSnackState } = useSnackBar();
+  const handleClick = () => {
+    setSnackState({ open:true, message: '開発中です', severity: 'warning' });
+  };
 
   return (
     <Zoom in={show}>
       <Tooltip title={'Anpadへ案件を登録します'}>
 
         <AndpadButton
+          onClick={handleClick}
           fullWidth sx={{ height: '100%' }}
           startIcon={<SaveIcon />}
         >

87a05e5ce6506f4dfb1a7836cdfdeaf583d5e7a1
RPA02coco <87287796+RPA02coco@users.noreply.github.com>
2023-02-20 11:35:03 +0900

    Update BillingTotalBody.tsx

--- packages/kokoas-client/src/pages/projInvoice/fieldComponents/BillingTotalBody.tsx
+++ packages/kokoas-client/src/pages/projInvoice/fieldComponents/BillingTotalBody.tsx
@@ -4,6 +4,7 @@ import { Big } from 'big.js';
 import { calculateAmountBeforeTax } from 'api-kintone';
 import { calcTaxAmount } from 'api-kintone/src/invoice/calculation/calcTaxAmount';
 import { Caption } from 'kokoas-client/src/components';
+import { useMemo } from 'react';
 
 /** 税額　※当面は10%で固定とする */
 const taxRate = 0.1;
@@ -11,73 +12,79 @@ const taxRate = 0.1;
 /** 非課税の適用設定 ※当面は顧客用の請求書のため、非適用とする */
 const isNonTaxableUse = false;
 
+interface BillingTotalResult {
+  billingTotalAfterTax: number,
+  billingTotalBeforeTax: number,
+  taxAmount: number,
+}
+
 export const BillingTotalBody = ({
   estimates,
 }: {
   estimates: TMaterials[]
 }) => {
 
-  let result = {
-    billingTotalAfterTax: 0,
-    billingTotalBeforeTax: 0,
-    taxAmount: 0,
-  };
 
-  if (isNonTaxableUse) {
-    result = estimates.reduce(
-      (acc,
-        {
-          isForPayment,
-          contractAmount,
-          billingAmount,
-          billedAmount,
-          nonTaxableAmount,
-        },
-      ) => {
-        if (!isForPayment) return acc;
+  const result: BillingTotalResult = useMemo(() => {
+    if (isNonTaxableUse) {
 
-        const {
-          billingAmountBeforeTax,
-        } = calculateAmountBeforeTax({
-          contractAmount,
-          nonTaxableAmount,
-          billingAmount,
-          billedAmount,
-          taxRate,
+      estimates.reduce(
+        (acc,
+          {
+            isForPayment,
+            contractAmount,
+            billingAmount,
+            billedAmount,
+            nonTaxableAmount,
+          },
+        ) => {
+          if (!isForPayment) return acc;
+
+          const {
+            billingAmountBeforeTax,
+          } = calculateAmountBeforeTax({
+            contractAmount,
+            nonTaxableAmount,
+            billingAmount,
+            billedAmount,
+            taxRate,
+          });
+
+          return {
+            ...acc,
+            billingTotalAfterTax: Big(acc.billingTotalAfterTax).plus(billingAmount).toNumber(),
+            billingTotalBeforeTax: Big(acc.billingTotalBeforeTax).plus(billingAmountBeforeTax).toNumber(),
+          };
+
+        }, {
+          billingTotalAfterTax: 0,
+          billingTotalBeforeTax: 0,
+          taxAmount: 0,
         });
 
-        return {
-          ...acc,
-          billingTotalAfterTax: Big(acc.billingTotalAfterTax).plus(billingAmount).toNumber(),
-          billingTotalBeforeTax: Big(acc.billingTotalBeforeTax).plus(billingAmountBeforeTax).toNumber(),
-        };
+      return {
+        ...result,
+        taxAmount: Big(result.billingTotalAfterTax).minus(result.billingTotalBeforeTax).toNumber(),
+      };
 
-      }, {
-        billingTotalAfterTax: 0,
-        billingTotalBeforeTax: 0,
-        taxAmount: 0,
-      });
+    } else {
+      const billingTotalAfterTax = estimates.reduce((acc, cur) => {
 
-    result = {
-      ...result,
-      taxAmount: Big(result.billingTotalAfterTax).minus(result.billingTotalBeforeTax).toNumber(),
-    };
+        return Big(acc).plus(cur.billingAmount).toNumber();
 
-  } else {
-    const billingTotalAfterTax = estimates.reduce((acc, cur) => {
+      }, 0);
 
-      return Big(acc).plus(cur.billingAmount).toNumber();
+      const taxAmount = calcTaxAmount(billingTotalAfterTax, taxRate);
 
-    }, 0);
-
-    const taxAmount = calcTaxAmount(billingTotalAfterTax, taxRate);
-
-    result = {
-      billingTotalAfterTax: billingTotalAfterTax,
-      billingTotalBeforeTax: Big(billingTotalAfterTax).minus(taxAmount).toNumber(),
-      taxAmount: taxAmount,
-    };
-  }
+      return {
+        billingTotalAfterTax: billingTotalAfterTax,
+        billingTotalBeforeTax: Big(billingTotalAfterTax).minus(taxAmount).toNumber(),
+        taxAmount: taxAmount,
+      };
+    }
+  }, [
+    estimates,
+  ]);
 
   /* 税抜金額の算出処理を移管する */
 
@@ -115,7 +122,7 @@ export const BillingTotalBody = ({
           {''}
         </TableCell>
         <TableCell align="left">
-          <Caption text={`消費税額(${taxRate * 100}%)`} />
+          <Caption text={`消費税額(${useMemo(() => (taxRate * 100), [])}%)`} />
         </TableCell>
         <TableCell align="right">
           {`${result.taxAmount.toLocaleString()} 円`}

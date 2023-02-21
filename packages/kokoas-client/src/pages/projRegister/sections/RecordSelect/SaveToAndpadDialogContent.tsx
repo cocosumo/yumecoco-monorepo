c7ce15858813a741042bb0c90d75f96777ff13b2
Lorenz Ras <lenzras@gmail.com>
2023-02-21 09:49:59 +0900

    Update SaveToAndpadDialogContent.tsx

--- packages/kokoas-client/src/pages/projRegister/sections/RecordSelect/SaveToAndpadDialogContent.tsx
+++ packages/kokoas-client/src/pages/projRegister/sections/RecordSelect/SaveToAndpadDialogContent.tsx
@@ -5,7 +5,7 @@ export const SaveToAndpadDialogContent = () => {
   return (
     <DialogContent>
       <DialogContentText>
-        以下の情報が登録されます。
+        Andpadへ格納します。
       </DialogContentText>
     </DialogContent>
   );

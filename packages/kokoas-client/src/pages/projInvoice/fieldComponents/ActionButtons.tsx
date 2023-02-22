a90638038c528d69651c666c593ceeff2c8f43f0
RPA02coco <87287796+RPA02coco@users.noreply.github.com>
2023-02-22 14:23:55 +0900

    Update ActionButtons.tsx

--- packages/kokoas-client/src/pages/projInvoice/fieldComponents/ActionButtons.tsx
+++ packages/kokoas-client/src/pages/projInvoice/fieldComponents/ActionButtons.tsx
@@ -1,17 +1,62 @@
-import { Button } from '@mui/material';
+import { Button, Stack } from '@mui/material';
 import { useFormikContext } from 'formik';
+import { useSnackBar } from 'kokoas-client/src/hooks';
 import { TypeOfForm } from '../form';
 
 export const ActionButtons = () => {
 
-  const { submitForm } = useFormikContext<TypeOfForm>();
+  const { submitForm, values } = useFormikContext<TypeOfForm>();
+  const { invoiceStatus } = values;
+  const { setSnackState } = useSnackBar();
+  const isCreating = invoiceStatus === 'created' || invoiceStatus === '';
+  const isSent = invoiceStatus === 'sent';
+
+
+  const handlesubmit = () => {
+    setSnackState({
+      open: true,
+      severity: 'warning',
+      message: '開発中です',
+    });
+  };
 
   return (
-    <Button
-      variant="contained"
-      onClick={submitForm}
+    <Stack
+      direction="row"
+      justifyContent="flex-start"
+      spacing={2}
     >
-      請求書発行
-    </Button>
+      <Button
+        variant="contained"
+        onClick={submitForm}
+        disabled={isSent}
+      >
+        保存
+      </Button>
+      
+      <Button
+        variant="contained"
+        onClick={handlesubmit}
+        disabled={isSent}
+      >
+        請求書発行
+      </Button>
+      
+      {!isCreating && 
+      <Button
+        variant="contained"
+        onClick={handlesubmit}
+      >
+        再発行
+      </Button>}
+      
+      {!isCreating && 
+      <Button
+        variant="contained"
+        onClick={handlesubmit}
+      >
+        破棄
+      </Button>}
+    </Stack>
   );
 };

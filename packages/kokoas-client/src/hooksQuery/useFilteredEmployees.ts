e32a28d8e3a26d9d5451ea55c1bed3ecabb15aa5
Lorenz Ras <lenzras@gmail.com>
2023-02-25 09:33:10 +0900

    fix employee filter

--- packages/kokoas-client/src/hooksQuery/useFilteredEmployees.ts
+++ packages/kokoas-client/src/hooksQuery/useFilteredEmployees.ts
@@ -4,9 +4,23 @@ import { useEmployees } from './useEmployees';
 import { useCallback } from 'react';
 
 
+
+
 /**
- * Return employees based on filter conditions
+ * フィルタ条件に基づいて社員を返します。
  *
+ * 注意：本関数は、混乱を招く仕組みがあるため、以下の点に注意してください。
+ *
+ * mainStoreId_v2 と mainStore の存在について
+ * どちらも、店舗リストに紐づく社員名簿のフィールドです。
+ * 前バージョンでは mainStore は店舗リストのレコード番号で紐づいていましたが、
+ * テスト環境に移行すると、同じレコード番号にはなりませんでした。
+ * そこで、ココアスに関わる全アプリに uuid を実装することにしました。
+ * ただし、店舗リストに紐づいているアプリがあるため、それらアプリを特定して、
+ * 更新するまで、旧 mainStore や territory などを残しています。
+ *
+ * 社員名簿：
+ * https://rdmuhwtt6gx7.cybozu.com/k/34/
  */
 export const useFilteredEmployees = ({
   storeId = [],
@@ -33,28 +47,29 @@ export const useFilteredEmployees = ({
 
         return data
           .filter(({
-            mainStoreId_v2,
+            mainStoreId_v2: mainStore,
             affStores,
             affiliation,
             役職: empRole,
             territory_v2: _territory,
           }) => {
 
-            const isInStore = storeIds.some((s) => (mainStoreId_v2.value === s
+            const isInStore = storeIds
+              .some((s) => (mainStore.value === s
               || affStores
                 .value
                 .some(({ value: { affStoreId: _storeId } }) => _storeId.value === s )
-            )); 
+              ));
 
             const isAffiliated = !affiliations.length || affiliations.includes(affiliation.value);
             const isInRole = !roles.length || roles.includes(empRole.value);
             const isInTerritory = !territory || territory === _territory.value;
 
             return (
-              isInStore
+              (isInStore
               && isAffiliated
-              && isInRole
-              && isInTerritory
+              && isInRole)
+              || isInTerritory
             );
 
           });

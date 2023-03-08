2c944cfd8355e0852d0732bc6475e7f1e52f48fd
Lorenz Ras <lenzras@gmail.com>
2023-03-08 10:01:26 +0900

    Update ControlledCheckbox.tsx

--- packages/kokoas-client/src/components/reactHookForm/ControlledCheckbox.tsx
+++ packages/kokoas-client/src/components/reactHookForm/ControlledCheckbox.tsx
@@ -2,7 +2,10 @@ import { Checkbox, CheckboxProps, FormControlLabel } from '@mui/material';
 import { Control, Controller, FieldValues, Path, UnPackAsyncDefaultValues } from 'react-hook-form';
 
 
-
+/**
+ * react-hook-form と連携したチェックボックス
+ * MUIのCheckboxPropsを継承しているので、MUIのCheckboxPropsのプロパティをそのまま使える
+ */
 export function ControlledCheckBox<T extends FieldValues >({
   name,
   label,

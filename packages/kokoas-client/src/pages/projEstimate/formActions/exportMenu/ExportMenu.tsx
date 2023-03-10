64d6817c3a71b7256b1b288d210a2a9edf8eb5ac
rpa03 <lenzras@gmail.com>
2023-03-10 16:12:30 +0900

    Fix icons

--- packages/kokoas-client/src/pages/projEstimate/formActions/exportMenu/ExportMenu.tsx
+++ packages/kokoas-client/src/pages/projEstimate/formActions/exportMenu/ExportMenu.tsx
@@ -1,10 +1,10 @@
 import { Box, SpeedDial, SpeedDialAction, Tooltip } from '@mui/material';
 import { RiFileExcel2Fill } from '@react-icons/all-files/ri/RiFileExcel2Fill';
 import { TiExport } from '@react-icons/all-files/ti/TiExport';
-import { FaFileCsv } from '@react-icons/all-files/fa/FaFileCsv';
 import PrintIcon from '@mui/icons-material/Print';
 import SpeedDialIcon from '@mui/material/SpeedDialIcon';
 import { useSnackBar } from 'kokoas-client/src/hooks';
+import { AndpadLogo } from 'kokoas-client/src/components/ui/icons';
 
 
 
@@ -25,26 +25,25 @@ export const ExportMenu = () => {
       <Box>
         <SpeedDial
           ariaLabel='出力'
-          icon={<SpeedDialIcon icon={<TiExport fontSize={'22px'} />} />}
+          icon={<SpeedDialIcon icon={<TiExport size={22} />} />}
           direction={'down'}
           FabProps={{
             size: 'small',
           }}
         >
           <SpeedDialAction
-            icon={<PrintIcon />}
-            tooltipTitle={'印刷'}
+            icon={<AndpadLogo size={22} />}
+            tooltipTitle={'アンドパッド形式'}
             onClick={handleClick}
           />
           <SpeedDialAction
-            icon={<RiFileExcel2Fill fontSize={'22px'} color={'green'} />}
+            icon={<RiFileExcel2Fill size={22} color={'green'} />}
             tooltipTitle={'エクセル'}
             onClick={handleClick}
           />
-
           <SpeedDialAction
-            icon={<FaFileCsv fontSize={'22px'} color={'#C8102E'} />}
-            tooltipTitle={'アンドパッド形式'}
+            icon={<PrintIcon />}
+            tooltipTitle={'印刷'}
             onClick={handleClick}
           />
           {/* 出力形式はここに追加。多くなったら、配列化してリファクタリングする。*/}

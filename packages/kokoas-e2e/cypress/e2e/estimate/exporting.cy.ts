6d3ceb1c794d4d9e7176c28b8b59c9a0d555bf94
rpa03 <lenzras@gmail.com>
2023-03-14 09:20:03 +0900

    add e2e

--- packages/kokoas-e2e/cypress/e2e/estimate/exporting.cy.ts
+++ packages/kokoas-e2e/cypress/e2e/estimate/exporting.cy.ts
@@ -1,10 +1,11 @@
-import { beforeEach, cy, describe } from 'local-cypress';
+import { Cypress, beforeEach, cy, describe, expect } from 'local-cypress';
+import path from 'path';
 
 describe('出力', () => {
+  const downloadsFolder = Cypress.config('downloadsFolder');
 
   beforeEach(() => {
     const testId = '6087cb6b-fa7f-4e75-8a64-0066985fb564';
-
     cy.login();
     cy.visit(`/project/estimate/register?projEstimateId=${testId}`);
     cy.get('[aria-label="出力"]').first()
@@ -12,16 +13,19 @@ describe('出力', () => {
     
   });
 
-  it('ANDPAD形式でエクスポート出来る', () => {
+  it.only('ANDPAD形式でエクスポート出来る', () => {
     cy.get('[aria-label*="アンドパッド"]')
-      .should('be.visible')
       .click();
-    // TODO: ダウンロード出来たかどうかのテスト
+
+
+    const downloadedFilename = path.join(downloadsFolder, '実行予算.xlsx');
+    console.log(downloadsFolder);
+    cy.readFile(downloadedFilename, 'binary', { timeout: 15000 })
+      .should(buffer => expect(buffer.length).to.be.gt(100));
   });
 
   it('EXCEL形式でエクスポート出来る', () => {
     cy.get('[aria-label*="エクセル"]')
-      .should('be.visible')
       .click();
     // TODO: ダウンロード出来たかどうかのテスト
   });

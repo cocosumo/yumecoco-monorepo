0717b84476b6ce63d556d6570e916b4f7c771b66
RPA02coco <87287796+RPA02coco@users.noreply.github.com>
2023-03-15 10:26:26 +0900

    Update parseInvoiceDat.test.ts

--- packages/kokoas-server/src/handleRequest/putInvoiceReport/parseInvoiceDat.test.ts
+++ packages/kokoas-server/src/handleRequest/putInvoiceReport/parseInvoiceDat.test.ts
@@ -2,7 +2,7 @@ import { getInvoiceById } from 'api-kintone/src/invoice/getInvoiceById';
 import { parseInvoiceDat } from './parseInvoiceDat';
 
 describe('parsing invoice record', () => {
-  it('should validate file', async () => {
+  it('returns a parsed invoice record object', async () => {
     const recInvoice = await getInvoiceById('5a7a506f-e8b8-42f0-9437-d54c5d790701');
     const result = await parseInvoiceDat(recInvoice);
     

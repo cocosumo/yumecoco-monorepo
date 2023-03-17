e82f58d196614a5d0b93d4169974f71776b1a4a3
rpa03 <lenzras@gmail.com>
2023-03-17 16:14:14 +0900

    Update ExportToCustEst.tsx

--- packages/kokoas-client/src/pages/projEstimate/formActions/exportMenu/ExportToCustEst.tsx
+++ packages/kokoas-client/src/pages/projEstimate/formActions/exportMenu/ExportToCustEst.tsx
@@ -5,18 +5,24 @@ import { useFormContext } from 'react-hook-form';
 import { TypeOfForm } from '../../form';
 import { kokoasAPIBaseUrl } from 'kokoas-client/src/config/settings';
 import { kokoasEndpoints } from 'libs';
+import { useSnackBar } from 'kokoas-client/src/hooks';
 
 /** 
  * 顧客が閲覧可能な情報をエクセル形式で出力するためのReactコンポーネントです。 
  * */
 export const ExportToCustEst = forwardRef<HTMLElement, SpeedDialActionProps>((props, ref) => {
-
+  const { setSnackState } = useSnackBar();
   const { getValues } = useFormContext<TypeOfForm>();
 
   const handleClick = () => {
     const estimateId = getValues('estimateId');
     const endpoint = `${kokoasAPIBaseUrl}/${kokoasEndpoints.downloadEstimateForCustomer}/${estimateId}`;
-
+    setSnackState({
+      open: true,
+      severity: 'info',
+      // 通じるかな？
+      message: 'この機能はベータ版です。不具合の報告につきましては、管理者までご連絡いただけますようお願いいたします。', 
+    });
     window.open(endpoint);
   };
 

571ee3b537f52579fc7f0bf82e8a7c9870220b23
rpa03 <lenzras@gmail.com>
2023-03-17 16:17:28 +0900

    Update ExportToAndpad.tsx

--- packages/kokoas-client/src/pages/projEstimate/formActions/exportMenu/ExportToAndpad.tsx
+++ packages/kokoas-client/src/pages/projEstimate/formActions/exportMenu/ExportToAndpad.tsx
@@ -5,15 +5,22 @@ import { TypeOfForm } from '../../form';
 import { forwardRef } from 'react';
 import { kokoasAPIBaseUrl } from 'kokoas-client/src/config/settings';
 import { kokoasEndpoints } from 'libs';
+import { useSnackBar } from 'kokoas-client/src/hooks';
 
 export const ExportToAndpad = forwardRef<HTMLElement, SpeedDialActionProps>((props, ref) => {
-
+  const { setSnackState } = useSnackBar();
   const { getValues } = useFormContext<TypeOfForm>();
 
   const handleClick = () => {
     const estimateId = getValues('estimateId');
     const endpoint = `${kokoasAPIBaseUrl}/${kokoasEndpoints.downloadEstimateAsAndpad}/${estimateId}`;
-
+    setSnackState({
+      open: true,
+      severity: 'info',
+      autoHideDuration: 5000,
+      // 通じるかな？
+      message: 'この機能はベータ版です。不具合の報告につきましては、管理者までご連絡いただけますようお願いいたします。', 
+    });
     window.open(endpoint);
   };
 

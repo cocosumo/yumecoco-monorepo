9aa5bdc281971314cd2800ddcb5de865c95b2349
rpa03 <lenzras@gmail.com>
2023-03-22 16:23:28 +0900

    Add documentation

--- packages/kokoas-client/src/pages/projEstimate/tables/estimatesVirtual/rowFields/ControlledCurrencyInput.tsx
+++ packages/kokoas-client/src/pages/projEstimate/tables/estimatesVirtual/rowFields/ControlledCurrencyInput.tsx
@@ -1,7 +1,42 @@
 import { OutlinedCurrencyInput } from 'kokoas-client/src/components';
 import { Controller, useFormContext, useWatch } from 'react-hook-form';
 import { getItemsFieldName, KRowFields, TypeOfForm } from '../../../form';
-import { convertToHalfWidth } from 'libs';
+import { useEffect, useRef } from 'react';
+
+
+/**
+    全角を対応するために、当コンポーネントをuncontrolledにする。
+    そうじゃないと、以下の不具合があります。
+    
+    全角の場合、
+
+      Edge：コンマ後の入力がダブって入る。
+      Chrome：全ての入力がダブって入る。
+
+    半角の場合、
+      Edge：問題なし
+      Chrome：全ての入力がダブって入る。
+
+    英数の場合、
+      どのブラウザでも、問題なし。
+
+    RFHのControllerのrefを渡してもプログラム的な設定では値が反映されないので、
+    ここで別のinputRefを作成してregisterの動作を再現する。ただし、
+    RFHのregisterでは反映されるが、defaultValueが反映されず、
+    既存のデータの初期ロード時にカンマが入らない。
+
+    *ブラウザ又はパッケージ更新で、変わるかもしれない。
+    
+    onCompositionStart, onCompositionEndを使って方法もあるらしいが、
+    ブラウザによっては、挙動が変わります。
+
+    この記事は古いが、同じ辛さ経験しました。
+    https://qiita.com/is_yanyan/items/d14ecb3d2e5b7d119d5d
+    
+    日本の独特な仕様もあるので、 方法は気づいていないだけかもしれません。
+    以下の解決策によりいい方法があれば教えてください。
+
+   */
 
 export const ControlledCurrencyInput = ({
   rowIdx,
@@ -18,15 +53,30 @@ export const ControlledCurrencyInput = ({
 
   const name = getItemsFieldName(rowIdx, fieldName);
 
+  const inputRef = useRef<HTMLInputElement | null>(null);
 
   const [
     envStatus,
+    fieldValue,
   ] = useWatch({
     name: [
       'envStatus',
+      name,
     ],
     control,
   });
+
+  useEffect(() => {
+    if (inputRef.current) {
+      // 表示を更新する
+      // 例：実値は1000だが、表示は1,000になる。
+      inputRef.current.value = fieldValue.toLocaleString();
+    }
+  }, [
+    fieldValue,
+    name,
+    rowIdx,
+  ]);
 
   return (
     <Controller
@@ -44,33 +94,26 @@ export const ControlledCurrencyInput = ({
           isTouched,
         },
       }) => {
-        if (name.includes('costPrice') && rowIdx === 0) {
-          console.log('costPrice', value, Number(value).toLocaleString());
-        }
 
-        const currValue = typeof value === 'string' && value === '' 
-          ? value 
-          : Number(value).toLocaleString(); 
-  
         return (
           <OutlinedCurrencyInput
-            ref={ref}
-            value={currValue}
+            inputRef={(el) => {
+              /** RFHと共有 */ 
+              ref(el);
+              inputRef.current = el;
+            }}
+            defaultValue={value.toLocaleString()}
             name={name}
+            onBlur={onBlur}
             onChange={(e) => {
               const rawValue = e.target.value;
-              const normalizedValue = convertToHalfWidth(rawValue);
-              const newValue = normalizedValue.replace(/,/g, '');
+              //const normalizedValue = convertToHalfWidth(rawValue);
+              //const newValue = normalizedValue.replace(/,/g, '');
  
-              console.log('costPriceOnChange', newValue, e.target.value);
-
-        
-              onChange(newValue);
+              //console.log('costPriceOnChange', newValue, e.target.value);
+              onChange(rawValue);
               handleChange();
-              
-     
             }}
-            onBlur={onBlur}
             error={!!error && isTouched}
             disabled={!!envStatus}
           />

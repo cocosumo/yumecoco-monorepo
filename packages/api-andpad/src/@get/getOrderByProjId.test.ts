c3191857c1443d5d2e787d277ad31f2429e5ab48
rpa03 <lenzras@gmail.com>
2023-03-27 16:14:36 +0900

    Update getOrderByProjId.test.ts

--- packages/api-andpad/src/@get/getOrderByProjId.test.ts
+++ packages/api-andpad/src/@get/getOrderByProjId.test.ts
@@ -7,7 +7,6 @@ describe('getOrderByProjId', () => {
 
     console.log(JSON.stringify(result, null, 2));
     
-    expect(result).toHaveProperty('案件管理ID');
     expect(result.案件管理ID).toBe(projId);
   });
 });

9bdc0dbff072f452d64065224494b980d8c8a52b
RPA02coco <87287796+RPA02coco@users.noreply.github.com>
2023-03-27 12:00:47 +0900

    Update getInvoiceByEstDataId.ts

--- packages/api-kintone/src/invoice/getInvoiceByEstDataId.ts
+++ packages/api-kintone/src/invoice/getInvoiceByEstDataId.ts
@@ -4,7 +4,7 @@ import { appId, RecordType } from './config';
 export const getInvoiceByEstDataId = async (estDataId: string) => {
   if (!estDataId) throw new Error('Invalid estData id.');
 
-  const dataIdKey = 'dataId';
+  const dataIdKey : keyof RecordType['estimateLists']['value'][number]['value'] = 'dataId';
   
   return getRecords<RecordType>({
     app: appId,

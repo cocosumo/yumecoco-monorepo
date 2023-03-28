f92569928223d30a1435c88d4e6ad3c231c3c8ee
rpa03 <lenzras@gmail.com>
2023-03-28 15:22:05 +0900

    add sanitizer

--- packages/kokoas-server/src/handleRequest/reqGetProjectFromAndpadByProjId.ts
+++ packages/kokoas-server/src/handleRequest/reqGetProjectFromAndpadByProjId.ts
@@ -1,14 +1,15 @@
 import { getOrderByProjId } from 'api-andpad';
 import { RequestHandler } from 'express';
+import validator from 'validator';
 
 export const reqGetProjectFromAndpadByProjId: RequestHandler<
 { projId: string }
 > = async (req, res) => {
   try {
-    const projId = req.params.projId;
+    const projId = validator.escape(req.params.projId);
     if (!projId) return res.status(400).send('projId not defined');
     console.log('getOrderByProjId', projId);
-
+    
     const result = await getOrderByProjId(projId);
     if (!result) return res.status(404).send(`Andpadで案件管理IDが見つかりません。Andpadへ登録をお願いします。${projId}`);
     console.log('result', result);

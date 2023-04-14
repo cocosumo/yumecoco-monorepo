0f3fa4b5deb5ca94b801c69622db0fb5723b9d6a
rpa03 <lenzras@gmail.com>
2023-04-14 16:25:07 +0900

    add descriptions

--- packages/kokoas-client/src/pages/projEstimate/tables/estimatesVirtual/rowFields/ControlledCurrencyInput.tsx
+++ packages/kokoas-client/src/pages/projEstimate/tables/estimatesVirtual/rowFields/ControlledCurrencyInput.tsx
@@ -2,66 +2,6 @@ import { Controller, useFormContext, useWatch } from 'react-hook-form';
 import { getItemsFieldName, KRowFields, TypeOfForm } from '../../../form';
 import { NumberCommaField } from 'kokoas-client/src/components/ui/textfield/NumberCommaField';
 
-
-/**
-  全角を対応するために、当コンポーネントをuncontrolledにする。
-  そうじゃないと、以下の不具合があります。
-  
-  全角の場合、
-
-    Edge：コンマ後の入力がダブって入る。
-    Chrome：全ての入力がダブって入る。
-
-  半角の場合、
-    Edge：問題なし
-    Chrome：全ての入力がダブって入る。
-
-  英数の場合、
-    どのブラウザでも、問題なし。
-
-  RHFのControllerのrefを渡してもプログラム的な設定では値が反映されないので、
-  ここで別のinputRefを作成してregisterの動作を再現する。ただし、
-  RHFのregisterでは反映されるが、defaultValueが反映されず、
-  既存のデータの初期ロード時にカンマが入らない。
-
-  *ブラウザ又はパッケージ更新で、変わるかもしれない。
-
-  ------------------
-
-  この記事は古いが、同じ辛さ経験しました。
-  https://qiita.com/is_yanyan/items/d14ecb3d2e5b7d119d5d
-
-
-  一番細かく説明しているのは以下の記事ですが、3年前のものなので、
-  不正確な部分があると分かります。
-  https://qiita.com/darai0512/items/fac4f166c23bf2075deb
-
-  W3c参考しますが、ドラフトなので、ブラウザーによっての実装はさまざま。
-  https://www.w3.org/TR/input-events-2/
-
-  W3cでイベント順番は議論中
-  https://github.com/w3c/uievents/issues/202
-
-  イベントの発火順番も、ブラウザーによって異なる。
-  そして、Google先生のトップに出てくる日本語での記事もほとんど不正確
-  自分で調査したところ、Chromeは以下の順番でイベントが発火する。
-    - compositionstart
-    - input, isComposing = true
-    - change, isComposing = true
-    - compositionend, ここでRHFにて値を更新する
-    - beforeinput, isComposing指定なし、他ブラウザーではcompositionendが先に発火する
-    - input, isComposing = false, この段階から、*条件を揃えば、2重になる
-    - change, isComposing = false
-
-  *条件：componsitionstartの前にselect()を発火し、マウスでIMEを確定
-  
-  その条件はFacebookの検索バーで試して、同じバグが発生します。
-  他サイトでも、強制的にselect()を組み込むと、同じバグが発生します。
-
-  議論中の標準もあり、挙動は変わるかもしれないので、それぞれケースにE2Eテストを書いておきます。
-  ~ ras 2023.03.24
-   */
-
 export const ControlledCurrencyInput = ({
   rowIdx,
   handleChange,

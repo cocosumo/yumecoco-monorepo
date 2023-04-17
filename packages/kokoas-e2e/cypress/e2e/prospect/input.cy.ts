609b65380dbed42f9d63fceb79079f349d0d3a7c
rpa03 <lenzras@gmail.com>
2023-04-17 12:10:33 +0900

    refactored test

--- packages/kokoas-e2e/cypress/e2e/prospect/input.cy.ts
+++ packages/kokoas-e2e/cypress/e2e/prospect/input.cy.ts
@@ -2,6 +2,7 @@ import { convertToFullWidth } from 'libs/src/convertToFullWidth';
 import { beforeEach, context, cy, describe, expect } from 'local-cypress';
 
 describe('見込み管理：入力', () => {
+
   beforeEach(() => {
     const testProjId = '5a5e6cae-bea3-48e9-b679-3dcbbcc7fc60';
     cy.login();
@@ -9,57 +10,45 @@ describe('見込み管理：入力', () => {
   });
 
   context('お金に関わるフィールドに、フォーカスを外したときに、コンマを追加します', () => {
+    const contractAmount = Math.floor(Math.random() * 999999999) + 100000;
 
-    it('半角', () => {
-      // generate random contract amount from 100000 to 999999999
-      const contractAmount = Math.floor(Math.random() * 999999999) + 100000;
-      cy.getTextInputsByLabel('契約予定金額').as('contractAmtField');
+    const testCases = [
+      ['半角', contractAmount.toString() ],
+      ['全角', convertToFullWidth(contractAmount) ],
+    ];
 
-      cy.get('@contractAmtField')
-        .should('be.enabled')
-        .focus();
+    testCases.forEach(([testName, testVal]) => {
+      it(testName, () => {
+        cy.getTextInputsByLabel('契約予定金額').as('contractAmtField');
+        
+        cy.get('@contractAmtField')
+          .should('be.enabled')
+          .focus();
 
-      cy.get('@contractAmtField')
-        .type(contractAmount.toString());
+        cy.get('@contractAmtField')
+          .type(testVal, { delay: 100 });
 
-      cy.get('@contractAmtField').blur();
+        cy.get('@contractAmtField').blur();
 
-      cy.get('@contractAmtField').then(($input) => {
-        const val = $input.val();
-        const normalizeValue = contractAmount.toLocaleString();
-        // コンマで区切られた数値が表示されていることを確認
-        expect(val).to.equal(normalizeValue);
-      });
+        cy.get('@contractAmtField').then(($input) => {
+          const val = $input.val();
+          const normalizeValue = contractAmount.toLocaleString();
+          // コンマで区切られた数値が表示されていることを確認
+          expect(val).to.equal(normalizeValue);
+        });
+
+        cy.contains('button', '保存').click();
+        cy.contains('保存が出来ました').should('exist');
             
 
-    });
-
-    it('全角', () => {
-      // 100000から999999999の間のランダムな契約金額を生成
-      const contractAmount = Math.floor(Math.random() * 999999999) + 100000;
-      const fullWidthVal = convertToFullWidth(contractAmount);
-      cy.getTextInputsByLabel('契約予定金額').as('contractAmtField');
-
-      cy.get('@contractAmtField')
-        .should('be.enabled')
-        .focus();
-
-      cy.get('@contractAmtField')
-        .type(fullWidthVal,  { delay: 100 });
-
-
-      cy.get('@contractAmtField').blur();
-      
-
-      cy.get('@contractAmtField').then(($input) => {
-        const val = $input.val();
-        const normalizeValue = contractAmount.toLocaleString();
-        // コンマで区切られた数値が表示されていることを確認
-        expect(val).to.equal(normalizeValue);
+        cy.get('@contractAmtField').invoke('val')
+          .then((val) => {
+            // 保存後に、コンマが消えていないことを確認
+            expect(val).to.equal(contractAmount.toLocaleString());
+          });
       });
-      
     });
-  }); 
+  });
 
   // TODO：入力中の計算のテスト
 });

47ae5444d64ab7047348cd4351e51f6e5155134c
rpa03 <lenzras@gmail.com>
2023-04-24 16:13:10 +0900

    Update LogDisplay.tsx

--- packages/kokoas-client/src/pages/projRegister/sections/logDisplay/LogDisplay.tsx
+++ packages/kokoas-client/src/pages/projRegister/sections/logDisplay/LogDisplay.tsx
@@ -1,15 +1,22 @@
-import { Box, Grid } from '@mui/material';
+import { Box, Button, Grid } from '@mui/material';
 import { blue } from '@mui/material/colors';
 import { useFormikContext } from 'formik';
 import { TypeOfForm } from '../../form';
 import { LogRow } from './LogRow';
 import { PageSubTitle } from 'kokoas-client/src/components';
+import { useState } from 'react';
 
 export const LogDisplay = () => {
   const { values: {
     andpadDetails,
     logs,
   } } = useFormikContext<TypeOfForm>();
+  
+  // デフォールトは直近の３件のみ表示
+
+  const [isAllLogs, setIsAllLogs] = useState(false);   
+
+  const displayedLogs = isAllLogs ? logs : logs.slice(0, 3) || [];
 
   const isRegistered = !!andpadDetails;
   
@@ -25,8 +32,15 @@ export const LogDisplay = () => {
           }}
         >
           {isRegistered && !logs.length && 'Andpadに登録されています。'}
-          {isRegistered && !!logs.length && logs.map((log) => <LogRow key={log.id} {...log} />)}
+          {isRegistered && !!logs.length && displayedLogs.map((log) => <LogRow key={log.id} {...log} />)}
           {!isRegistered && 'Andpadに登録されていません。'}
+          {logs.length > 3 && (
+            <Button
+              onClick={() => setIsAllLogs(!isAllLogs)}
+            >
+              {`[${logs.length}] ${isAllLogs ? '閉じる' : 'もっと見る'}`}
+            </Button>
+          )}
         </Box>
       </Grid>
     </>

6d2a3ec6eb49b432c0af867a22990b99cd9c1486
rpa03 <lenzras@gmail.com>
2023-04-24 16:01:38 +0900

    add test

---
+++ packages/kokoas-e2e/cypress/e2e/project/log.cy.ts
@@ -0,0 +1,45 @@
+import { beforeEach, cy, describe, expect } from 'local-cypress';
+
+describe('log', () => {
+  const testProjId = '5a5e6cae-bea3-48e9-b679-3dcbbcc7fc60';
+  beforeEach(() => {
+    cy.login();
+    cy.visit(`/project/edit?projId=${testProjId}`);
+  });
+  it('アンドパッドに保存後、ログに表示', () => {
+
+    cy.contains('form div .MuiGrid-root', 'ログ')
+      .next('div')
+      .children('div')
+      .eq(0)
+      .as('latestLog')
+      .invoke('text')
+      .then((text) => {
+        const logBefore = text;
+        cy.log(logBefore);
+        cy.contains('button', 'Andpadへ').as('andpadBtn');
+        cy.get('@andpadBtn').click();
+        cy.contains('button', 'はい').as('saveBtn')
+          .should('be.enabled', { timeout: 10000 });
+        cy.get('@saveBtn').click();
+
+        cy.contains('Andpadへ案件更新しますか').should('not.exist');
+
+        cy.get('@latestLog').invoke('text')
+          .then((text2) => {
+            const logAfter = text2;
+            cy.log(logAfter);
+            expect(logAfter).not.to.equal(logBefore);
+
+          });
+
+      });
+
+    /* 
+    cy.contains('button', 'Andpadへ').as('andpadBtn');
+    cy.get('@andpadBtn').click();
+
+    cy.contains('button', 'はい').as('saveBtn')
+      .should('be.enabled'); */
+  });
+});

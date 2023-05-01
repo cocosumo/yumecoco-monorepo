7b2fca06ff87c85dfc44048c05176084c42abd71
rpa03 <lenzras@gmail.com>
2023-05-01 16:14:08 +0900

    add save api

---
+++ packages/api-kintone/src/contracts/config.ts
@@ -0,0 +1,7 @@
+import { AppIds } from 'config';
+import { IContracts, KContracts } from 'types';
+
+export const appId = AppIds.contracts;
+export type RecordType = IContracts;
+export type RecordKey = KContracts;
+export const dataIdPadding = 2;

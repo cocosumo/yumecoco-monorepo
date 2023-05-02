e73320868e3f3d714ae1fcc9f662d4909f4cb253
rpa03 <lenzras@gmail.com>
2023-05-02 15:30:04 +0900

    Update PreviewContent.tsx

--- packages/kokoas-client/src/pages/projContractV2/parts/preview/PreviewContent.tsx
+++ packages/kokoas-client/src/pages/projContractV2/parts/preview/PreviewContent.tsx
@@ -2,15 +2,21 @@ import { DialogContent } from '@mui/material';
 import { useWatch } from 'react-hook-form';
 import { TypeOfForm } from '../../schema';
 import { useContractFilesById } from 'kokoas-client/src/hooksQuery';
+import { Loading } from 'kokoas-client/src/components/ui/loading/Loading';
 
 export const PreviewContent = () => {
   const contractId = useWatch<TypeOfForm>({
     name: 'contractId',
   });
 
-  const { data } = useContractFilesById({ id: contractId as string });
+  const { data, isLoading } = useContractFilesById({ id: contractId as string });
   
-  console.log(data);
+  const {
+    documents,
+  } = data || {};
+
+  const pdfUrl = `data:application/pdf;base64,${documents?.[0]}`;
+
   
   return (
     <DialogContent
@@ -20,7 +26,19 @@ export const PreviewContent = () => {
         p: 0,
       }}
     >
-      Helllo
+      {isLoading && (<Loading />)}
+      {!isLoading && !documents && (
+        'プレビューの生成が失敗しました。管理者にご連絡ください。'
+      )}
+      {!isLoading && documents && (
+        <object 
+          data={pdfUrl} 
+          type="application/pdf" 
+          width="100%"
+          height='100%'
+        />
+
+      )}
     </DialogContent>
   );
 };

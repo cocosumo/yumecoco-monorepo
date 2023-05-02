642193f2967cddb5928a192551f134d10a70ce79
rpa03 <lenzras@gmail.com>
2023-05-02 14:43:32 +0900

    add endpoint

--- packages/kokoas-server/src/api/docusign/contracts/construction/index.ts
+++ packages/kokoas-server/src/api/docusign/contracts/construction/index.ts
@@ -1,2 +1,5 @@
 export * from './generateContractPdf';
+export * from './generateContractPdfV2';
 export * from './generateContractXlsx';
+export * from './makeEnvelope';
+export * from './processContract';
--- packages/kokoas-server/src/handleRequest/reqDownloadContractV2.ts
+++ packages/kokoas-server/src/handleRequest/reqDownloadContractV2.ts
@@ -1,10 +1,9 @@
 
 import { RequestHandler } from 'express';
-import {
-  generateContractPdf,
-} from '../api/docusign/contracts/';
+
 import { ReqDownloadContractParams } from 'types';
 import { getContractDataV2 } from '../api/kintone/getContractDataV2';
+import { generateContractPdfV2 } from '../api/docusign/contracts';
 
 
 export const reqDownloadContractV2: RequestHandler<
@@ -33,31 +32,17 @@ ReqDownloadContractParams
       envelopeStatus,
     } = contractData;
 
-    res.json(contractData);
-
     console.log('Contract data', projName, envelopeStatus); 
-    /*    switch (fileType) {
-      case 'xlsx':
-        file = await generateContractXlsx(contractData, 'xlsx') as Xlsx;
 
-        res.attachment(`請負契約書 - ${projName}.xlsx`)
-          .status(200);
-        await file.write(res);
-        break;
-      case 'pdf':
-        console.log('Generating pdf');
-        file = await generateContractPdf(contractData, 'base64');
-
-        console.log('PDF File generated');
-        res.status(200).json( {
-          // Array here to accomodate multi-documents in the future
-          documents: [file],
-          envelopeStatus,
-        });
-
-        break;
-    } */
-    //res.end();
+    const file = await generateContractPdfV2(contractData, 'base64');
+    console.log('PDF File generated');
+    res.status(200).json( {
+      // Array here to accomodate multi-documents in the future
+      documents: [file],
+      envelopeStatus,
+    });
+  
+    res.end();
   } catch (err) {
     console.error(err?.message);
     res.status(400).send(

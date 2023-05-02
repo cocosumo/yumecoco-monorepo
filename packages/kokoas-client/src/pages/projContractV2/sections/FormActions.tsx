1cb268fdfe9bdf58a57e06ef29fe6786c65a6411
rpa03 <lenzras@gmail.com>
2023-05-02 12:05:37 +0900

    add error handling

--- packages/kokoas-client/src/pages/projContractV2/sections/FormActions.tsx
+++ packages/kokoas-client/src/pages/projContractV2/sections/FormActions.tsx
@@ -1,10 +1,31 @@
-import { Button, Stack } from '@mui/material';
+import { Alert, Button, Stack } from '@mui/material';
 import SaveIcon from '@mui/icons-material/Save';
 import PreviewIcon from '@mui/icons-material/Preview';
 import { useSubmitHandler } from '../hooks/useSubmitHandler';
+import { useFormState } from 'react-hook-form';
+import { TypeOfForm } from '../schema';
+import { useMemo } from 'react';
 
 export const FormActions = () => {
   const handleSubmit = useSubmitHandler();
+  const {
+    isDirty,
+    isValid,
+    isValidating, 
+  } = useFormState<TypeOfForm>();
+
+  const errorMessage = useMemo(() => {
+    if (isDirty)
+      return '保存されていない変更があります。保存してください。';
+    if (!isValid)
+      return 'フォームにエラーがあります。保存ボタンを押すと、エラー箇所が分かります。';
+    if (isValidating)
+      return '処理中です。';
+  }, [
+    isDirty,
+    isValid,
+    isValidating, 
+  ]);
 
   return (
     <Stack
@@ -26,11 +47,18 @@ export const FormActions = () => {
         variant="outlined"
         size="large"
         startIcon={<PreviewIcon />}
-        //onClick={handleOpenPreview}
-        //disabled={isPreviewDisabled}
+        disabled={isDirty}
       >
         プレビュー
       </Button>
+
+      {errorMessage && (
+      <Alert severity="error" >
+        {errorMessage}
+      </Alert>
+      )}
+
+      
     </Stack>
   );
 };

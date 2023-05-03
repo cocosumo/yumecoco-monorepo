7ae806804734d20f43d4605ceb5a8fca928f9572
Lorenz Ras <lenzras@gmail.com>
2023-05-03 18:02:13 +0900

    Update processContractV2.test.ts

--- packages/kokoas-server/src/handleRequest/reqSendContractDirectV2/processContractV2.test.ts
+++ packages/kokoas-server/src/handleRequest/reqSendContractDirectV2/processContractV2.test.ts
@@ -4,6 +4,11 @@ import { processContractV2 } from './processContractV2';
 it('should porcess contract', async () => {
   console.log(process.env.NODE_ENV);
 
+  // prepare data 
+
+
+  // TODO: handle dlete 
+
   const result = await processContractV2(
     {
       contractId: '12128397-14e7-47d5-90b6-f8b655b39988',

ce4a151c9aae6191a68bba683668285d9fc1998f
Lorenz Ras <lenzras@gmail.com>
2023-05-07 14:58:49 +0900

    Update getBasicKintoneAuth.ts

--- packages/api-kintone/src/@auth/getBasicKintoneAuth.ts
+++ packages/api-kintone/src/@auth/getBasicKintoneAuth.ts
@@ -4,7 +4,6 @@ export const getBasicKintoneAuth = () => {
   const loginAuth = process.env.KT_LOGIN_AUTH;
   const decodedAuth = Buffer.from(loginAuth, 'base64').toString('ascii');
   const [username, password] = decodedAuth.split(':');
-  console.log(username, password);
   return {
     username,
     password,

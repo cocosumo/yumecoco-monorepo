921e5dd279c6a962b3174314144e0e8f48322aab
rpa03 <lenzras@gmail.com>
2023-05-08 15:18:46 +0900

    Update MenuVoidContract.tsx

--- packages/kokoas-client/src/pages/projContractV2/parts/preview/menu/void/MenuVoidContract.tsx
+++ packages/kokoas-client/src/pages/projContractV2/parts/preview/menu/void/MenuVoidContract.tsx
@@ -8,7 +8,8 @@ import { TEnvelopeStatus } from 'types';
 
 /**
  * 無効化（取り下げ）出来ない状態
- * @see 
+ * @see https://support.docusign.com/s/articles/FAQs-related-to-Voiding-Envelopes-in-DocuSign?language=en_US
+ * @see https://support.docusign.com/s/document-item?language=en_US&bundleId=oeq1643226594604&topicId=vks1578456426150.html&_LANG=enus
  */
 const terminalStates: TEnvelopeStatus[] = ['voided', 'voiding', 'completed'];
 

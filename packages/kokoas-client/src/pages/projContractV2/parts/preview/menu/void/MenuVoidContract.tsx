4c98fadf48ee3eab0c5aa87c58a6c03e2523ffb0
rpa03 <lenzras@gmail.com>
2023-05-08 15:08:39 +0900

    Update MenuVoidContract.tsx

--- packages/kokoas-client/src/pages/projContractV2/parts/preview/menu/void/MenuVoidContract.tsx
+++ packages/kokoas-client/src/pages/projContractV2/parts/preview/menu/void/MenuVoidContract.tsx
@@ -1,23 +1,39 @@
-import { ListItemText, MenuItem  } from '@mui/material';
+import { MenuItem, Tooltip  } from '@mui/material';
 
 import { VoidContractDialog } from './VoidContractDialog';
 import { useState } from 'react';
+import { useWatch } from 'react-hook-form';
+import { TypeOfForm } from 'kokoas-client/src/pages/projContractV2/schema';
+import { TEnvelopeStatus } from 'types';
 
-
+/**
+ * 無効化（取り下げ）出来ない状態
+ * @see 
+ */
+const terminalStates: TEnvelopeStatus[] = ['voided', 'voiding', 'completed'];
 
 export const MenuVoidContract = () => {
   const [open, setOpen] = useState(false);
+  const envelopeStatus = useWatch<TypeOfForm>({
+    name: 'envelopeStatus',
+  }) as TEnvelopeStatus;
+
+  const isTerminalState = terminalStates.includes(envelopeStatus);
 
   return (
     <>
-      <MenuItem onClick={() => {
-        setOpen(true);
-      }}
-      >
-        <ListItemText>
-          取り下げ
-        </ListItemText>
-      </MenuItem>
+      <Tooltip title={isTerminalState ? '進捗中の契約は取り下げできません' : ''}>
+        <div>
+          <MenuItem 
+            disabled={isTerminalState}
+            onClick={() => {
+              setOpen(true);
+            }}
+          >
+            取り下げ
+          </MenuItem>
+        </div>
+      </Tooltip>
       <VoidContractDialog 
         handleClose={()=> setOpen(false)} 
         open={open}

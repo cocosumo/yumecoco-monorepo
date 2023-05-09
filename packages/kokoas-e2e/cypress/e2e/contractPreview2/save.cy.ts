3b7fc6ddd4ab30713c4f0b3663fe1fa592585844
rpa03 <lenzras@gmail.com>
2023-05-09 15:35:36 +0900

    add calculation check

--- packages/kokoas-e2e/cypress/e2e/contractPreview2/save.cy.ts
+++ packages/kokoas-e2e/cypress/e2e/contractPreview2/save.cy.ts
@@ -22,9 +22,56 @@ describe('保存と入力をテストする', () => {
       .type(totalProfit.toString())
       .blur();
 
-    cy.log('税抜金額が計算されていることを確認する');
-    cy.contains('span', '税抜金額').siblings('span')
-      .should('have.text', totalContractAmt);
+    cy.log('計算されていることを確認する');
+    cy.contains('span', '税率')
+      .siblings('span')
+      .invoke('text')
+      .then((textTaxRate) => {
+        // getNumber from text
+        const taxRate = 1 + (Number(textTaxRate.replace(/[^0-9]/g, '')) / 100);
+        return cy.wrap(taxRate);
+      })
+      .then((taxRate) => cy
+        .contains('span', '税抜金額')
+        .siblings('span')
+        .invoke('text')
+        .then((textTaxExcludeAmt) => {
+          const taxExcludedAmt = Number(textTaxExcludeAmt.replace(/[^0-9]/g, ''));
+          return cy.wrap(taxExcludedAmt)
+            .should('eq', Math.round(totalContractAmt / taxRate));
+        }))
+      .then((taxExcludedAmt) => cy
+        .contains('span', '原価')
+        .siblings('span')
+        .invoke('text')
+        .then((textCost) => {
+          const cost = Number(textCost.replace(/[^0-9]/g, ''));
+          cy.wrap(cost)
+            .should('eq', Math.round(taxExcludedAmt - totalProfit));
+          return cy.wrap({
+            taxExcludedAmt,
+            cost,
+          });
+        }))
+      .then(({
+        taxExcludedAmt,
+        cost,
+      }) => cy
+        .contains('span', '粗利率')
+        .siblings('span')
+        .invoke('text')
+        .then((textProfitRate) => {
+          // get profitRate with decimal point
+          const profitRate = Number(textProfitRate.replace(/[^0-9.]/g, ''));
+          console.log(taxExcludedAmt, cost, profitRate);
+         
+          /** 利益率 = ( 単価 - 原価) / 単価 */
+          const calculatedTaxRate = ((taxExcludedAmt - cost) /  taxExcludedAmt) * 100;
+        
+          cy.wrap(profitRate.toFixed(2))
+            .should('eq', calculatedTaxRate.toFixed(2));
+        }));
+
 
   });
 });
--- packages/kokoas-e2e/cypress/e2e/contractPreview2/testData.ts
+++ packages/kokoas-e2e/cypress/e2e/contractPreview2/testData.ts
@@ -2,11 +2,11 @@
 import { faker } from '@faker-js/faker';
 
 export const correctInputData = () => {
-
+  const minTotalContractAmt = 100000;
   const maxTotalContractAmt = 10000000;
-  
+  const totalContractAmt = faker.datatype.number({ min: minTotalContractAmt, max: maxTotalContractAmt, precision: 1000 });
   return ({ 
-    totalContractAmt: faker.datatype.number({ min: 100000, max: maxTotalContractAmt, precision: 1000 }),
-    totalProfit: faker.datatype.number({ min: 100000, max: maxTotalContractAmt, precision: 1000 }),
+    totalContractAmt,
+    totalProfit: faker.datatype.number({ min: minTotalContractAmt / 2, max: totalContractAmt / 2, precision: 1000 }),
   });
 };

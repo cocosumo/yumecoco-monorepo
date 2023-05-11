11beea2e84af12fc2084489cb75258e391843d83
rpa03 <lenzras@gmail.com>
2023-05-11 17:08:04 +0900

    Create delete.cy.ts

---
+++ packages/kokoas-e2e/cypress/e2e/contractPreview2/delete.cy.ts
@@ -0,0 +1,12 @@
+import { describe, it } from 'local-cypress';
+
+describe('削除処理', () => {
+  it('未処理の契約が削除できること', () => {
+    // TODO
+  });
+
+  it('未処理以外の契約は削除できないこと', () => {
+    // TODO
+  });
+
+});

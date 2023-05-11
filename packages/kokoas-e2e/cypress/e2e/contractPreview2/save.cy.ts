806f7d6d7d07bac1a708d68b5e58cffebf9c300e
rpa03 <lenzras@gmail.com>
2023-05-11 15:19:23 +0900

    create save

--- packages/kokoas-e2e/cypress/e2e/contractPreview2/save.cy.ts
+++ packages/kokoas-e2e/cypress/e2e/contractPreview2/save.cy.ts
@@ -1,5 +1,6 @@
 import { beforeEach, cy, describe, it } from 'local-cypress';
 import { correctInputData, testProjId } from './testData';
+import format from 'date-fns/format';
 
 
 describe('保存処理', { scrollBehavior: 'center' }, () => {
@@ -8,6 +9,7 @@ describe('保存処理', { scrollBehavior: 'center' }, () => {
   beforeEach(() => {
     cy.login();
   });
+
   it('新規保存できること', () => {
     cy.visit(`/project/contract/preview/v2?projId=${testProjId}`);
     cy.getTextInputsByLabel('契約合計金額')
@@ -15,6 +17,22 @@ describe('保存処理', { scrollBehavior: 'center' }, () => {
       .should('have.value', correctInput.totalContractAmt.toString());
 
     cy.getCheckboxesByLabel('契約金').check();
-    cy.get('input[name=""contractAmt""]').should('have.value', correctInput.totalContractAmt.toString());
+    cy.get('input[name="contractAmt"]').should('have.value', correctInput.totalContractAmt.toLocaleString());
+
+    cy.get('input[name="contractAmtDate"]').type(format(new Date(), 'yyyy/MM/dd'));
+
+    // 実際に保存されないように、保存リクエストをモックする。
+    cy.intercept('POST', '**/k/v1/record.json', {
+      statusCode: 200,
+      body: {
+        'revision': '1',
+      },
+    }).as('saveContract');
+
+    cy.contains('button', '保存').click();
+    cy.wait('@saveContract');
+
+    cy.get('.MuiAlert-message').should('contain', '保存が出来ました。');
+
   });
 });

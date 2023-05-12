a7afd2b0540c4cc675ed4276cd9c90065a7ea92f
rpa03 <lenzras@gmail.com>
2023-05-12 14:37:58 +0900

    add delete test

--- packages/kokoas-e2e/cypress/e2e/contractPreview2/delete.cy.ts
+++ packages/kokoas-e2e/cypress/e2e/contractPreview2/delete.cy.ts
@@ -1,4 +1,18 @@
-import { cy, describe, it } from 'local-cypress';
+import { cy, describe, expect, it } from 'local-cypress';
+import { TEnvelopeStatus, envelopeStatuses } from 'types';
+import qs from 'qs';
+
+
+
+const interceptContractRequest = (status: TEnvelopeStatus) => {
+  cy.intercept('GET', '**/k/v1/records.json?app=231*', (req) => {
+    req.continue((res) => {
+      if (res?.body?.records?.[0]?.envelopeStatus) {
+        res.body.records[0].envelopeStatus.value = status;
+      }
+    });
+  }).as('getContract');
+};
 
 describe('削除処理', () => {
   beforeEach(() => {
@@ -8,25 +22,43 @@ describe('削除処理', () => {
       .then((testIds) => {
         cy.visit(`/project/contract/preview/v2?contractId=${testIds.contractId}`);
       });
+  });
+
+  it('未処理の契約が削除できること', () => {
+    interceptContractRequest('');
+    /* * 
+      * Kintone側の処理になるので、
+      * 実際に削除されたテストは不要です。
+      * 当面、契約削の理関数の既存の結合テストで十分です。
+      * 
+      * 実際に削除リクエストを呼ばず、モックする ~ ras 2023-05-12
+      * */
+    cy.intercept('DELETE', '**/k/v1/records.json*', (req) => {
+      const query = qs.parse(req.url.split('?')[1]);
+      expect(query.ids?.length).to.be.eq(1, '削除対象の契約が1件であること');
+      req.reply({
+        statusCode: 200,
+      });
+    }).as('deleteContract');
+
+    cy.contains('button', '削除').click();
+    cy.contains('削除しますか？').should('be.visible');
+
+    cy.contains('.MuiDialogActions-root button', '削除').click();
+
+    cy.wait('@getContract');
+
+    cy.contains('削除が出来ました。').should('be.visible');
 
   });
-  it.only('未処理の契約が削除できること', () => {
-    // intercept API GET request to k/v1/records.json with query param containing app=231
-    cy.intercept('GET', '**/k/v1/records.json?app=231*', (req) => {
-      req.continue((res) => {
-        // modify the response body
-        if (res?.body?.records?.[0]?.envelopeStatus) {
-          res.body.records[0].envelopeStatus.value = 'sent';
-          
-        }
+
+  envelopeStatuses
+    .filter(Boolean)
+    .forEach((status) => {
+      it(`「${status}」状態の契約は削除できないこと`, () => {
+        interceptContractRequest(status);
+        cy.contains('button', '削除').should('be.disabled');
       });
     });
-
-
-  });
-
-  it('未処理以外の契約は削除できないこと', () => {
-    // TODO
-  });
 
 });

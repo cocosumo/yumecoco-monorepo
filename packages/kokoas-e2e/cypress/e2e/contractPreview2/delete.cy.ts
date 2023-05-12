e7afc31e5cbae8b16b406c3520d5c23e68f2db80
rpa03 <lenzras@gmail.com>
2023-05-12 11:01:08 +0900

    add fixture

--- packages/kokoas-e2e/cypress/e2e/contractPreview2/delete.cy.ts
+++ packages/kokoas-e2e/cypress/e2e/contractPreview2/delete.cy.ts
@@ -1,8 +1,15 @@
-import { describe, it } from 'local-cypress';
+import { cy, describe, it } from 'local-cypress';
 
 describe('削除処理', () => {
-  it('未処理の契約が削除できること', () => {
-    // TODO
+  beforeEach(() => {
+    cy.login();
+    cy.fixture('testIds').then((testIds) => {
+      cy.visit(`/project/contract/preview/v2?contractId=${testIds.contractId}`);
+    });
+
+  });
+  it.only('未処理の契約が削除できること', () => {
+    
   });
 
   it('未処理以外の契約は削除できないこと', () => {

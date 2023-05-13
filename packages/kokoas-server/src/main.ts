dafe212162a284ad483ce1fafbb14e16186959c5
rpa03 <lenzras@gmail.com>
2023-05-13 17:50:21 +0900

    Update main.ts

--- packages/kokoas-server/src/main.ts
+++ packages/kokoas-server/src/main.ts
@@ -3,12 +3,14 @@ import express from 'express';
 import docusignSend from './route/docusign';
 import kokoas from './route/kokoas';
 import { ApiNodes } from 'types';
+import axios from 'axios';
 
 
 loadEnv();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+const COCO_BASE_URL = process.env.COCO_BASE_URL;
 
 app.get('/', (_, res )=>{
   res.send('YEHEY');
@@ -19,6 +21,20 @@ const kokoasApiRoot : ApiNodes = 'kokoas';
 
 app.use(`/${docusignApiRoot}`, docusignSend);
 app.use(`/${kokoasApiRoot}`, kokoas);
+
+// cocosumo proxy
+app.get('/wp/*', (req, res) => {
+
+  console.log("'/wp' call", req.url);
+  const actualEndpoint = req.url.replace('/wp', '');
+  const endpoint = `${COCO_BASE_URL}/${actualEndpoint}`;
+
+  console.log('endpoint', endpoint);
+
+  axios.get(endpoint)
+    .then(data => res.json(data))
+    .catch(err => res.status(400).json(err));
+});
 
 app.listen(PORT, () => {
   console.log(`App listening at http://localhost:${PORT}`);

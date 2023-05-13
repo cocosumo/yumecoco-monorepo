96a004d03f8c7f61250b64f4ceca47d38436cf3e
rpa03 <lenzras@gmail.com>
2023-05-13 11:38:39 +0900

    updated alert

--- packages/kokoas-client/src/pages/projContractV2/parts/preview/menu/correct/EditContractDialog.tsx
+++ packages/kokoas-client/src/pages/projContractV2/parts/preview/menu/correct/EditContractDialog.tsx
@@ -1,4 +1,4 @@
-import { Alert, Button, Dialog, DialogActions, DialogContent, DialogTitle } from '@mui/material';
+import { Alert, AlertTitle, Button, Dialog, DialogActions, DialogContent, DialogTitle, Link } from '@mui/material';
 import { DialogCloseButton } from 'kokoas-client/src/components';
 import { Loading } from 'kokoas-client/src/components/ui/loading/Loading';
 import { useContractCorrectView } from 'kokoas-client/src/hooksQuery';
@@ -45,9 +45,18 @@ export const EditContractDialog = ({
         )}
         {!isLoading && (
         <Alert
-          severity="info"
+          severity="warning"
         >
-          送信したエンベロープが完了していない場合は、そのエンベロープを修正することができます。
+          <AlertTitle>
+            送信したエンベロープが完了していない場合は、修正することができます。
+          </AlertTitle>
+  
+          文書を修正したら、必ず［修正］または［変更の破棄］をクリックしてください。文書を修正した後でこれらをクリックせずにブラウザーを閉じると、受信者が文書にアクセスなくなります。
+          詳細は
+          <Link href='https://support.docusign.com/s/articles/FAQs-related-to-correcting-envelopes-in-DocuSign?language=ja&rsc_301' target='_blank'>
+            こちら
+          </Link>
+          。
         </Alert>
         )}
 

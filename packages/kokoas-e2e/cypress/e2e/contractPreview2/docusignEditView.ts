9d305ecfdc0b6da3b11c29083eef069b24e2e416
rpa03 <lenzras@gmail.com>
2023-05-13 14:08:03 +0900

    Create docusignEditView.ts

---
+++ packages/kokoas-e2e/cypress/e2e/contractPreview2/docusignEditView.ts
@@ -0,0 +1,11 @@
+import { beforeEach, cy, describe, it } from 'local-cypress';
+
+describe('getCorrectViewUrl', () => {
+  beforeEach(() => {
+    cy.login();  
+  });
+
+  it('修正画面のURLを取得できる', () => {
+    
+  });
+});

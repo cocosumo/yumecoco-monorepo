68ca764d4d589be536cb2fe50d4e5d6ea2819625
rpa03 <lenzras@gmail.com>
2023-05-23 11:01:00 +0900

    Revert "deprecated some hooks related 見積もり"
    This reverts commit a4f43ba2bb7c428e258aa3c45818547153c58bd3.

--- packages/kokoas-client/src/hooksQuery/useContractsByProjId.ts
+++ packages/kokoas-client/src/hooksQuery/useContractsByProjId.ts
@@ -5,7 +5,6 @@ import { useEstimates } from './useEstimates';
 
 /**
  * 工事番号で契約済み見積もり一覧を取得する
- * @deprecated 契約DBは見積もりから独立になり、このフックは廃止されます。useContractsByProjIdV2ご利用くだご利用ください。
  */
 export const useContractsByProjId = (
   projId = '',

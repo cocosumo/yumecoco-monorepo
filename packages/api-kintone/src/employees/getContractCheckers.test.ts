89443069e5ebc0a15c2f64c6570e7faacd5613a0
rpa03 <lenzras@gmail.com>
2023-05-25 13:57:59 +0900

    Update getContractCheckers.test.ts

--- packages/api-kintone/src/employees/getContractCheckers.test.ts
+++ packages/api-kintone/src/employees/getContractCheckers.test.ts
@@ -5,6 +5,6 @@ describe('getContractCheckers', () => {
     const result = await getContractCheckers('83128853-98af-47af-9e5a-9d711bee4a43');
     console.log(result);
 
-    expect(result).toBeTruthy();
+    expect(result.mainAccounting.mainStore_v2.value).toBe('本社');
   });
 });

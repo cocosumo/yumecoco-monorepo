da647262a34cfe0afd1067520cbae186950a9e47
rpa03 <lenzras@gmail.com>
2023-06-02 15:31:41 +0900

    add color

--- packages/kokoas-client/src/pages/projSearch/FormProjectSearch.tsx
+++ packages/kokoas-client/src/pages/projSearch/FormProjectSearch.tsx
@@ -6,6 +6,7 @@ import { zodResolver } from '@hookform/resolvers/zod';
 import { Filter } from './sections/filter/Filter';
 import { Stack } from '@mui/material';
 import { PageTitle3 } from 'kokoas-client/src/components/ui/labels/PageTitle3';
+import { Result } from './sections/result/Result';
 
 export const FormProjectSearch = () => {
   const formReturn = useForm<TypeOfForm>({
@@ -18,10 +19,12 @@ export const FormProjectSearch = () => {
     <Stack
       spacing={2}
     >
-      <PageTitle3 label='台帳一覧' color='#333333' />
+      <PageTitle3 label='顧客一覧' color='#333333' />
       <FormProvider {...formReturn}>
         <Filter />
       </FormProvider>
+
+      <Result />
 
     </Stack>
   );

28834faff00f0e7b2934765e608f5627b8b67227
rpa03 <lenzras@gmail.com>
2023-06-02 15:30:46 +0900

    fix initialForm

--- packages/kokoas-client/src/pages/projSearch/form.ts
+++ packages/kokoas-client/src/pages/projSearch/form.ts
@@ -1,11 +1,10 @@
 import { TypeOfForm } from './schema';
 
 export const initialForm : TypeOfForm = {
-  stores: [],
+  stores: null,
   areas: null,
   cocoAG: null,
   cocoConst: null,
   yumeAG: null,
   keyword: null,
-  
 };

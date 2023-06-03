bae68499a4610478b3d87f936861752d0e87fd85
rpa03 <lenzras@gmail.com>
2023-06-03 11:59:52 +0900

    migrated rounding to Bigjs

--- packages/libs/src/roundTo.ts
+++ packages/libs/src/roundTo.ts
@@ -1,3 +1,5 @@
+import { Big } from 'big.js';
+
 /**
  * なぜ：
  * 
@@ -7,10 +9,9 @@
  * 22.555.toPrecision(2) // 23 ×
  * Math.round(22.555) // 23 ×
  * 
- * 長くなりますが、小数点に正確さが必要で、追加しました。
- * それ以外、出来るだけ通常の関数かライブラリーを利用します。
- * 
- * ざっくりなので、もし、よりいいやり方ありましたら、以下の編集し、roundTo.test.tsでテストください。
+ * @see https://mikemcl.github.io/big.js/#round
  *  
  */
-export const roundTo = (value: number, precision = 0) =>  Number(Math.round(Number(`${value}e${precision}`)) + 'e-' + precision);
+export const roundTo = (value: number, precision = 0) => new Big(value)
+  .round(precision)
+  .toNumber();

2da260c1ab3283a8860496500d5619373e4a3549
Lorenz Ras <lenzras@gmail.com>
2023-06-09 06:26:38 +0900

    Update Territories.tsx

--- packages/kokoas-client/src/pages/projSearch/sections/filter/filterForm/Territories.tsx
+++ packages/kokoas-client/src/pages/projSearch/sections/filter/filterForm/Territories.tsx
@@ -1,15 +1,42 @@
 import { Checkbox, FormControlLabel, FormGroup, FormLabel } from '@mui/material';
 import Grid from '@mui/material/Unstable_Grid2';
 import { useFormContext, Controller } from 'react-hook-form';
-import { territories } from 'types';
+import { Territory, territories } from 'types';
 import { TypeOfForm } from '../../../schema';
+import { useStores } from 'kokoas-client/src/hooksQuery';
 
 
 export const Territories = () => {
 
   const { 
     control,
+    setValue,
+    getValues,
   } = useFormContext<TypeOfForm>();
+
+  const { data } = useStores((s) => {
+    return s.reduce(
+      (acc, cur) => {
+        const {
+          territory,
+          storeNameShort,
+        } = cur;
+        if (!storeNameShort.value) return acc;
+
+        const resolvedTerritory = territory.value as Territory;
+
+        if (!acc[resolvedTerritory]) {
+          acc[resolvedTerritory] = [];
+        }
+        acc[resolvedTerritory].push(storeNameShort.value);
+        return acc;
+      }, 
+      {} as Record<Territory, string[]>,
+    );
+  });
+
+
+  if (!data) return null;
 
 
   return (
@@ -44,11 +71,18 @@ export const Territories = () => {
                     )} 
                     label={territory}
                     onChange={(_, checked) => {
-                      const newVal = checked 
-                        ? [...(territoriesVal ?? []), territory] 
+                      const newTerritoriesVal = checked
+                        ? [...(territoriesVal ?? []), territory]
                         : (territoriesVal ?? []).filter((t) => t !== territory);
-                        
-                      onChange(newVal); 
+                      onChange(newTerritoriesVal);
+
+                      // side effect that sets stores
+                      const storesVal = getValues('stores');
+                      const updatedStoresVal = checked
+                        ? [...new Set([...(storesVal ?? []), ...data[territory]])]
+                        : (storesVal ?? []).filter((store) => !data[territory].includes(store));
+                      setValue('stores', updatedStoresVal);
+                      
                     }}
                   />
                 </Grid>

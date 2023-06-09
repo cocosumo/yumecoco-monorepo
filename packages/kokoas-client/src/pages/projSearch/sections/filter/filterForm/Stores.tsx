67dc7f6b0834a4fbd464f34e6c42fb705041f588
rpa03 <lenzras@gmail.com>
2023-06-09 15:24:11 +0900

    Update Stores.tsx

--- packages/kokoas-client/src/pages/projSearch/sections/filter/filterForm/Stores.tsx
+++ packages/kokoas-client/src/pages/projSearch/sections/filter/filterForm/Stores.tsx
@@ -2,10 +2,15 @@ import { Checkbox, FormControlLabel, FormGroup, FormLabel } from '@mui/material'
 import Grid from '@mui/material/Unstable_Grid2';
 import { useStores } from 'kokoas-client/src/hooksQuery';
 import { useCallback } from 'react';
+import { Controller, useFormContext } from 'react-hook-form';
 import { IStores } from 'types';
+import { TypeOfForm } from '../../../schema';
 
 
 export const Stores = () => {
+
+  const { control } = useFormContext<TypeOfForm>();
+
   const { data } = useStores(
     useCallback(
       (d: IStores[]) => {
@@ -20,34 +25,49 @@ export const Stores = () => {
       [],
     ),
   );
+
+
   return (
     <FormGroup >
       <FormLabel>
         店舗
       </FormLabel>
-      <Grid container>
+      <Controller 
+        name="stores"
+        control={control}
+        render={({
+          field: {
+            value,
+            onChange,
+          },
+        }) => (
+          <Grid container>
      
-        {data?.map(({ key, label }) => {
-          return (
-            <Grid 
-              key={key} 
-              xs={'auto'}
-            >
-              <FormControlLabel 
-                control={(
-                  <Checkbox 
+            {data?.map(({ key, label }) => {
+              return (
+                <Grid 
+                  key={key} 
+                  xs={'auto'}
+                >
+                  <FormControlLabel 
+                    control={(
+                      <Checkbox 
                     //checked={stores?.includes(label) ?? false}
-                    value={label} 
-                    //onChange={handleChange}
+                        value={label}
+                      />
+                    )} 
+                    label={label}
                   />
-                )} 
-                label={label}
-              />
-            </Grid>
-          );
-        })}
+                </Grid>
+              );
+            })}
       
-      </Grid>
+          </Grid>
+        )}
+      />
+
+
+      
     </FormGroup>
   );
 };

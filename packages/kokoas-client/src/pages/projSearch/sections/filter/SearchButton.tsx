72cec373407f926e18b3c0dffa7612aedcaba0ae
rpa03 <lenzras@gmail.com>
2023-06-10 10:46:53 +0900

    Update SearchButton.tsx

--- packages/kokoas-client/src/pages/projSearch/sections/filter/SearchButton.tsx
+++ packages/kokoas-client/src/pages/projSearch/sections/filter/SearchButton.tsx
@@ -1,10 +1,25 @@
 import { Button, Tooltip } from '@mui/material';
 import SearchIcon from '@mui/icons-material/Search';
+import { useFormContext } from 'react-hook-form';
+import { TypeOfForm } from '../../schema';
 
 export const SearchButton = () => {
+  const {
+    handleSubmit,
+  } = useFormContext<TypeOfForm>();
+
+  const onSubmit = handleSubmit((data) => {
+    // 仮実装、コメントを残します。
+    console.log(data);
+  });
+
+
   return (
     <Tooltip title={'検索'}>
-      <Button variant={'outlined'}>
+      <Button
+        onClick={onSubmit} 
+        variant={'outlined'}
+      >
         <SearchIcon />
       </Button>
     </Tooltip>

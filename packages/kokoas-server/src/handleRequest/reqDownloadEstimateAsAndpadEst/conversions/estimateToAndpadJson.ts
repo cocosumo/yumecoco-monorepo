7930f39eb307a202299c0b17fa28e06cf386e4cb
rpa03 <lenzras@gmail.com>
2023-06-12 16:30:17 +0900

    Update estimateToAndpadJson.ts

--- packages/kokoas-server/src/handleRequest/reqDownloadEstimateAsAndpadEst/conversions/estimateToAndpadJson.ts
+++ packages/kokoas-server/src/handleRequest/reqDownloadEstimateAsAndpadEst/conversions/estimateToAndpadJson.ts
@@ -28,9 +28,9 @@ export const estimateToAndpadJson = ({
         ...initialRow,
         フォルダ１: 大項目?.value,
         工事種類: 大項目?.value,
-        摘要: 部材名?.value,
-        備考1: 中項目?.value,
-        備考2: 部材名?.value,
+        備考1: 部材名?.value,
+        摘要: 中項目?.value,
+        //備考2: 部材名?.value, 空欄
         原単価: +原価?.value,
         見積単価: +単価?.value,
         数量: +数量?.value,

a4c8e99ab7bf90e142d9774e9209a04bab8b4e9d
rpa03 <lenzras@gmail.com>
2023-06-12 10:37:29 +0900

    remapped

--- packages/kokoas-server/src/handleRequest/reqDownloadEstimateAsAndpadEst/conversions/estimateToAndpadJson.test.ts
+++ packages/kokoas-server/src/handleRequest/reqDownloadEstimateAsAndpadEst/conversions/estimateToAndpadJson.test.ts
@@ -8,13 +8,15 @@ describe('estimateToAndpadJson', () => {
 
 
   beforeAll(async () => {
-    const testEstimateId = 'fc8f798a-a73f-4447-b4bb-99d51da2f198';
+    const testEstimateId = '7f595c5f-09bc-406f-9b0b-5454f2c30d63';
     testEstimateRec = await getEstimateById(testEstimateId);
     expect(testEstimateRec).toBeTruthy();
   });
 
   it('見積書のレコードから、ANDPADで必要な情報となる情報をJSON形式に抽出します', () => {
     const result = estimateToAndpadJson(testEstimateRec);
+
+    console.log(result);
     expect(result).toBeTruthy();
   }); 
 });

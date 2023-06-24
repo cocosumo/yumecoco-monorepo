486b836460b8c16a29d17a02bcaf36eba676efd2
Lorenz Ras <lenzras@gmail.com>
2023-06-24 20:55:23 +0900

    Update useTypedRHF.tsx

--- packages/kokoas-client/src/pages/projRegisterV2/hooks/useTypedRHF.tsx
+++ packages/kokoas-client/src/pages/projRegisterV2/hooks/useTypedRHF.tsx
@@ -1,5 +1,11 @@
 import { useFormContext, useWatch } from 'react-hook-form';
 import { TForm } from '../schema';
 
+/** 
+ * RHF Wrappers to avoid type import hell
+ */
+
 export const useTypedFormContext = useFormContext<TForm>;
+
 export const useTypedWatch = useWatch<TForm>;
+

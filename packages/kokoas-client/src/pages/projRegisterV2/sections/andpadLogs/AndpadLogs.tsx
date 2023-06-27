68910e7271f8a44e9c7f90db342cad3752abb341
rpa03 <lenzras@gmail.com>
2023-06-27 13:27:35 +0900

    add animation

--- packages/kokoas-client/src/pages/projRegisterV2/sections/andpadLogs/AndpadLogs.tsx
+++ packages/kokoas-client/src/pages/projRegisterV2/sections/andpadLogs/AndpadLogs.tsx
@@ -1,9 +1,10 @@
-import { Button, Stack, Typography } from '@mui/material';
+import { Button, Collapse, Stack, Typography } from '@mui/material';
 import { useTypedWatch } from '../../hooks/useTypedRHF';
 import { TForm } from '../../schema';
 import format from 'date-fns/format';
 import { grey } from '@mui/material/colors';
 import { useState } from 'react';
+import { TransitionGroup } from 'react-transition-group';
 
 export const AndpadLog = () => {
   const [expanded, setExpanded] = useState(false);
@@ -16,37 +17,44 @@ export const AndpadLog = () => {
       mt={20}
       justifyContent={'flex-start'}
       alignItems={'flex-start'}
+      sx={{
+        transition: 'all 1s ease-in-out',
+      }}
     >
-      {logs.slice(0, !expanded ? 3 : undefined)?.map(({
-        id,
-        log,
-        dateTime,
-      }) => (
-        <Stack 
-          direction={'row'} 
-          spacing={2} 
-          key={id}
-        >
-          <Typography
-            variant='caption'
-            color={grey[400]}
-            minWidth={100}
-          >
-            {dateTime ? format(dateTime, 'yyyy/MM/dd HH:mm') : '-'}
-          </Typography>
-          <Typography
-            variant='caption'
-            color={grey[400]}
-          >
-            {log}
-          </Typography>
-        </Stack>
-      ))}
+      <TransitionGroup>
+        {logs.slice(0, !expanded ? 3 : undefined)?.map(({
+          id,
+          log,
+          dateTime,
+        }) => (
+          <Collapse key={id}>
+          
+            <Stack 
+              direction={'row'} 
+              spacing={2} 
+            >
+              <Typography
+                variant='caption'
+                color={grey[400]}
+                minWidth={100}
+              >
+                {dateTime ? format(dateTime, 'yyyy/MM/dd HH:mm') : '-'}
+              </Typography>
+              <Typography
+                variant='caption'
+                color={grey[400]}
+              >
+                {log}
+              </Typography>
+            </Stack>
+          </Collapse>
+        ))}
+      </TransitionGroup>
       <Button
         size='small'
         onClick={() => setExpanded((prev) => !prev)}
       >
-        {`${ expanded ? '非表示' :  `全部表示 ${logs.length})`} `}
+        {`${ expanded ? '非表示' :  `全部表示 (${logs.length})`} `}
 
       </Button>
     </Stack>

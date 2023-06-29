685934ebeaf0feda4127646aa312b2674e09f4d8
rpa03 <lenzras@gmail.com>
2023-06-29 10:04:06 +0900

    navigation hook

--- packages/kokoas-client/src/pages/projRegisterV2/sections/contractsSummary/Contracts.tsx
+++ packages/kokoas-client/src/pages/projRegisterV2/sections/contractsSummary/Contracts.tsx
@@ -1,6 +1,8 @@
 import { TabContext, TabList, TabPanel } from '@mui/lab';
-import { Box, Tab } from '@mui/material';
+import { Box, Button, Tab } from '@mui/material';
+import { StaticContentActions } from 'kokoas-client/src/components/ui/information/StaticContentActions';
 import { StaticContentInfos } from 'kokoas-client/src/components/ui/information/StaticContentInfos';
+import { useNavigateWithQuery } from 'kokoas-client/src/hooks';
 import { useState } from 'react';
 import { IContracts } from 'types';
 
@@ -9,49 +11,63 @@ export const Contracts = ({
 }: {
   data: IContracts[]
 }) => {
-  const [value, setValue] = useState('1');
+  const [value, setValue] = useState(data[0].uuid.value);
+  const navigate = useNavigateWithQuery();
+
   const handleChange = (_: unknown, newValue: string) => {
-    setValue(String(newValue));
+    setValue(newValue);
   };
 
 
 
   return (
-    <TabContext value={value}>
-      <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
-        <TabList onChange={handleChange} aria-label="lab API tabs example">
-          {data.map((_, index) => (
-            <Tab key={`契約${index + 1}`} label={`契約${index + 1}`} value={String(index + 1)} />
-          ))}
-        </TabList>
-      </Box>
-      {data.map((
-        contract, 
-        index,
-      ) => {
-        const {
-          uuid,
-          totalContractAmt,
-          contractDate,
-        } = contract;
+    <>
+  
+      <TabContext value={value}>
+        <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
+          <TabList onChange={handleChange} aria-label="lab API tabs example">
+            {data.map(({ uuid }, index) => (
+              <Tab key={`契約${index + 1}`} label={`契約${index + 1}`} value={uuid.value} />
+            ))}
+          </TabList>
+        </Box>
+        {data.map((
+          contract, 
+        ) => {
+          const {
+            uuid,
+            totalContractAmt,
+            contractDate,
+          } = contract;
 
-        const parsedData = [
-          { label: '契約金額', value: `${(+totalContractAmt.value).toLocaleString()} 円` },
-          { label: '契約日', value: contractDate.value },
-        ];
+          const parsedData = [
+            { label: '契約金額', value: `${(+totalContractAmt.value).toLocaleString()} 円` },
+            { label: '契約日', value: contractDate.value },
+          ];
 
-        return (
-          <TabPanel 
-            key={uuid.value} 
-            value={String(index + 1)}
-            sx={{
-              px: 0,
-            }}
-          >
-            <StaticContentInfos data={parsedData} />
-          </TabPanel>
-        );
-      })}
-    </TabContext>
+          return (
+            <TabPanel 
+              key={uuid.value} 
+              value={uuid.value}
+              sx={{
+                px: 0,
+              }}
+            >
+              <StaticContentInfos data={parsedData} />
+            </TabPanel>
+          );
+        })}
+      </TabContext>
+      <StaticContentActions>
+        <Button
+          onClick={() => navigate('projContractPreviewV2', {
+            contractId: value,
+          })}
+          variant='outlined'
+        >
+          編集
+        </Button>
+      </StaticContentActions>
+    </>
   );
 };

1dcf01bc43981406171816be6de98b4c5b7d35bd
rpa03 <lenzras@gmail.com>
2023-07-05 14:07:34 +0900

    removed andpad

--- packages-automation/auto-andpad/src/login/login.test.ts
+++
@@ -1,9 +0,0 @@
-import { describe, expect } from '@jest/globals';
-import { login } from './login';
-
-describe('Login', () => {
-  it('should login', () => {
-    await login()
-    expect(1).toBe(1);
-  });
-});

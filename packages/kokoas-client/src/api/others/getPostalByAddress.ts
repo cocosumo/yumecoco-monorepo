0fa18bb1d9b55d75b3e6e541b9cd44cea5327df3
rpa03 <lenzras@gmail.com>
2023-07-07 09:47:21 +0900

    Update getPostalByAddress.ts

--- packages/kokoas-client/src/api/others/getPostalByAddress.ts
+++ packages/kokoas-client/src/api/others/getPostalByAddress.ts
@@ -4,18 +4,25 @@ export const getPostalByAddress = async (
   address: string,
 ) => {
 
-  const result = await getAddressDetails(address);
+  try {
+    const result = await getAddressDetails(address);
 
-  const { data } = result;
+    const { data } = result;
 
-  const postalComponent = data
-    .results?.[0]
-    .address_components
-    ?.find((component) => component.types.includes('postal_code'));
+    const postalComponent = data
+      .results?.[0]
+      .address_components
+      ?.find((component) => component.types.includes('postal_code'));
 
 
-  if (!postalComponent) return '';
+    if (!postalComponent) return '';
 
-  return postalComponent.short_name.replace('-', '');
+    return postalComponent.short_name.replace('-', '');
+
+
+  } catch (err) {
+    console.error(`Error: getPostalByAddress. Params: ${address}. ${err?.message} `);
+    return '';
+  }
 
 };

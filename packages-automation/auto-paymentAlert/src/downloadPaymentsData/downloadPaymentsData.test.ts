a3a24b6e220c4c552f89cf2e7fe917ad251b8e72
YokoHayakawa <kaba3kobu@gmail.com>
2023-07-10 11:12:05 +0900

    Update downloadPaymentsData.test.ts

--- packages-automation/auto-paymentAlert/src/downloadPaymentsData/downloadPaymentsData.test.ts
+++ packages-automation/auto-paymentAlert/src/downloadPaymentsData/downloadPaymentsData.test.ts
@@ -1,44 +1,46 @@
-import { downloadFile } from 'auto-common';
 import fs from 'fs';
-import path from 'path';
-import xlsx from 'xlsx';
 import { connectToBrowserPage } from 'auto-common/src/connectToBrowserPage';
+import { downloadPaymentfile } from './downloadPaymentsData';
+import path from 'path';
+import { describe, it, expect } from '@jest/globals';
 
 
+/**
+ * 対象のファイルのタイムスタンプを取得する
+ * @param filePath : タイムスタンプを取得するファイルのパス
+ * @returns 
+ */
+function getFileModifiedTime(filePath: string): number {
+  const stats = fs.statSync(filePath);
+  return stats.mtimeMs;
+}
 
 describe('Download Payment File', () => {
-  it('should download excel file from andpad', async () => {
+  it('should download csv file from andpad', async () => {
     // Must be logged in to andpad. It doesn't matter which page of andpad.
-
 
     const {
       page,
       browser,
     } = await connectToBrowserPage();
 
-    const result = await downloadFile(
-      page,
-      'https://andpad.jp/manager/our/box_in/customer_agreement_payments?action=index&controller=manager%2Fpc%2Fusers%2Four%2Fbox_in%2Fcustomer_agreement_payments&format=xlsx&host=andpad.jp&page=1&pp=20&q%5Bplanned_date_start%5D=2023-07&q%5Bs%5D=created_at+desc',
-    );
+    const tempFolderPath = path.join(__dirname, '__TEMP__');
+    const csvFilePath = `${tempFolderPath}/入金一覧.csv`;
+
+    // フォルダが存在しない場合は作成
+    if (!fs.existsSync(tempFolderPath)) {
+      fs.mkdirSync(tempFolderPath);
+    }
+
+    const initialMtimeMs = getFileModifiedTime(csvFilePath);
 
 
-    const data = Buffer.from(result, 'binary');  // バイナリをバッファーへ変換
-    const workbook = xlsx.read(data, { type: 'buffer' });
-    // console.log('workbook', workbook);
+    await downloadPaymentfile(page);
 
+    const updatedMtimeMs = getFileModifiedTime(csvFilePath);
 
-    // ダウンロードしたファイルを保存するディレクトリを作成する 
-    const downloadDir = path.join(__dirname, '__TEMP__');
-
-    // ディレクトリが存在するかどうかを確認します。ない場合は作成します。
-    fs.existsSync(downloadDir) || fs.mkdirSync(downloadDir);
-
-    // const outputFileName = `${Date.now()}_入金一覧.xlsx`;
-    const outputFileName = 'test_入金一覧.csv';
-
-    // excelファイルのデータを読み込む
-    xlsx.writeFile(workbook, path.join(downloadDir, outputFileName), { bookType: 'csv' });
-
+    // 処理実行前後で、ファイルの更新時間が変更されていることを確認する
+    expect(updatedMtimeMs).toBeGreaterThan(initialMtimeMs);
 
     browser.disconnect();
   });

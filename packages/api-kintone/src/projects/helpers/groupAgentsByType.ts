b08b40827a0fe925fe3e83ec28898de4752fa7d2
rpa03 <lenzras@gmail.com>
2023-07-11 12:28:28 +0900

    fix spacing

--- packages/api-kintone/src/projects/helpers/groupAgentsByType.ts
+++ packages/api-kintone/src/projects/helpers/groupAgentsByType.ts
@@ -4,16 +4,18 @@ import { RecordType } from '../config';
 export const groupAgentsByType = (
   agents: RecordType['agents'],
 ) => {
-  return agents?.value.reduce((acc, cur) => {
-    const { agentType } = cur.value;
-    const type = agentType.value as TAgents;
-    if (acc[type]) {
-      acc[type].push(cur);
-    } else {
-      acc[type] = [cur];
-    }
-    return acc;
-  }
-  , {} as Record<TAgents, RecordType['agents']['value']>);
+  return agents?.value.reduce(
+    (acc, cur) => {
+      const { agentType } = cur.value;
+      const type = agentType.value as TAgents;
+      if (acc[type]) {
+        acc[type].push(cur);
+      } else {
+        acc[type] = [cur];
+      }
+      return acc;
+    }, 
+    {} as Record<TAgents, RecordType['agents']['value']>,
+  );
   
 };

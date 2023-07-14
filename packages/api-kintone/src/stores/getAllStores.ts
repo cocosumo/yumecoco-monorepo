8a58677611c87316a2e6c4766ca9904e8ed15652
rpa03 <lenzras@gmail.com>
2023-07-14 12:59:59 +0900

    add stores

--- packages/api-kintone/src/stores/getAllStores.ts
+++ packages/api-kintone/src/stores/getAllStores.ts
@@ -7,6 +7,6 @@ import { appId, RecordType } from './config';
 export const getAllStores = async () => {
   return (await ktRecord()).getRecords({
     app: appId, 
-    query: 'sortNumber > 0 order by sortNumber asc', 
+    query: 'sortNumber > 0 order by sortNumber desc', 
   }).then(({ records }) => records as unknown as RecordType[]);
 };

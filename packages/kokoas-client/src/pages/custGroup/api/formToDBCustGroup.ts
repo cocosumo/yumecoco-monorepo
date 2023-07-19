a26201b8e0f21a0979705186d0efc6e5fabd2e61
rpa03 <lenzras@gmail.com>
2023-07-19 13:33:50 +0900

    add save

---
+++ packages/kokoas-client/src/pages/custGroup/api/formToDBCustGroup.ts
@@ -0,0 +1,51 @@
+import { ICustgroups } from 'types';
+import { TForm } from '../schema';
+
+export const formToDBCustGroup = (
+  formData: TForm,
+): Partial<ICustgroups> => {
+
+
+  const {
+    store,
+    cocoAG1,
+    cocoAG2,
+    yumeAG1,
+    yumeAG2,
+    isDeleted,
+  } = formData;
+
+
+  /* Only include specified agents */
+  const agents = Object.entries({
+    cocoAG1,
+    cocoAG2,
+    yumeAG1,
+    yumeAG2,
+  }).reduce((accu, [key, value]) => {
+    if (value) {
+      return [...accu, [key.replace(/\d+/g, ''), value]];
+    }
+    return accu;
+  }, [] as Array<[string, string]>);
+
+
+  return {
+    isDeleted: { value: (+isDeleted).toString() },
+    storeId: { value: store },
+    agents: {
+      type: 'SUBTABLE',
+      value: agents?.map(([type, empId])=>{
+        return {
+          id: '',
+          value: {
+            agentType: { value: type },
+            employeeId: { value: empId },
+            employeeName: { value: 'auto' },
+            email: { value: 'auto' },
+          },
+        };
+      }) || [],
+    },
+  };
+};
--- packages/kokoas-client/src/pages/custGroup/hooks/useSubmitHandler.tsx
+++ packages/kokoas-client/src/pages/custGroup/hooks/useSubmitHandler.tsx
@@ -3,6 +3,8 @@ import { useNavigate } from 'react-router-dom';
 import { useTypedFormContext } from './useTypedHooks';
 import { useSaveCustGroup } from 'kokoas-client/src/hooksQuery';
 import { KForm } from '../schema';
+import { formToDBCustomers } from '../api/formToDBCustomers';
+import { formToDBCustGroup } from '../api/formToDBCustGroup';
 
 export const useSubmitHandler = () => {
   const { setSnackState } = useSnackBar();
@@ -12,22 +14,27 @@ export const useSubmitHandler = () => {
     getValues,
   } = useTypedFormContext();
 
-  const { mutateAsync } = useSaveCustGroup();
+  const { mutateAsync: saveCustGroupMutation } = useSaveCustGroup();
 
   const handleSave = handleSubmit(
     async (data) => {
-      console.log('save!', data);
+      const {
+        custGroupId,
+      } = data;
+      const customerRecords = formToDBCustomers(data);
+      const custGroupRecord = formToDBCustGroup(data);
+
+      const { id: newCustGroupId } = await saveCustGroupMutation({
+        custGroupId: custGroupId,
+        record: custGroupRecord,
+        customerRecords,
+      });
+
+      console.log('save!', newCustGroupId);
     },
+
     (errors) => {
       console.warn(errors); // 保存できない原因で、残す
-      // summarize errors into string
-      const errorString = Object.entries(errors).reduce((acc, [key, value]) => {
-        if (value) {
-          acc += `${key} : ${value.message}\n`;
-        }
-        return acc;
-      }, '');
-
       setSnackState({
         open: true,
         message: 'フォームに不備があります。 修正が出来ない場合はお手数ですが、管理者に連絡してください。', 

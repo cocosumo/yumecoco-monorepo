214b54e219c52f81fff2ea6866faedd76cc1ee33
rpa03 <lenzras@gmail.com>
2023-07-25 11:21:59 +0900

    fix icons

--- packages/kokoas-client/src/pages/@home/sections/newContracts/certViewer/CertViewer.tsx
+++ packages/kokoas-client/src/pages/@home/sections/newContracts/certViewer/CertViewer.tsx
@@ -1,4 +1,8 @@
 import { Button, Dialog, DialogActions, DialogContent, DialogTitle } from '@mui/material';
+import { useNavigateWithQuery } from 'kokoas-client/src/hooks';
+import { useCallback, useRef } from 'react';
+import { toPng, toJpeg } from 'html-to-image';
+
 
 export const CertViewer = ({
   open,
@@ -9,6 +13,27 @@ export const CertViewer = ({
   handleClose: () => void,
   contractId: string,
 }) => {
+  
+  const ref = useRef<HTMLDivElement>(null);
+
+  const navigate = useNavigateWithQuery();
+
+  const onButtonClick = useCallback(() => {
+    if (ref.current === null) {
+      return;
+    }
+
+    toJpeg(ref.current, { cacheBust: true })
+      .then((dataUrl) => {
+        const link = document.createElement('a');
+        link.download = 'my-image-name.png';
+        link.href = dataUrl;
+        link.click();
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, [ref]);
 
 
   return (
@@ -23,8 +48,10 @@ export const CertViewer = ({
         契約報告書
       </DialogTitle>
       <DialogContent
+        ref={ref}
         sx={{
           height: '80vh',
+          color: 'red',
         }}
       >
         Hello
@@ -37,7 +64,17 @@ export const CertViewer = ({
           閉じる
         </Button>
         <Button
+          onClick={() => {
+            navigate('projContractPreviewV2', {
+              contractId,
+            });
+          }} 
+        >
+          編集する
+        </Button>
+        <Button
           variant='contained'
+          onClick={onButtonClick}
         >
           ダウンロード
         </Button>

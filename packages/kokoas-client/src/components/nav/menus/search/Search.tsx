e570fc1436339ef87d711bda540eb52ebc4f11f1
rpa03 <lenzras@gmail.com>
2023-07-31 15:16:35 +0900

    fix tree shaking

--- packages/kokoas-client/src/components/nav/menus/search/Search.tsx
+++ packages/kokoas-client/src/components/nav/menus/search/Search.tsx
@@ -3,7 +3,8 @@ import { LinkListItemButton } from '../common/LinkListItemButton';
 import { Divider } from '@mui/material';
 import PersonSearchIcon from '@mui/icons-material/PersonSearch';
 import FindInPageIcon from '@mui/icons-material/FindInPage';
-import { TbUserQuestion } from 'react-icons/tb';
+import TroubleshootIcon from '@mui/icons-material/Troubleshoot';
+
 
 export const Search = () => {
   return (
@@ -12,7 +13,7 @@ export const Search = () => {
         検索
       </Divider>
       <LinkListItemButton to={pages.projSearch} icon={<PersonSearchIcon />} text={'顧客検索'}  />
-      <LinkListItemButton to={pages.projProspectSearch} icon={<TbUserQuestion size={20} />} text={'見込検索'}  />
+      <LinkListItemButton to={pages.projProspectSearch} icon={<TroubleshootIcon />} text={'見込検索'}  />
       <LinkListItemButton to={pages.projContractSearch} icon={<FindInPageIcon />} text={'契約検索'}  />
 
     </>

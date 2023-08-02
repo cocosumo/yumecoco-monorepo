2ed58589467237ca3d69a6e84b175764ce86531c
rpa03 <lenzras@gmail.com>
2023-08-02 14:37:33 +0900

    Update processContractV2.test.ts

--- packages/kokoas-server/src/handleRequest/reqSendContractDirectV2/processContractV2.test.ts
+++ packages/kokoas-server/src/handleRequest/reqSendContractDirectV2/processContractV2.test.ts
@@ -1,29 +1,44 @@
 import { ukeoiContractVersion } from 'config';
 import { processContractV2 } from './processContractV2';
-import { expect, it } from '@jest/globals';
+import { describe, expect, it } from '@jest/globals';
 
+describe('processContractV2', () => {
+  it('should porcess electronic contract', async () => {
+    console.log(process.env.NODE_ENV);
 
-it('should porcess contract', async () => {
-  console.log(process.env.NODE_ENV);
+    const result = await processContractV2(
+      {
+        contractId: '9d8f1262-0e6b-43ff-a48e-33b300bdd183',
+        signMethod: 'electronic',
+      },
+      'sent',
+    ).catch((e) => {
+      console.log(e);
+      throw new Error(e);
+    });
 
-  // prepare data 
+    console.log(result);
 
+    expect(result).toBeTruthy();
+  }, 30000);
 
-  // TODO: handle dlete 
+  it('should porcess wetInk contract', async () => {
 
-  const result = await processContractV2(
-    {
-      contractId: '9d8f1262-0e6b-43ff-a48e-33b300bdd183',
-      signMethod: 'electronic',
-      ukeoiDocVersion: ukeoiContractVersion,
-    },
-    'sent',
-  ).catch((e) => {
-    console.log(e);
-    throw new Error(e);
-  });
+    const result = await processContractV2(
+      {
+        contractId: '9d8f1262-0e6b-43ff-a48e-33b300bdd183',
+        signMethod: 'wetInk',
+        ukeoiDocVersion: ukeoiContractVersion,
+      },
+      'sent',
+    ).catch((e) => {
+      console.log(e);
+      throw new Error(e);
+    });
 
-  console.log(result);
+    console.log(result);
 
-  expect(result).toBeTruthy();
-}, 30000);
+    expect(result).toBeTruthy();
+  }, 30000);
+
+});

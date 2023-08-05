af169e50913c583e4b34daff8c1de8d1f0776812
rpa03 <lenzras@gmail.com>
2023-08-05 10:56:36 +0900

    Update useAndpadCostMgtData.ts

--- packages/kokoas-client/src/hooksQuery/useAndpadCostMgtData.ts
+++ packages/kokoas-client/src/hooksQuery/useAndpadCostMgtData.ts
@@ -1,20 +1,15 @@
-import { useQuery } from "@tanstack/react-query";
-import { getCostMgtData } from "api-kintone";
-import { AppIds } from "config";
-import { useCommonOptions } from "./useCommonOptions";
+import { useQuery } from '@tanstack/react-query';
+import { getCostMgtData } from 'api-kintone';
+import { AppIds } from 'config';
 
 export const useCostManagementData = (projId: string) => {
-  const {
-    onError,
-  } = useCommonOptions()
 
   return useQuery(
     [AppIds.projects, 'andpad', projId],
     () => getCostMgtData(projId),
     {
-      enabled: !!projId || enabled,
+      enabled: !!projId,
       staleTime: 5000,
-      onError,
     },
   );
 };

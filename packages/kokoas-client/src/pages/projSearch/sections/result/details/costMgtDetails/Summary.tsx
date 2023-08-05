70b52883109f2ad1dfe0d6eebacf343a955d664d
rpa03 <lenzras@gmail.com>
2023-08-05 15:14:39 +0900

    WIP

--- packages/kokoas-client/src/pages/projSearch/sections/result/details/costMgtDetails/Summary.tsx
+++ packages/kokoas-client/src/pages/projSearch/sections/result/details/costMgtDetails/Summary.tsx
@@ -1,3 +1,16 @@
+import { Stack } from '@mui/material';
+
 export const Summary = () => {
-  
-}
+  return (
+    <Stack 
+      spacing={2}
+      direction='row'
+    >
+      <Stack spacing={}>
+
+      </Stack>
+
+
+    </Stack>
+  );
+};

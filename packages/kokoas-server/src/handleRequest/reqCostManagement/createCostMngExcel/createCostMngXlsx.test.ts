43b001ad102de2b96bb783474f0965034e2e281d
rpa03 <lenzras@gmail.com>
2023-09-01 14:43:18 +0900

    fix 発注金額 data source

--- packages/kokoas-server/src/handleRequest/reqCostManagement/createCostMngExcel/createCostMngXlsx.test.ts
+++ packages/kokoas-server/src/handleRequest/reqCostManagement/createCostMngExcel/createCostMngXlsx.test.ts
@@ -1,36 +1,41 @@
-import { beforeAll, describe, expect } from '@jest/globals';
+import { describe, expect, it } from '@jest/globals';
 import { createCostMngXlsx } from './createCostMngXlsx';
 import fs from 'fs';
 import path from 'path';
+import { getCostMgtDataByProjIdV4 } from '../getCostMgtDataByProjIdV4';
 
 
 
 describe('createCostMngXlsx', () => {
-  // Need to run test for getCostMgtDataByProjIdV3,
-  // then transfer the result to the following file.
-  const testDataPath = path.join(__dirname, './__TEST__/testData.json');
-  const testData = JSON.parse(fs.readFileSync(testDataPath, 'utf8'));
-
-  const outputFilePath = path.join(__dirname, `../__TEST__/原価見積_${testData.projNum}.xlsx`);
-
-  // テストが実行される前に実行される処理
-  beforeAll(() => {
-
-    if (fs.existsSync(outputFilePath)) {
-      fs.unlinkSync(outputFilePath);
-    }
-  });
 
 
   it('should get andpad orders by AndpadProjId', async () => {
 
-    /* const projId = '055c2aca-cbdd-42ab-be2a-e4cd6dd362de';
-    const testCostMngDat = await getCostMgtDataByProjId(projId) || {} as GetCostMgtData; */
+
+    const testId = 'e6a8e273-8c62-4fff-82cd-9ca97c97813f';
+    const testDataPath = path.join(__dirname, `./__TEST__/testData_${testId}.json`);
+    let testData = Object.create(null);
+
+
+    if (fs.existsSync(testDataPath)) {
+      testData = JSON.parse(fs.readFileSync(testDataPath, 'utf8'));
+
+    } else {
+
+      testData = await getCostMgtDataByProjIdV4(testId);
+      if (!testData) throw new Error('testData is null');
+      fs.writeFileSync(
+        path.resolve(testDataPath),
+        JSON.stringify(testData, null, 2),
+      );
+    }
+    
     const workbook = await createCostMngXlsx(testData);
 
-    console.log('outputFilePath::', outputFilePath);
+    const outputFilePath = path.join(__dirname, `./__TEST__/原価見積_${testId}.xlsx`);
+
     await workbook.xlsx.writeFile(outputFilePath);
     // ファイルが存在することを確認
     expect(fs.existsSync(outputFilePath)).toBe(true);
-  });
+  }, 50000);
 });

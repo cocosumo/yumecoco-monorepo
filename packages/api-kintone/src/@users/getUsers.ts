3726fd8095cb8e749a6dd0724491b0a58eb17665
YokoHayakawa <kaba3kobu@gmail.com>
2023-09-08 13:48:27 +0900

    リファクタリングgetUsers

--- packages/api-kintone/src/@users/getUsers.ts
+++ packages/api-kintone/src/@users/getUsers.ts
@@ -2,7 +2,12 @@ import axios from 'axios';
 import { IGetUsersResult } from 'types';
 import { ktClient } from '../client';
 
+type Params = {
+  ids?: number[] | number,
+  codes?: string[] | string,
+};
 
+const dataSize = 100;
 
 /**
  * Get kintone users by ids.
@@ -11,12 +16,8 @@ import { ktClient } from '../client';
  * @param obj if not defined, will retrieve all users
  * @returns {Promise<IGetUsersResult>} user api's return type
  */
-export const getUsers = async ({
-  ids, codes,
-}: {
-  ids?: number[] | number,
-  codes?: string[] | string,
-}) => {
+export const getUsers = async (params?: Params) => {
+  const { codes, ids } = params || {};
   const KintoneClient = await ktClient();
   try {
     const baseUrl = `${KintoneClient.getBaseUrl()}`;
@@ -36,18 +37,53 @@ export const getUsers = async ({
       data: {
         ids: cleanIds,
         codes: cleanCodes,
+        size: dataSize,
       },
       headers: {
         'X-Cybozu-Authorization': auth,
       },
     });
 
-
     if (status === 200) {
-      return data as IGetUsersResult;
+
+      // idとcode指定なしで全ユーザーを取得する場合
+      let usersData = data.users as IGetUsersResult['users'];
+      if (!ids && !codes) {
+        for (let i = 1; i < 10; i++) {
+
+          const ofs = dataSize * i;
+          const {
+            status: ofsStatus,
+            data: ofsData,
+          } = await axios({
+            url: endpoint,
+            method: 'get',
+            data: {
+              size: dataSize,
+              offset: ofs,
+            },
+            headers: {
+              'X-Cybozu-Authorization': auth,
+            },
+          });
+
+
+          if (ofsStatus === 200) {
+            usersData = usersData.concat(ofsData.users);
+            if (ofsData.users.length < 100) break;
+          } else {
+            throw new Error(`Request for offset failed. status: ${status} ${ofsData}`);
+          }
+        }
+      }
+
+      usersData = usersData.filter(({ valid }) => valid === true);
+
+      return usersData as IGetUsersResult['users'];
     } else {
       throw new Error(`Request failed. status: ${status} ${data}`);
     }
+
   } catch (err: any) {
     throw new Error(`getUsers: ${err?.response?.data?.message}`);
   }

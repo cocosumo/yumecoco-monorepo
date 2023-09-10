0878e53457a94b301d3ac2f6166000bb70b5beea
rpa03 <lenzras@gmail.com>
2023-09-10 17:13:24 +0900

    refactor

--- packages-kintone-customize/app-229-contracts/jsedit/fee-pullDown.ts
+++ packages-kintone-customize/app-229-contracts/jsedit/fee-pullDown.ts
@@ -1,10 +1,10 @@
 
-import moment from 'moment';
 import { IStores } from 'types';
 import  $ from 'jquery';
 import './yumetetsu_syokairyou.css';
 import { refreshResult } from './refreshResult';
 import { hightlightRowsByWeek } from './highlightRowsByWeek';
+import { initializeContainer } from './initializeContainer';
 
 
 (function () {
@@ -24,23 +24,7 @@ import { hightlightRowsByWeek } from './highlightRowsByWeek';
     console.log('viewId', viewId);
     if (viewId === commissionRates || viewId === commissionRatesDev) {
 
-      $('#custom_toolbar').append(`
-        <label for="selectYear">　年：</label>
-        <select id="selectYear"></select>
-        
-        <label for="selectMonth">月：</label>
-        <select id="selectMonth"></select>
-        
-        <label for="selectStore">店舗：</label>
-        <select id="selectStore"></select>
-
-        <label for="selectWeek">ハイライト：</label>
-        <select id="selectWeek"></select>
-
-        <button id="printButton">印刷</button>
-      `);
-
-      $('#todayDate').text(`作成日：${moment().format('YYYY年MM月DD日')}`);
+      initializeContainer();
 
       const selectYear = document.getElementById('selectYear') as HTMLSelectElement;
       const selectMonth = document.getElementById('selectMonth') as HTMLSelectElement;
@@ -68,34 +52,6 @@ import { hightlightRowsByWeek } from './highlightRowsByWeek';
       }
       const toMonth = new Date().getMonth() + 1;
       selectMonth.value = String(toMonth);
-
-      // middle align 件数　with 30px width
-
-      $('#mainTable')
-        .append(`
-        <thead>
-            <tr>
-              
-                <th style="width: 30px;">
-                  件数
-                </th>
-                <th style="width: 80px;">工事種別</th>
-                <th style="width: 150px;">発注者</th>
-                <th style="width: 200px;">工事名</th>
-                <th style="width: 80px;">契約日</th>
-                <th>契約金額(税込)</th>
-                <th>粗利金額(税抜)</th>
-                <th>粗利金額(税抜)</th> 
-                <th>粗利金額(税込)</th>       <!-- フィールド内は税抜きのためprofit*1.1すること -->
-                <th>夢てつ入金(税込)</th>
-                <th style="width: 70px;">総粗利率</th>
-                <th>AG1</th>
-                <th>AG2</th>
-            </tr>
-        </thead>
-        <tbody>
-        </tbody>
-                `);
         
       kintone.api(
         kintone.api.url('/k/v1/records', true),

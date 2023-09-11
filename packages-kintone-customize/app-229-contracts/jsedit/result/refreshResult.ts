5743e2bc29ab58c1208686472236d65a93ad33e4
rpa03 <lenzras@gmail.com>
2023-09-11 17:59:17 +0900

    fix sorting

--- packages-kintone-customize/app-229-contracts/jsedit/result/refreshResult.ts
+++ packages-kintone-customize/app-229-contracts/jsedit/result/refreshResult.ts
@@ -1,9 +1,10 @@
 import { getContractsGroupedByStore } from '../api/getContractRecords';
 import { getFormValues } from '../api/getFormValues';
 import { getPrintArea } from '../initialize/createPrintArea';
+import { getSelectStore } from '../initialize/createToolbar/createSelectStore';
 import { createHightlightOptions } from './createHightlightOptions';
 import { showResultByStore } from './store/showResultByStore';
-
+import $ from 'jquery';
 
 export const refreshResult = async () => {
   const {
@@ -22,13 +23,28 @@ export const refreshResult = async () => {
   
   getPrintArea().empty();
 
-  console.log(result);
-
-  for (const [storeId, contracts] of Object.entries(result)) {
-
-    await showResultByStore({
-      records: contracts,
-      storeId: storeId,
+  if (store) {
+    // 店舗が選択されている場合はその店舗のみ表示する
+    showResultByStore({
+      records: result[store] || [],
+      storeId: store,
     });
+  } else {
+    // 店舗の選択肢の順に全店舗を表示する
+    getSelectStore().find('option')
+      .each((_, option) => {
+
+        const optVal = $(option).val() as string;
+        if (!optVal) return;
+
+        showResultByStore({
+          records: result[optVal] || [],
+          storeId: optVal,
+        });
+
+      });
   }
+  
+
+
 };

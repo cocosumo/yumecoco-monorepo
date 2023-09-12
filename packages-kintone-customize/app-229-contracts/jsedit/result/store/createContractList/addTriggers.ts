8ef1d804c9cab6129da2dc35e1f41ddb6b986645
rpa03 <lenzras@gmail.com>
2023-09-12 10:29:42 +0900

    fix vulnerability

--- packages-kintone-customize/app-229-contracts/jsedit/result/store/createContractList/addTriggers.ts
+++ packages-kintone-customize/app-229-contracts/jsedit/result/store/createContractList/addTriggers.ts
@@ -5,11 +5,11 @@ import { appId } from '../../../../src/constants';
 export const addTriggers = () => {
 
   $('.table_contracts_list tbody tr').off()
-    .on('click', function () {
+    .on('click', (event) => {
 
 
       const kintonePath = getRecordPath({
-        recordId: $(this).data('id'),
+        recordId: $(event.target).data('id'),
         appId: String(appId),
 
       });

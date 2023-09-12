4d31590da28a17963a3dba130905ce9c80beec67
rpa03 <lenzras@gmail.com>
2023-09-12 10:39:20 +0900

    Update createHeaderTitle.ts

--- packages-kintone-customize/app-229-contracts/jsedit/result/store/createHeaderTitle.ts
+++ packages-kintone-customize/app-229-contracts/jsedit/result/store/createHeaderTitle.ts
@@ -1,6 +1,7 @@
 import { getStoreNameById } from '../../api/getCachedStores';
 import { getFormValues } from '../../api/getFormValues';
 import './createHeaderTitle.css';
+import DOMPurify from 'dompurify';
 
 export const createHeaderTitle = (el: JQuery<HTMLElement>, storeId: string) => {
   const {
@@ -9,10 +10,12 @@ export const createHeaderTitle = (el: JQuery<HTMLElement>, storeId: string) => {
 
   const storeName = getStoreNameById(storeId);
 
-  el.append(`
+  const sanitizedContent = DOMPurify.sanitize(`
     <h2 class="store_header_title">
       ここすも　${storeName}　${month}月契約一覧
     </h2>
   `);
+
+  el.append(sanitizedContent);
   
 };

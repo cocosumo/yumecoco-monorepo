e5faddd86369d8e20646d481976b5b8368dbfb29
rpa03 <lenzras@gmail.com>
2023-09-12 10:24:41 +0900

    Update button.ts

--- packages-kintone-customize/app-229-contracts/jsedit/triggers/button.ts
+++ packages-kintone-customize/app-229-contracts/jsedit/triggers/button.ts
@@ -4,4 +4,6 @@ import { getPrintButton } from '../initialize/createToolbar/createPrintButton';
  * プリントボタンを押したときのトリガーを追加する
  * */
 export const addPrintButtonTrigger = () => getPrintButton()
-  .on('click', window.print);
+  .on('click', () => {
+    window.print();
+  });

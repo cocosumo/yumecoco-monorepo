5052cda25ddf7c43316460e0dcedf8bdc9cabc85
kana-takahashi0705 <cocosumo.takahashi@gmail.com>
2023-09-24 13:38:38 +0900

    フォント調整

--- packages-kintone-customize/app-229-contracts/src/view/indexShow/prospect_6343185/prospect.ts
+++ packages-kintone-customize/app-229-contracts/src/view/indexShow/prospect_6343185/prospect.ts
@@ -2,26 +2,16 @@ import $ from 'jquery';
 import { getCachedStores } from "../../../../jsedit/api/getCachedStores";
 import { displayResult } from './displayResult';
 import './sheet.css';
-import { createPrintButton } from '../../../../jsedit/initialize/createToolbar/createPrintButton';
-import { addPrintButtonTrigger } from '../../../../jsedit/triggers/button';
 
 
 export const prospect = async () => {
     console.log("見込み一覧、開始しました");
 
-    const printButtonId = 'printButton';
-    createPrintButton();
-    addPrintButtonTrigger();
-
-    
-
-
+   
     $('#root').append(`
         <label>店舗：</label>
         <select id="selectStore"></select>
-        <button value='test' id="${printButtonId}">印刷</button>
-
-        
+             
         
         
     `)

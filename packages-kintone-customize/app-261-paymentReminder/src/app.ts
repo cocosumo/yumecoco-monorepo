065a349744384c2746e7364a0e8914f52a81a8e4
YokoHayakawa <kaba3kobu@gmail.com>
2023-10-02 12:24:36 +0900

    Update app.ts

--- packages-kintone-customize/app-261-paymentReminder/src/app.ts
+++ packages-kintone-customize/app-261-paymentReminder/src/app.ts
@@ -5,10 +5,14 @@ import { KeyOfDB } from './types/types';
 import { onChangeRDHandler } from './eventHandlers/onChangeRDHandler';
 import { onEditOrCreateHandler } from './eventHandlers/onEditOrCreateHandler';
 
+const watchFieldKeys = [
+  'reminderDate',
+  'expectedPaymentDate',
+] as Array<KeyOfDB>;
 
 (() => {
   console.log(`Running in ${isProd ? 'production' : 'development'}`);
   kintone.events.on(onDetailShow, onDetailShowHandler);
   kintone.events.on(onEditOrCreate, onEditOrCreateHandler);
-  kintone.events.on(onFieldChange(['reminderDate'] as Array<KeyOfDB>), onChangeRDHandler);
+  kintone.events.on(onFieldChange(watchFieldKeys), onChangeRDHandler);
 })();

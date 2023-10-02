e760e1d051cc9809325586784ceb843b4de0d01c
YokoHayakawa <kaba3kobu@gmail.com>
2023-10-02 10:35:58 +0900

    修正

--- packages-kintone-customize/app-261-paymentReminder/src/eventHandlers/onEditOrCreateHandler.ts
+++ packages-kintone-customize/app-261-paymentReminder/src/eventHandlers/onEditOrCreateHandler.ts
@@ -1,10 +1,10 @@
 import { setFieldShown } from 'api-kintone';
 
 /**
- * レコード編集画面を表示した後のイベント処理
+ * レコード編集画面とレコード追加画面を表示した後のイベント処理
  * サブテーブルと一部メニューを非表示にします
  */
-export const onEditHandler = () => {
+export const onEditOrCreateHandler = () => {
   setFieldShown('notificationSettings', false);
   setFieldShown('scheduledAlertDate', false);
 };

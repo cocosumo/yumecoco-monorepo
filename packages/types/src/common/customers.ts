8791a344d8a9eb8456e8eacc975a154e8ca8dce8
rpa03 <lenzras@gmail.com>
2023-10-09 10:02:49 +0900

    Update customers.ts

--- packages/types/src/common/customers.ts
+++ packages/types/src/common/customers.ts
@@ -15,7 +15,19 @@ export type TProjRank = typeof ranks[number];
 export type TContact = 'email' | 'tel';
 
 
-export const relations = ['契約者', '配偶者', '婚約者', '家「固定電話」', '子', '祖父母', '兄弟姉妹', '同居人', '会社（固定電話）', '法人担当者', 'その他'] as const;
+export const relations = [
+  '契約者',
+  '配偶者',
+  '婚約者',
+  '家「固定電話」',
+  '親',
+  '子',
+  '祖父母',
+  '兄弟姉妹',
+  '同居人',
+  '会社（固定電話）',
+  '法人担当者',
+  'その他'] as const;
 export type TRelation = typeof relations[number];
 
 export const getCustGroupKey = (k: KFlatCustGroup) => k;

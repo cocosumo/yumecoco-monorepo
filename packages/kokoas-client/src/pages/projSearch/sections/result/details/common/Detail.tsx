7da2ce619abd66e10e425a7dc4625b1117e40c09
YokoHayakawa <kaba3kobu@gmail.com>
2023-10-18 12:27:32 +0900

    Update Detail.tsx

--- packages/kokoas-client/src/pages/projSearch/sections/result/details/common/Detail.tsx
+++ packages/kokoas-client/src/pages/projSearch/sections/result/details/common/Detail.tsx
@@ -7,45 +7,31 @@ export const Detail = ({
   value,
   fonstSize,
   isSubtle,
-}: IDetail) => {
-
-  let newVal = value;
-  if (label === '備考' && typeof value === 'string') {
-    // value内の改行文字を<br>要素に変換
-    newVal = value.split('\n').map((line, index) => (
-      <div key={`${label}-${line}`}>
-        {line}
-        {index < value.split('\n').length - 1 && <br />} 
-      </div>
-    ));
-  }
-
-  return (
-    <Stack
-      direction={'row'}
-      sx={{
-        ':hover': {
-          backgroundColor: grey[100],
-        },
-      }}
+}: IDetail) => (
+  <Stack
+    direction={'row'}
+    sx={{
+      ':hover': {
+        backgroundColor: grey[100],
+      },
+    }}
+  >
+    <Typography
+      color={grey[600]}
+      width={'15%'}
+      component={'div'}
+      px={2}
+      fontSize={isSubtle ? 8 : 14}
     >
-      <Typography
-        color={grey[600]}
-        width={'15%'}
-        component={'div'}
-        px={2}
-        fontSize={isSubtle ? 8 : 14}
-      >
-        {label}
-      </Typography>
-      <Typography
-        width={'75%'}
-        color={isSubtle ? grey[600] : undefined}
-        fontSize={isSubtle ? 8 : fonstSize}
-        component={'div'}
-      >
-        {newVal}
-      </Typography>
-    </Stack>
-  );
-};
+      {label}
+    </Typography>
+    <Typography
+      width={'75%'}
+      color={isSubtle ? grey[600] : undefined}
+      fontSize={isSubtle ? 8 : fonstSize}
+      component={'div'}
+    >
+      {value}
+    </Typography>
+  </Stack>
+);

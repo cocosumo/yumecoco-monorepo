8cbec04cde589d575642b692a39204bad2a4b208
Lorenz Ras <lenzras@gmail.com>
2023-11-06 16:08:41 +0900

    Update getContractsSummary.ts

--- packages/api-kintone/src/contracts/getContractsSummary.ts
+++ packages/api-kintone/src/contracts/getContractsSummary.ts
@@ -52,7 +52,6 @@ export const getContractsSummary = (contractRecs: RecordType[]) => {
       newAcc.減額Amt += hasReduction.value === 'はい' ? +reductionAmt.value : 0;
       newAcc.返金Amt += hasRefund.value === 'はい' ? +refundAmt.value : 0;
 
-
       return newAcc;
     },
     {

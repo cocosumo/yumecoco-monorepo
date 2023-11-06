2405a360a22515b416998114c3d2cafbf5c5cec2
kana-takahashi <130330050+kana-takahashi0705@users.noreply.github.com>
2023-11-06 16:15:59 +0900

    Update packages-kintone-customize/app-229-contracts/jsedit/api/calcFontSize.ts
    Co-authored-by: Yoko.H <87287796+YokoHayakawa@users.noreply.github.com>

--- packages-kintone-customize/app-229-contracts/jsedit/api/calcFontSize.ts
+++ packages-kintone-customize/app-229-contracts/jsedit/api/calcFontSize.ts
@@ -2,10 +2,9 @@
 import { getTextWidth } from '../../src/helpers/getTextWidth';
 
 /**
- * 表のセル内に文字を収める。
- * 別ファイルにimportして調整したい部分を指定する。
+ * 取得したテキストの幅を計算して、コンテナに収まるようにサイズを再調整する
  * @param {number} width コンテナ（セル）の幅（px、pt）
- * @param {string} text 取得したテキストの幅を計算して、コンテナに収まるようにサイズを再調整する
+ * @param {string} text 表示する文字列
  * @returns number フォントサイズ
 **/
 

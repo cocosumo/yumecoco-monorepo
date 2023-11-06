95073f22e4e77552898514c5a1390f106ea6e0d1
kana-takahashi <130330050+kana-takahashi0705@users.noreply.github.com>
2023-11-06 16:16:57 +0900

    Update packages-kintone-customize/app-229-contracts/jsedit/api/getAllStoresMeeting.ts
    Co-authored-by: Yoko.H <87287796+YokoHayakawa@users.noreply.github.com>

--- packages-kintone-customize/app-229-contracts/jsedit/api/getAllStoresMeeting.ts
+++ packages-kintone-customize/app-229-contracts/jsedit/api/getAllStoresMeeting.ts
@@ -5,8 +5,8 @@ import { AppIds } from 'config';
 import { IStores } from 'types';
 
 /**
- * 店舗の順番 ：会議資料用
- * @returns 豊田、大林、美里、高浜・・・
+ * kintoneアプリより"meetingNumber"毎にソートした店舗情報を取得します
+ * @returns 店舗情報の配列
 **/
 
 export const getAllStoresMeeting = async () => {

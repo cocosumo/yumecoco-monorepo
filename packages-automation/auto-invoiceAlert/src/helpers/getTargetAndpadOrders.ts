512ce1e5c0a01aeafba98f990199d4b5f4ff0b7c
YokoHayakawa <kaba3kobu@gmail.com>
2023-11-09 12:39:39 +0900

    debug用処理追加

--- packages-automation/auto-invoiceAlert/src/helpers/getTargetAndpadOrders.ts
+++ packages-automation/auto-invoiceAlert/src/helpers/getTargetAndpadOrders.ts
@@ -1,4 +1,4 @@
-import { SaveProjectData, getMyOrders } from 'api-andpad';
+import { SaveProjectData, getMyOrders, getOrderBySystemId } from 'api-andpad';
 import { IContracts, IProjects } from 'types';
 
 
@@ -34,26 +34,24 @@ export const getTargetAndpadOrders = async ({
     return contracts.find(({ projId }) => uuid.value === projId.value);
   });
 
-  const tgtOrders = tgtProjects.filter(({ forceLinkedAndpadSystemId }) => forceLinkedAndpadSystemId.value !== '');
-  const consoleOrders = tgtOrders.map(({ projName }) => projName.value);
-  console.log('tgtSystemIds', tgtOrders.length, consoleOrders);
-
-
-  const queyForce = tgtOrders.map(({ forceLinkedAndpadSystemId }) => `システムID = ${forceLinkedAndpadSystemId.value}`);
-  console.log('queyForce', queyForce);
+  const forceLinkedProjects = tgtProjects.filter(({ forceLinkedAndpadSystemId }) => forceLinkedAndpadSystemId.value !== '');
+  const consoleOrders = forceLinkedProjects.map(({ projName }) => projName.value);
+  console.log('tgtSystemIds', forceLinkedProjects.length, consoleOrders);
 
   // 通常接続と強制接続の案件をまとめる
   let orders = normalOrders.data.objects;
-  if (queyForce.length !== 0) {
+  if (forceLinkedProjects.length !== 0) {
 
-    const forceOrders = await getMyOrders({
-      q: queyForce.join(' OR '),
-      limit: queyForce.length,
-      series: [additionalProperty],
-    });
-    console.log('forceOrders', JSON.stringify(forceOrders.data.objects, null, 2));
+    for (const forceLinkedProject of forceLinkedProjects) {
+      const forceLinkedOrder = await getOrderBySystemId({
+        systemId: forceLinkedProject.forceLinkedAndpadSystemId.value,
+        series: [additionalProperty],
+      });
+      console.log(`forceOrders: systemId=${forceLinkedProject.forceLinkedAndpadSystemId.value},
+       ${JSON.stringify(forceLinkedOrder, null, 2)}`);
 
-    orders = normalOrders.data.objects.concat(forceOrders.data.objects);
+      orders = orders.concat(forceLinkedOrder.data.object);
+    }
   }
 
   return orders;

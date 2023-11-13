917baa8e82064467fa41e3792f11f7de2d7d18fe
Lorenz Ras <lenzras@gmail.com>
2023-11-13 11:26:57 +0900

    add test

--- packages/api-kintone/src/custgroups/searchCustGroupByKeyword.ts
+++ packages/api-kintone/src/custgroups/searchCustGroupByKeyword.ts
@@ -2,6 +2,12 @@ import { RecordKey } from './config';
 import { getAllCustGroups } from './getAllCustGroups';
 
 
+/**
+ * Searches for customer groups by keyword.
+ * @param params - The options object.
+ * @param params.keyword - The keyword to search for.
+ * @returns  A promise that resolves to an array of customer groups.
+ */
 export const searchCustGroupByKeyword = async ({
   keyword,
 }:{

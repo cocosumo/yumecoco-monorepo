a30044276331aece2a888ffa06e65ca844f4f53f
Lorenz Ras <lenzras@gmail.com>
2023-11-13 11:15:05 +0900

    fill the fuction

--- packages/api-kintone/src/custgroups/searchCustGroupByKeyword.ts
+++ packages/api-kintone/src/custgroups/searchCustGroupByKeyword.ts
@@ -1,8 +1,23 @@
+import { RecordKey } from './config';
+import { getAllCustGroups } from './getAllCustGroups';
+
+
 export const searchCustGroupByKeyword = async ({
   keyword,
 }:{
   keyword: string
 }) => {
 
-  console.log(keyword);
+  const keywordKeys : RecordKey[] = [
+    'customerName',
+    'custNameReading',
+  ];
+   
+
+  return getAllCustGroups({
+    condition: keywordKeys
+      .map((field) => `${field} like "${keyword}"`)
+      .join(' or '),
+  });
+  
 };

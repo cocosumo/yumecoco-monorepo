762285cab4e3c47ccc43e86b0449fd3dd26a9339
Lorenz Ras <lenzras@gmail.com>
2023-11-28 13:59:41 +0900

    Update useHasMainContract.ts

--- packages/kokoas-client/src/pages/projContractV2/hooks/useHasMainContract.ts
+++ packages/kokoas-client/src/pages/projContractV2/hooks/useHasMainContract.ts
@@ -20,15 +20,17 @@ export const useHasMainContract = () => {
     ...others
   } = useContractsByProjIdV2(projId);
 
-  const isNewContract = !contractId; // Check if it's a new contract
+  /** 新規契約かどうか */
+  const isNewContract = !contractId; 
 
   return {
     data: !!contracts?.length && contracts
       .some((contract) => {
 
-        const isNotCurrentContract = contract.uuid.value !== contractId; // Check if it's a different contract than the one currently open
+        /** 開いている契約以外の契約かどうか */
+        const isNotCurrentContract = contract.uuid.value !== contractId; 
 
-        return contract.contractType.value === '契約' // Check if it's a main contract
+        return contract.contractType.value === '契約' // 本契約かどうか
           && (
             isNotCurrentContract
             || isNewContract 

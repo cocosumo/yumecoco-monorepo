bfa62d091162d0fed81cde9f24d4d08b30debdf7
YokoHayakawa <kaba3kobu@gmail.com>
2023-11-29 15:02:13 +0900

    init

--- packages-kintone-customize/app-292-grossProfitStatement/src/eventHandlers/formGrossProfitTable/FormGrossProfitTable.tsx
+++ packages-kintone-customize/app-292-grossProfitStatement/src/eventHandlers/formGrossProfitTable/FormGrossProfitTable.tsx
@@ -1,10 +1,36 @@
-import { Typography } from '@mui/material';
+import { Stack } from '@mui/material';
+import { Form, FormProvider, useForm } from 'react-hook-form';
+import { initialForm } from './form';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { TForm, schema } from './schema';
+import { Toolbar } from './toolbar/Toolbar';
 
 export const FormGrossProfitTable = () => {
+  const formReturn = useForm<TForm>({
+    defaultValues: initialForm,
+    resolver: zodResolver(schema),
+  });
 
+  
+  
   return (
-    <Typography variant="h5">
-      粗利集計表
-    </Typography>
+    <FormProvider {...formReturn}>
+      <Form
+        noValidate
+      >
+        <Stack
+          spacing={2}
+          px={4}
+          pt={2}
+          pb={4}
+        >
+
+          <Toolbar />
+          {/* <Results /> */}
+        </Stack>
+
+      </Form>
+    </FormProvider>
   );
 };
+  
---
+++ packages-kintone-customize/app-292-grossProfitStatement/src/eventHandlers/formGrossProfitTable/form.ts
@@ -0,0 +1,14 @@
+import { getFiscalYear } from '../../../../app-229-contracts/src/helpers/getFiscalYear';
+import { getLatestMonths } from '../../../../app-229-contracts/src/helpers/getLatestMonths';
+import { TForm } from './schema';
+
+
+const today = new Date();
+const fiscalYear = getFiscalYear(today);
+
+
+export const initialForm: TForm = {
+  months: getLatestMonths(fiscalYear, 3),
+  year: fiscalYear.toString(),
+  storeId: '',
+};

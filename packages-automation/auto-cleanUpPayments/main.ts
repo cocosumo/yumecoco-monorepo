16e5091f75d2acc4ac6d5620b5f47f9490ef03b3
YokoHayakawa <kaba3kobu@gmail.com>
2023-11-29 14:19:30 +0900

    mainの修正

--- packages-automation/auto-cleanUpPayments/main.ts
+++ packages-automation/auto-cleanUpPayments/main.ts
@@ -1,10 +1,3 @@
 import { cleanupAndpadPayments } from './cleanUpAndpadPayments';
 
-
-import cron from 'node-cron';
-/**
- * Diff sync andpad payment list to kintone.
- *
- * Runs every day at 11:02
- */
-cron.schedule('2 11 * * *', () => cleanupAndpadPayments());
+cleanupAndpadPayments();

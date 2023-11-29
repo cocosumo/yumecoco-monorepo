7b98c6e134994ee8acf19c9bb0d43fdb55b2fc04
Lorenz Ras <lenzras@gmail.com>
2023-11-29 09:54:13 +0900

    Update PreviewContent.tsx

--- packages/kokoas-client/src/pages/projContractV2/parts/preview/PreviewContent.tsx
+++ packages/kokoas-client/src/pages/projContractV2/parts/preview/PreviewContent.tsx
@@ -1,4 +1,4 @@
-import { DialogContent } from '@mui/material';
+import { Alert, AlertTitle, Button, DialogContent, List, ListItem } from '@mui/material';
 import { Loading } from 'kokoas-client/src/components/ui/loading/Loading';
 import { useIsFetching } from '@tanstack/react-query';
 
@@ -28,6 +28,37 @@ export const PreviewContent = ({
           width="100%"
           height='100%'
         />
+      )}
+
+      {!isFetching && !documentB64 && (
+        <Alert 
+          severity={'error'}
+          action={(
+            <Button
+              color={'inherit'}
+              size={'small'}
+              onClick={() => window.location.reload()}
+            >
+              更新
+            </Button>
+          )}
+        >
+          <AlertTitle>
+            プレビューが出来ませんでした。
+          </AlertTitle>
+          <List sx={{ listStyleType: 'disc' }}>
+            <ListItem sx={{ display: 'list-item' }}>
+              ブラウザを更新(F5)してください。
+            </ListItem>
+            <ListItem sx={{ display: 'list-item' }}>
+              メールに電子契約書が届いているか確認してください。
+            </ListItem>
+
+          </List>
+
+          当エラーの再現性について、おかけしますが、システム開発部門にご連絡ください。
+          
+        </Alert>  
       )}
 
     </DialogContent>

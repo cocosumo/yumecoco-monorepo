4767a8c8d94587921bea63b23f047c9ae7aa7de4
YokoHayakawa <kaba3kobu@gmail.com>
2024-01-18 15:06:39 +0900

    Update convAlertDate.ts

--- packages-automation/auto-paymentAlertV2/src/helpers/convAlertDate.ts
+++ packages-automation/auto-paymentAlertV2/src/helpers/convAlertDate.ts
@@ -1,4 +1,5 @@
 import addDays from 'date-fns/addDays';
+import format from 'date-fns/format';
 
 /**
  * 通知予定日と支払予定日を比較し、遅い方の日付を次回の通知日に設定する
@@ -15,8 +16,9 @@ export const convAlertDate = ({
 }) => {
 
   // 支払予定日の翌日が通知日となる場合
-  if (expectedPaymentDate && new Date(scheduledAlertDate) <= addDays(new Date(expectedPaymentDate), 1)) {
-    return addDays(new Date(expectedPaymentDate), 1);
+  if (expectedPaymentDate
+     && new Date(scheduledAlertDate).getTime() <= addDays(new Date(expectedPaymentDate), 1).getTime()) {
+    return format(addDays(new Date(expectedPaymentDate), 1), 'yyyy-MM-dd');
   }
 
   return scheduledAlertDate;

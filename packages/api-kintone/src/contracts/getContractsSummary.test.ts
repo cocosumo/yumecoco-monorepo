839cb27c6ab194eb2bc0c33d11c7d618daec7add
YokoHayakawa <kaba3kobu@gmail.com>
2024-01-19 11:48:04 +0900

    Update getContractsSummary.test.ts

--- packages/api-kintone/src/contracts/getContractsSummary.test.ts
+++ packages/api-kintone/src/contracts/getContractsSummary.test.ts
@@ -140,6 +140,74 @@ describe('getContractsSummary', () => {
   }, 60000);
 
 
+  it('減額工事の契約書の要約ができること', async () => {
+
+    const contractDat = await getContractById('3bbf6f94-5958-4d7f-810c-a7891a722c36');
+
+    const formalContract = produce(contractDat, draft => {
+      // 本契約レコードを作成
+      draft.contractType.value = '契約';
+      draft.contractAddType.value = '';
+
+      // 契約金額を設定
+      draft.totalContractAmt.value = '5000000';
+
+      // 返金を設定
+      draft.hasRefund.value = 'いいえ';
+      draft.refundAmt.value = '0';
+
+      // 減額を設定
+      draft.hasReduction.value = 'いいえ';
+      draft.reductionAmt.value = '0';
+
+      // 補助金を設定
+      draft.hasSubsidy.value = 'いいえ';
+      draft.subsidyAmt.value = '0';
+    });
+
+
+
+    const addtionalContract = produce(contractDat, draft => {
+      // 追加契約を取得
+      // 契約書区分の修正
+      draft.contractType.value = '追加';
+      draft.contractAddType.value = '減額工事';
+
+      // 契約金額を設定
+      draft.totalContractAmt.value = '-50000';
+
+      // 返金を設定
+      draft.hasRefund.value = 'いいえ';
+      draft.refundAmt.value = '0';
+
+      // 減額を設定
+      draft.hasReduction.value = 'いいえ';
+      draft.reductionAmt.value = '0';
+
+      // 補助金を設定
+      draft.hasSubsidy.value = 'いいえ';
+      draft.subsidyAmt.value = '0';
+    });
+
+    const contracts = [formalContract, addtionalContract];
+
+
+    console.log('契約内容', contracts);
+
+    const result = getContractsSummary(contracts);
+
+    expect(result.契約金額税込).toBe(5000000);
+    expect(result.追加金額税込).toBe(-50000);
+    expect(result.合計受注金額税込).toBe(4950000);
+    expect(result.税率).toBe(0.1);
+    expect(result.返金).toBe(false);
+    expect(result.返金Amt).toBe(0);
+    expect(result.減額).toBe(true);
+    expect(result.減額Amt).toBe(50000);
+    expect(result.補助金).toBe(false);
+    expect(result.補助金Amt).toBe(0);
+  }, 60000);
+
   //　仕様参考 https://github.com/Lorenzras/yumecoco-monorepo/issues/817
   it('本契約無しで、設計契約のみだったら、設計契約金を「合計受注金額税込」に足す', async () => {
     const contractDat = await getContractById('3bbf6f94-5958-4d7f-810c-a7891a722c36');

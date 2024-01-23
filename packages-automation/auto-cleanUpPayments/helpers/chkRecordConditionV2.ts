5aa4b434f24312675dc07447eaaf12409323d626
YokoHayakawa <kaba3kobu@gmail.com>
2024-01-23 14:36:21 +0900

    入金一覧を論理削除から物理削除へ変更

--- packages-automation/auto-cleanUpPayments/helpers/chkRecordConditionV2.ts
+++ packages-automation/auto-cleanUpPayments/helpers/chkRecordConditionV2.ts
@@ -1,16 +1,13 @@
 import { IAndpadpayments } from 'types';
-import { UpdateAndpadPayments } from 'api-kintone/src/andpadPayments/updateAndpadPayments';
 import { GetAndpadPaymentsReturn } from './getAndpadPaymentsCsv';
 
 
 /**
  * ANDPAD入金一覧のバッグアップと現在の状態を比較し、
- * 削除されたレコードには削除ステータスを付与します
+ * 削除されたレコードは、バックアップ側でも削除します
  * @param param0 
- * 
- * @deprecated V2に移行のため
  */
-export const chkRecordCondition = ({
+export const chkRecordConditionV2 = ({
   unpaidBackupPayments,
   andpadPaymentsCsv,
 }: {
@@ -22,20 +19,12 @@ export const chkRecordCondition = ({
     const isExist = andpadPaymentsCsv.data.some(({ ID }) => ID.toString() === cur.ID.value);
 
     if (!isExist) {
-      acc.push({
-        updateKey: {
-          field: 'ID',
-          value: cur.ID.value,
-        },
-        record: {
-          deleteStatus: { value: '削除' },
-        },
-      });
+      acc.push(cur.$id.value);
     }
 
     return acc;
 
-  }, [] as UpdateAndpadPayments);
+  }, [] as string[]);
 
 
 };

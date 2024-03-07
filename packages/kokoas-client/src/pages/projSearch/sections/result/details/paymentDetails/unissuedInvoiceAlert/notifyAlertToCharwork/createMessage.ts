b91270009ed4b720905d5e90193666bb3c98b3d5
YokoHayakawa <kaba3kobu@gmail.com>
2024-03-07 12:45:40 +0900

    Update createMessage.ts

--- packages/kokoas-client/src/pages/projSearch/sections/result/details/paymentDetails/unissuedInvoiceAlert/notifyAlertToCharwork/createMessage.ts
+++ packages/kokoas-client/src/pages/projSearch/sections/result/details/paymentDetails/unissuedInvoiceAlert/notifyAlertToCharwork/createMessage.ts
@@ -35,23 +35,26 @@ export const createMessage = async ({
   const title = '[title]【ココアス】お客さまへの請求書の作成が確認できていません[/title]';
 
   const message0 = `下記工事に対して、${alertMessages[purpose]}`;
-  const message1 = `請求書の発行をお願いします。
-`;
+  const message1 = '請求書の発行をお願いします。\n';
 
-  const content = `【工事内容】
-契約日　 :  ${format(parseISO(contractDate), 'yyyy年M月d日')}
-工事名　 :  ${recProj.projName.value}
-契約金額 :  ${(contractAmt ?? 0).toLocaleString()} 円
-担当者　 :  ${cocoAGs}
-夢てつAG :  ${yumeAGs} 
-`;
+  console.log('contractDate', contractDate);
 
-  const reminder = `[info][title]下記リンク先より、再通知日を設定していただけます[/title]
-${reminderUrl ?? 'URLの取得に失敗しました'}[/info]
-`;
+  const content = [
+    '【工事内容】',
+    `契約日　 :  ${contractDate ? format(parseISO(contractDate), 'yyyy年M月d日') : ''}`,
+    `工事名　 :  ${recProj.projName.value}`,
+    `契約金額 :  ${(contractAmt ?? 0).toLocaleString()} 円`,
+    `担当者　 :  ${cocoAGs}`,
+    `夢てつAG :  ${yumeAGs} `,
+  ].join('\n');
+
+  const reminder = [
+    '[info][title]下記リンク先より、再通知日を設定していただけます[/title]',
+    `${reminderUrl ?? 'URLの取得に失敗しました'} [/info]`,
+  ].join('\n');
 
   const message2 = '本連絡と前後して処理されている場合はご容赦ください。';
 
 
-  return `[info]${[title, message0, message1, content, reminder, message2].join('\n')} [/info]`;
+  return `[info]${[title, message0, message1, content, reminder, message2].join('\n')}[/info]`;
 };

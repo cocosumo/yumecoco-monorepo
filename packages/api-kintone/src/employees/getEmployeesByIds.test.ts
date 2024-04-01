b69c7ed1241db423b8494562a67f8368450b4afc
Lorenz Ras <lenzras@gmail.com>
2024-04-01 15:40:55 +0900

    fix order of getEmployeeByIds

--- packages/api-kintone/src/employees/getEmployeesByIds.test.ts
+++ packages/api-kintone/src/employees/getEmployeesByIds.test.ts
@@ -1,18 +1,15 @@
 import { expect, describe, it } from '@jest/globals';
-import { RecordType } from './config';
 import { getEmployeesByIds } from './getEmployeesByIds';
 
 describe('getEmployeesByIds', () =>{
-  const existingEmpIds = ['69', '68', '70'];
-  let records: RecordType[] = [];
-
-  beforeAll(async () => {
-    const result = await getEmployeesByIds(existingEmpIds);
-    records = result.records;
-  });
+  const existingEmpIds = ['efce6ca1-48ae-4001-a9f5-0c92ad3a73c7', 'c41b4d70-8fe3-4277-99be-25d794a757f0'];
+  
   it('should get employees by existing ids', async () => {
-    const retrievedIds = records.map(({ $id }) => $id.value );
-    expect(retrievedIds.sort()).toEqual(existingEmpIds.sort());
+    const result = await getEmployeesByIds(existingEmpIds);
+    const records = result.records;
+    const resultIds = records.map(({ uuid }) => uuid.value);
+
+    expect(resultIds).toEqual(existingEmpIds);
   });
 
 });

533c5d01bcae2632d2c0d6b3871eb55a8d8ec1d2
YokoHayakawa <kaba3kobu@gmail.com>
2024-04-05 11:55:35 +0900

    getDisplayPaymentDateの修正

--- packages/kokoas-client/src/pages/projSearch/sections/result/details/paymentDetails/unissuedInvoiceAlert/helper/getDisplayPaymentDate.test.ts
+++ packages/kokoas-client/src/pages/projSearch/sections/result/details/paymentDetails/unissuedInvoiceAlert/helper/getDisplayPaymentDate.test.ts
@@ -5,31 +5,24 @@ import { describe, it, expect } from '@jest/globals';
 
 describe('getDisplayPaymentDate', () => {
   it('正しい日付を返す', async () => {
-    const testDate: (string | null) = 'Thu May 22 2014 00:00:00 GMT+0900 (日本標準時)';
+    const testDate: (Date | null) = new Date(2014, 4, 22);
     const result = getDisplayPaymentDate(testDate);
 
     expect(result).toBe('2014年5月22日');
   });
 
   it('nullの場合は空文字を返す', async () => {
-    const testDate: (string | null) = null;
+    const testDate = null;
     const result = getDisplayPaymentDate(testDate);
 
     expect(result).toBe('');
   });
-  
+
   it('不正な入力の場合は空文字を返す', async () => {
-    const testDate: (string | null) = 'Invalid Date';
+    const testDate = new Date('2000-6-40');
     const result = getDisplayPaymentDate(testDate);
 
     expect(result).toBe('');
   });
-  
-  it('空文字の場合は空文字を返す', async () => {
-    const testDate: (string | null) = '';
-    const result = getDisplayPaymentDate(testDate);
 
-    expect(result).toBe('');
-  });
-  
 });
--- packages/kokoas-client/src/pages/projSearch/sections/result/details/paymentDetails/unissuedInvoiceAlert/helper/getDisplayPaymentDate.ts
+++ packages/kokoas-client/src/pages/projSearch/sections/result/details/paymentDetails/unissuedInvoiceAlert/helper/getDisplayPaymentDate.ts
@@ -1,14 +1,10 @@
 import format from 'date-fns/format';
-
+import isValid from 'date-fns/isValid';
 
 
 export const getDisplayPaymentDate = (paymentDate: Date | null) => {
-
-  if (!paymentDate || isNaN(new Date(paymentDate).getDate())) {
-    // 日付データではない場合
-    return '';
-  }
-
-  return `${format(new Date(paymentDate), 'yyyy年M月d日')}`;
+  
+  if (!paymentDate) return '';
+  return isValid(paymentDate) ? format(new Date(paymentDate), 'yyyy年M月d日') : '';
 
 };

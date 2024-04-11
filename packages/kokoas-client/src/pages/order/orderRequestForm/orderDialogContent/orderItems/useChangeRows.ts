eee274ff6703606af6982907c0e4b362fc8be1e1
Lorenz Ras <lenzras@gmail.com>
2024-04-11 17:38:12 +0900

    add hotkeys

---
+++ packages/kokoas-client/src/pages/order/orderRequestForm/orderDialogContent/orderItems/useChangeRows.ts
@@ -0,0 +1,76 @@
+import { useCallback } from 'react';
+import { roundTo } from 'libs';
+import { RowItem } from './useColumns';
+import { produce } from 'immer';
+import { KItem } from '../../../schema';
+import { useOrderFormContext } from '../../hooks/useOrderRHF';
+
+
+
+
+
+export const useChangeRows = () => {
+  const { setValue } = useOrderFormContext();
+
+  const calculatedRow = useCallback((row: RowItem, fieldName: KItem) => {
+
+    const {
+      costPrice,
+      quantity,
+    } = row;
+
+    let newRow = { ...row };
+
+
+    switch (fieldName) {
+      case 'quantity': 
+      case 'costPrice': {
+
+        const newRowCostPrice = roundTo(costPrice * quantity);
+
+        newRow = {
+          ...newRow,
+          rowCostPriceBeforeTax: roundTo(newRowCostPrice),
+        };
+        
+        break;
+      }
+      // TODO: add more cases if needed
+    }
+
+    return  newRow;
+  }, []);
+
+
+  const handleRowChange = useCallback((
+    indexes: number[], 
+    fieldName: KItem,
+    rows: RowItem[],
+  ) => {
+    //update(index, newRow);
+
+    setValue(
+      'selectedItems', 
+      produce(rows, draft => {
+        indexes.forEach((index) => {
+          draft[index] = calculatedRow(draft[index], fieldName);
+        });
+      }),
+      {
+        shouldValidate: true,
+        shouldDirty: true,
+        shouldTouch: true,
+      },
+    );
+
+  }, [ 
+    setValue, 
+    calculatedRow,  
+  ]);
+
+
+  return {
+    handleRowChange,
+  };
+
+};

93c386ba092562f31d0dc6fbb4cfc70ac86dd3cb
Lorenz Ras <lenzras@gmail.com>
2024-04-11 09:53:18 +0900

    fix filename

--- packages/kokoas-client/src/pages/order/orderRequestForm/orderDialogContent/orderMethod/EmailFields.tsx
+++ packages/kokoas-client/src/pages/order/orderRequestForm/orderDialogContent/orderMethod/EmailFields.tsx
@@ -1,5 +1,5 @@
 import { Stack } from '@mui/material';
-import { CustomEmailField } from './CustomTextField';
+import { CustomEmailField } from './CustomEmailField';
 
 export const EmailFields = () => {
   return (

c274852b1ee7a4eab69b50b0fa7a4895a3399a30
Lorenz Ras <lenzras@gmail.com>
2024-04-12 17:16:43 +0900

    add summary calculation

--- packages/kokoas-client/src/pages/order/orderRequestForm/orderDialogContent/summary/Summary.tsx
+++ packages/kokoas-client/src/pages/order/orderRequestForm/orderDialogContent/summary/Summary.tsx
@@ -1,7 +1,7 @@
 import { Stack, Typography } from '@mui/material';
 import { grey } from '@mui/material/colors';
-
 import { useSummary } from './useSummary';
+
 
 const LabeledInfo = ({
   label,
@@ -38,16 +38,26 @@ const LabeledInfo = ({
 export const Summary = () => {
 
   const {
-    totalAmountAfterTax,
-    totalTax,
-    totalAmountBeforeTax,
+    groupedByTaxArray,
+    totalTaxAmount,
+    nonTaxableAmount,
+    totalAmount,
   } = useSummary();
 
   return (
     <Stack spacing={1}>
-      <LabeledInfo label={'小計'} value={totalAmountBeforeTax} />
-      <LabeledInfo label={'消費税'} value={totalTax} />
-      <LabeledInfo label={'税込'} value={totalAmountAfterTax} />
+      {groupedByTaxArray.length 
+        ? (
+          groupedByTaxArray.map(([taxRate, amount]) => (
+            <LabeledInfo key={taxRate} label={`小計 ${taxRate}%対象`} value={amount} />
+          ))
+        ) 
+        : (
+          <LabeledInfo label={'小計 課税'} value={0} />
+        )}
+      <LabeledInfo label={'消費税'} value={totalTaxAmount} />
+      <LabeledInfo label={'非課税'} value={nonTaxableAmount} />
+      <LabeledInfo label={'合計'} value={totalAmount} />
     </Stack>
   );
 };
--- packages/kokoas-client/src/pages/order/orderRequestForm/orderDialogContent/summary/useSummary.ts
+++ packages/kokoas-client/src/pages/order/orderRequestForm/orderDialogContent/summary/useSummary.ts
@@ -1,49 +1,13 @@
 import { useMemo } from 'react';
 import { useOrderWatch } from '../../hooks/useOrderRHF';
 import { TOrderForm } from '../../schema';
-import { Big } from 'big.js';
+import { summarizeItems } from './summarizeItems';
 
-interface ReturnType {
-  totalAmountBeforeTax: number;
-  totalTax: number;
-  totalAmountAfterTax: number;
-}
-
-interface GroupByTaxRate {
-  number: Big;
-}
 
 export const useSummary = () => {
   const selectedItems = useOrderWatch({
     name: 'selectedItems',
   }) as  TOrderForm['selectedItems'];
 
-  return useMemo<ReturnType>(() => {
-
-    const {
-      totalAmountBeforeTax,
-      totalTaxable,
-    } = selectedItems.reduce<GroupByTaxRate>((acc, item) => {
-      const {
-        rowCostPriceBeforeTax,
-        taxRate,
-      } = item;
-
-      const rowCostPriceBeforeTaxBig = new Big(rowCostPriceBeforeTax);
-      const taxRateBig = new Big(taxRate);
-
-      if(!acc[taxRate]) {
-        acc[taxRate] = rowCostPriceBeforeTaxBig;
-      }
-
-      return acc;
-    }, Object.create(null));
-
-    return {
-      totalAmountBeforeTax: totalAmountBeforeTax.toNumber(),
-      totalTax: ,
-      totalAmountAfterTax: totalAmountBeforeTax.plus(totalTax).toNumber(),
-    };
-
-  }, [selectedItems]);
+  return useMemo(() => summarizeItems(selectedItems), [selectedItems]);
 };

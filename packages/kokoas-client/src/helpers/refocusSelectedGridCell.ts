18b765f63d44946f53a432497e72922b2d282571
Lorenz Ras <lenzras@gmail.com>
2024-04-13 14:21:07 +0900

    Update refocusSelectedGridCell.ts

--- packages/kokoas-client/src/helpers/refocusSelectedGridCell.ts
+++ packages/kokoas-client/src/helpers/refocusSelectedGridCell.ts
@@ -1,6 +1,5 @@
 export const refocusSelectedGridCell = () => {
   const el = document.querySelector('div[role="gridcell"][aria-selected="true"]') as HTMLElement;
-  console.log('el', el);
  
   el?.focus();
 };

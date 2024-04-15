93813860ebc305413541b84016a674c3b4ce5b88
Lorenz Ras <lenzras@gmail.com>
2024-04-15 09:24:34 +0900

    fix lose edit mode after pressing enter

--- packages/kokoas-client/src/pages/order/inputGrid/useChangeRows.ts
+++ packages/kokoas-client/src/pages/order/inputGrid/useChangeRows.ts
@@ -3,7 +3,6 @@ import { roundTo } from 'libs';
 import { KItem } from '../schema';
 import { FillEvent } from 'react-data-grid';
 import { RowItem } from './useColumns';
-import { produce } from 'immer';
 import { useTypedFormContext } from '../hooks/useTypedRHF';
 
 
@@ -48,21 +47,18 @@ export const useChangeRows = () => {
     fieldName: KItem,
     rows: RowItem[],
   ) => {
-    //update(index, newRow);
 
-    setValue(
-      'items', 
-      produce(rows, draft => {
-        indexes.forEach((index) => {
-          draft[index] = calculatedRow(draft[index], fieldName);
-        });
-      }),
-      {
-        shouldValidate: true,
-        shouldDirty: true,
-        shouldTouch: true,
-      },
-    );
+    indexes.forEach((index) => {
+      setValue(
+        `items.${index}`, 
+        calculatedRow(rows[index], fieldName),
+        {
+          shouldValidate: true,
+          shouldDirty: true,
+          shouldTouch: true,
+        },
+      );
+    });
 
   }, [ 
     setValue, 

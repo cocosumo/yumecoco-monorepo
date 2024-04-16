1fa4a65312c8acf7832a858ba12657941854ae4c
YokoHayakawa <kaba3kobu@gmail.com>
2024-04-16 14:57:06 +0900

    Update order.ts

--- packages/types/src/common/order.ts
+++ packages/types/src/common/order.ts
@@ -11,6 +11,7 @@ export interface OrderData {
   /** 発注日 */
   orderDate: string,
 
+
   /** 工事id */
   projId: string,
 
@@ -29,17 +30,40 @@ export interface OrderData {
   /** 工事場所住所 */
   constAddress: string,
 
-  /** 工事期間 */
-  constPeriod: string,
+  /** 工事期間 開始日 */
+  constStartDate: string,
+
+  /** 工事期間 開始日 */
+  constFinishDate: string,
 
   /** 工事担当者 */
   cocoConst: string,
 
+
+  /** 会社名 */
+  companyName: string,
+
   /** 担当店舗 */
   store: string,
 
-  /** 業者住所 */
-  vendorAddress: string,
+  /** 店舗住所 */
+  storeAddress: string,
+
+  /** 店舗TEL */
+  storeTel: string,
+
+  /** 店舗FAX */
+  storeFax: string,
+
+  /** 建築業許可番号 */
+  buildingLicenseNumber: string,
+
+
+  /** 業者住所1 */
+  vendorAddress1: string,
+
+  /** 業者住所2 */
+  vendorAddress2: string,
 
   /** 業者担当者 */
   vendorManeger: string,

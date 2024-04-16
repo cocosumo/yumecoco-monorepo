5de7b23c95b38c4c9e142ff21863daab7613355d
YokoHayakawa <kaba3kobu@gmail.com>
2024-04-16 14:20:45 +0900

    工事期間取得処理の更新

--- packages/kokoas-server/src/handleRequest/reqDownloadOrderToSupplier/helper/getConstPeriod.test.ts
+++ packages/kokoas-server/src/handleRequest/reqDownloadOrderToSupplier/helper/getConstPeriod.test.ts
@@ -18,40 +18,40 @@ describe('getConstPeriod', () => {
 
     const result = getConstPeriod({
       startDate: '',
-      finishDate: '2024-1-1',
+      finishDate: '2024-01-01',
     });
 
-    expect(result).toBe('2024-1-1');
+    expect(result).toBe('2024年1月1日');
   });
 
   it('終了日が空の場合', async () => {
 
     const result = getConstPeriod({
-      startDate: '2024-2-1',
+      startDate: '2024-02-01',
       finishDate: '',
     });
 
-    expect(result).toBe('2024-2-1');
+    expect(result).toBe('2024年2月1日');
   });
 
   it('開始日、終了日が同日の場合', async () => {
 
     const result = getConstPeriod({
-      startDate: '2024-3-10',
-      finishDate: '2024-3-10',
+      startDate: '2024-03-10',
+      finishDate: '2024-03-10',
     });
 
-    expect(result).toBe('2024-3-10');
+    expect(result).toBe('2024年3月10日');
   });
 
   it('開始日、終了日が別日で設定されている場合', async () => {
 
     const result = getConstPeriod({
-      startDate: '2024-1-1',
-      finishDate: '2024-3-31',
+      startDate: '2024-01-01',
+      finishDate: '2024-03-31',
     });
 
-    expect(result).toBe('2024-1-1 から 2024-3-31 まで');
+    expect(result).toBe('2024年1月1日 から 2024年3月31日 まで');
   });
 
 });
--- packages/kokoas-server/src/handleRequest/reqDownloadOrderToSupplier/helper/getConstPeriod.ts
+++ packages/kokoas-server/src/handleRequest/reqDownloadOrderToSupplier/helper/getConstPeriod.ts
@@ -1,3 +1,14 @@
+import format from 'date-fns/format';
+import parseISO from 'date-fns/parseISO';
+
+
+
+export const formatDate = (date: string) => {
+  const parsedDate = parseISO(date);
+  return format(parsedDate, 'yyyy年M月d日');
+};
+
+
 export const getConstPeriod = ({
   startDate,
   finishDate,
@@ -10,12 +21,12 @@ export const getConstPeriod = ({
   if (!startDate && !finishDate) return '';
 
   // 工期が開始日と終了日のどちらかしか入力されていない場合
-  if (!startDate) return finishDate;
-  if (!finishDate) return startDate;
+  if (!startDate) return formatDate(finishDate);
+  if (!finishDate) return formatDate(startDate);
 
   // 開始日と終了日が同日の場合
-  if (startDate === finishDate) return startDate;
+  if (startDate === finishDate) return formatDate(startDate);
 
-  return `${startDate} から ${finishDate} まで`;
+  return `${formatDate(startDate)} から ${formatDate(finishDate)} まで`;
 
 };

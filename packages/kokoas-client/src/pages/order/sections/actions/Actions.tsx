f0aac3921e3bc7fea5808267d99f3670552fc079
Lorenz Ras <lenzras@gmail.com>
2024-04-17 16:56:45 +0900

    add insert row button

--- packages/kokoas-client/src/pages/order/sections/actions/Actions.tsx
+++ packages/kokoas-client/src/pages/order/sections/actions/Actions.tsx
@@ -2,6 +2,7 @@ import { Stack } from '@mui/material';
 import { CopyEstimates } from './copyEstimates/CopyEstimates';
 import { OrderSelected } from './OrderSelected';
 import { DeleteRows } from './deleteRows/DeleteRows';
+import { InsertRow } from './insertRow/InsertRow';
 
 export const Actions = () => {
   return (
@@ -17,6 +18,7 @@ export const Actions = () => {
 
         <OrderSelected  />
         <CopyEstimates />
+        <InsertRow />
 
       </Stack>
 
---
+++ packages/kokoas-client/src/pages/order/sections/actions/insertRow/InsertRow.tsx
@@ -0,0 +1,43 @@
+import { Button } from '@mui/material';
+import AddIcon from '@mui/icons-material/Add';
+import { useTypedFormContext } from '../../../hooks/useTypedRHF';
+import { produce } from 'immer';
+import { useRowValues } from '../../../inputGrid/useRowValues';
+
+export const InsertRow = () => {
+
+  const { getValues, setValue } = useTypedFormContext();
+  const { getNewRow } = useRowValues();
+  
+  const handleInsert = () => {
+    const items = getValues('items');
+    // I didn't find a built-in way in react-data-grid get selected cell so I will use querySelector,
+    // however, this might break if the specifications of the grid changes.
+    const selectedCell = document.querySelector('div[role="grid"] div[aria-selected="true"]');
+    let parsedRowIdx = items.length;
+
+    if (selectedCell) {
+      const selectedRow = selectedCell.parentElement as HTMLDivElement;
+
+      const gridRowIdx = selectedRow.getAttribute('aria-rowindex');
+      parsedRowIdx = Number(gridRowIdx) - 1;
+    }
+
+    setValue('items', produce(items, draft => {
+      draft.splice(parsedRowIdx, 0, getNewRow());
+    }));
+    
+
+  };
+
+  return (
+    <Button 
+      variant={'contained'}
+      color='info'
+      startIcon={<AddIcon />}
+      onClick={handleInsert} // TODO: Implement insert row
+    >
+      {'行を追加'}
+    </Button>
+  );
+};

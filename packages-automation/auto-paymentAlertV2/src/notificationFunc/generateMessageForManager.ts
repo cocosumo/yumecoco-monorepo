ae5803e6e24f7059b2d938b5788d24f7ccfe80ff
YokoHayakawa <kaba3kobu@gmail.com>
2024-04-22 13:05:23 +0900

    Update generateMessageForManager.ts

--- packages-automation/auto-paymentAlertV2/src/notificationFunc/generateMessageForManager.ts
+++ packages-automation/auto-paymentAlertV2/src/notificationFunc/generateMessageForManager.ts
@@ -6,31 +6,52 @@ export const generateMessageForManager = (paymentReminder: PaymentReminder[]) =>
 
   console.log(paymentReminder);
 
-  const contractSummary = paymentReminder.map(({
-    projName,
-    cwRoomIds,
-    storeName,
-    expectedPaymentAmt,
-    expectedPaymentDate,
-  }, idx) => {
-    const agentNames = cwRoomIds.map(({ agentName }) => agentName).join(', ');
-
-    return `${idx + 1}件目
-${storeName}　${projName}
-入金予定日: ${expectedPaymentDate}　入金予定額 ￥${(+expectedPaymentAmt).toLocaleString()}　担当者:${agentNames}
-`;
-  });
-
-
   const title = '[title]【ココアス】お客さまからの入金が確認できていません[/title]';
 
   const message = `${paymentReminder.length}件の入金確認が遅れています。
 必要に応じて担当者へのフォローをお願いします。
 ※この連絡は入金予定日を過ぎてもお客さまからの入金がない請求に対して実施しています。`;
 
-  const content = `[info][title]概要[/title]${contractSummary.join('[hr]\n')}[/info]`;
+  const contractSummary = paymentReminder.reduce((
+    acc,
+    {
+      projName,
+      cwRoomIds,
+      storeName,
+      expectedPaymentAmt,
+      expectedPaymentDate,
+      andpadPaymentUrl,
+    },
+    idx,
+  ) => {
+    const agentNames = cwRoomIds.map(({ agentName }) => agentName).join(', ');
 
+    const result = [`${idx + 1}件目`,
+      `${storeName}　${projName}`,
+      `入金予定日: ${expectedPaymentDate}　入金予定額 ￥${(+expectedPaymentAmt).toLocaleString()}　担当者:${agentNames}`,
+      `ANDPAD引合粗利管理[入金]　${andpadPaymentUrl}`,
+      '[hr]\n',
+    ].join('\n');
 
-  return `[info]${[title, message, content].join('\n')}[/info]`;
+    const arrayPos = Math.floor(idx / 5);
+
+    console.log(`idx=${idx}:: arrayPos=${arrayPos}`);
+
+    if (idx === 0) {// 最初のみ挿入するメッセージ
+      acc[0] = `[info]${[title, message, `[info][title]概要[/title] ${result}`].join('\n')}`;
+    } else if ((idx + 1) % 5 === 0) {
+      // 各ブロックの最後(ブロックを閉じる)
+      acc[arrayPos] = `${acc[arrayPos]} ${result}[/info][/info]`;
+    } else if ((idx + 1) % 5 === 1) {
+      // 各ブロックの最初で、要素の追加とタイトルを入れる
+      acc.push(`[info][title]概要[/title] ${result}`);
+    } else {
+      acc[arrayPos] = `${acc[arrayPos]} ${result}`;
+    }
+
+    return acc;
+  }, [] as string[]);
+
+  return contractSummary;
 
 };

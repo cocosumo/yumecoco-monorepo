e001351aeeac3d705ab26b25934352805d27f722
YokoHayakawa <kaba3kobu@gmail.com>
2024-05-10 14:00:25 +0900

    ページフォーマットに合わせて修正

--- packages/kokoas-server/src/handleRequest/reqDownloadOrderToSupplier/createOrderDocment/createOrderDocument.ts
+++ packages/kokoas-server/src/handleRequest/reqDownloadOrderToSupplier/createOrderDocment/createOrderDocument.ts
@@ -1,13 +1,13 @@
 import { getTemplate } from 'api-aws/src/s3/getTemplate';
-import { PDFDocument, rgb } from 'pdf-lib';
+import { PDFDocument } from 'pdf-lib';
 import { OrderData } from 'types/src/common/order';
 import fontkit from '@pdf-lib/fontkit';
 import fs from 'fs/promises';
 import { getFilePath, getFont } from 'kokoas-server/src/assets';
 import { drawText } from 'kokoas-server/src/api/docusign/contracts/helpers/pdf';
 import { Big } from 'big.js';
-import { chkStrLength } from '../helper/chkStrLength';
 import { createOrderHeader } from './createOrderHeader';
+import { createOrderDetails } from './createOrderDetails';
 
 
 
@@ -101,6 +101,22 @@ export const createOrderDocument = async (
     if (isFirstPage) {
       // 1枚目のみに記載する情報の反映
       createOrderHeader(data, tgtPage, msChinoFont);
+      if (!isLastPage) {
+        // ページ番号
+        drawText(
+          tgtPage,
+          `${pageNum + 1} / ${maxPageNum}`,
+          {
+            x: 790,
+            y: 570,
+            font: msChinoFont,
+            size: 9,
+          },
+          {
+            weight: 0.1,
+          },
+        );
+      }
     } else {
       // 2枚目以降に記載する情報
       // 発注番号
@@ -133,6 +149,20 @@ export const createOrderDocument = async (
         },
       );
 
+      // ページ番号
+      drawText(
+        tgtPage,
+        `${pageNum + 1} / ${maxPageNum}`,
+        {
+          x: 790,
+          y: 570,
+          font: msChinoFont,
+          size: 9,
+        },
+        {
+          weight: 0.1,
+        },
+      );
     }
 
 
@@ -142,196 +172,7 @@ export const createOrderDocument = async (
     const maxI = isLastPage ? orderDetails.length : startI + rowNum;
 
 
-    for (let i = startI; i < maxI; i++) {
-      const posOffset = 14.9 * (i - startI);
-      const posYTop = isFirstPage ? 334 : 517;
-      const posY = posYTop - posOffset;
-
-      // 番号
-      drawText(
-        tgtPage,
-        (i + 1).toString(),
-        {
-          x: -10,
-          y: posY,
-          font: msChinoFont,
-          size: 9,
-        },
-        {
-          weight: 0.1,
-          align: 'center',
-        },
-      );
-
-      // 大項目
-      drawText(
-        tgtPage,
-        orderDetails[i].majorItem,
-        {
-          x: 54,
-          y: posY,
-          font: msChinoFont,
-          size: chkStrLength({
-            text: orderDetails[i].majorItem,
-            maxlen: 24,
-            fontSize: 9,
-          }),
-        },
-        {
-          weight: 0.1,
-        },
-      );
-
-      // 中項目
-      drawText(
-        tgtPage,
-        orderDetails[i].middleItem,
-        {
-          x: 173,
-          y: posY,
-          font: msChinoFont,
-          size: chkStrLength({
-            text: orderDetails[i].middleItem,
-            maxlen: 24,
-            fontSize: 9,
-          }),
-        },
-        {
-          weight: 0.1,
-        },
-      );
-
-      // 部材
-      drawText(
-        tgtPage,
-        orderDetails[i].material,
-        {
-          x: 291,
-          y: posY,
-          font: msChinoFont,
-          size: chkStrLength({
-            text: orderDetails[i].material,
-            maxlen: 24,
-            fontSize: 9,
-          }),
-        },
-        {
-          weight: 0.1,
-        },
-      );
-
-      // 単位
-      drawText(
-        tgtPage,
-        orderDetails[i].unit,
-        {
-          x: 375,
-          y: posY,
-          font: msChinoFont,
-          size: 9,
-        },
-        {
-          weight: 0.1,
-          align: 'center',
-        },
-      );
-
-      // 数量
-      const quantity = orderDetails[i].quantity.toFixed(2);
-
-      drawText(
-        tgtPage,
-        quantity,
-        {
-          x: 388,
-          y: posY,
-          font: msChinoFont,
-          size: 9,
-          color: orderDetails[i].quantity >= 0 ? rgb(0, 0, 0) : rgb(1, 0, 0),
-        },
-        {
-          weight: 0.1,
-          align: 'right',
-        },
-      );
-
-      // 単価
-      drawText(
-        tgtPage,
-        orderDetails[i].costPrice.toLocaleString(),
-        {
-          x: 458,
-          y: posY,
-          font: msChinoFont,
-          size: 9,
-          color: orderDetails[i].costPrice >= 0 ? rgb(0, 0, 0) : rgb(1, 0, 0),
-        },
-        {
-          weight: 0.1,
-          align: 'right',
-        },
-      );
-
-      // 発注金額
-      drawText(
-        tgtPage,
-        orderDetails[i].orderAmountBeforeTax.toLocaleString(),
-        {
-          x: 552,
-          y: posY,
-          font: msChinoFont,
-          size: 9,
-          color: orderDetails[i].orderAmountBeforeTax >= 0 ? rgb(0, 0, 0) : rgb(1, 0, 0),
-        },
-        {
-          weight: 0.1,
-          align: 'right',
-        },
-      );
-
-
-      // 税区分
-      const taxRate = orderDetails[i].taxRate === 0 ? '非課税'
-        : `課税(${Big(orderDetails[i].taxRate).mul(100)
-          .toNumber()
-          .toString()}%)`;
-
-      drawText(
-        tgtPage,
-        taxRate,
-        {
-          x: 660,
-          y: posY,
-          font: msChinoFont,
-          size: 9,
-        },
-        {
-          weight: 0.1,
-        },
-      );
-
-      // 備考
-      drawText(
-        tgtPage,
-        orderDetails[i].rowRemarks,
-        {
-          x: 707,
-          y: posY,
-          font: msChinoFont,
-          size: chkStrLength({
-            text: orderDetails[i].rowRemarks,
-            maxlen: 22,
-            fontSize: 9,
-          }),
-        },
-        {
-          weight: 0.1,
-        },
-      );
-
-
-    } // 発注明細 ここまで
-
+    createOrderDetails(orderDetails, tgtPage, msChinoFont, isFirstPage, startI, maxI);
 
     // summary
     if (!isLastPage) {
@@ -339,7 +180,7 @@ export const createOrderDocument = async (
         taxRate,
         orderAmountBeforeTax,
       }, idx) => {
-        if (idx < startI || maxI < idx) return acc;
+        if (idx < startI || maxI <= idx) return acc;
 
         acc.subtotal += orderAmountBeforeTax;
         if (taxRate === 0) {
@@ -382,7 +223,7 @@ export const createOrderDocument = async (
       // ページ計
       drawText(
         tgtPage,
-        pageTotal.totalAmount.toLocaleString(),
+        pageTotal.subtotal.toLocaleString(),
         {
           x: 707,
           y: pageTotalPosY,

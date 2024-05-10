2fd2ee424b83d4c46a9e0cf8b67baa6b590c9aad
Lorenz Ras <lenzras@gmail.com>
2024-05-10 14:27:03 +0900

    WIP schema

---
+++ packages/kokoas-client/src/pages/orderInvoice/schema.ts
@@ -0,0 +1,22 @@
+import { zodErrorMapJA } from 'kokoas-client/src/lib/zodErrorMapJA';
+import { invoiceProgress } from 'types/src/common/order';
+import { z } from 'zod';
+
+z.setErrorMap(zodErrorMapJA());
+
+const schema = z.object({
+  status: z.enum(invoiceProgress).optional(),
+  projName: z.string().optional(),
+  storeName: z.string().optional(),
+  cocoAgName: z.string().optional(),
+  supplierName: z.string().optional(),
+  invoiceSystemNumber: z.string().optional(),
+  orderAmount: z.number().optional(),
+  invoiceAmount: z.number().optional(),
+  invoiceDate: z.date().optional(),
+});
+
+export type TypeOfForm = z.infer<typeof schema>;
+export type KeyOfForm = keyof TypeOfForm;
+
+export default schema;

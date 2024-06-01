eab1d8818c68a7f812c483abb74341d2c6c15840
Lorenz Ras <lenzras@gmail.com>
2024-06-01 15:43:00 +0900

    add return button

--- packages/kokoas-client/src/pages/order/orderRequestForm/orderDialogActions/OrderDialogActions.tsx
+++ packages/kokoas-client/src/pages/order/orderRequestForm/orderDialogActions/OrderDialogActions.tsx
@@ -1,16 +1,31 @@
-import { DialogActions } from '@mui/material';
+import { DialogActions, Stack } from '@mui/material';
 import { SaveButton } from './SaveButton';
 import { DownloadButton } from './DownloadButton';
+import { ReturnButton } from './returnButton.tsx/ReturnButton';
+import { useOrderWatch } from '../hooks/useOrderRHF';
+import { TOrderForm } from '../schema';
 
 export const OrderDialogActions = () => {
+  const orderId = useOrderWatch({
+    name: 'orderId',
+  }) as TOrderForm['orderId'];
+
+  const hasOrder = !!orderId;
+
   return (
-    <DialogActions
-      sx={{
-        justifyContent: 'center',
-      }}
-    >
-      <SaveButton />
-      <DownloadButton />
+    <DialogActions>
+      {hasOrder && <ReturnButton />}
+      
+      <Stack
+        spacing={2}
+        direction="row"
+        justifyContent="center"
+        flexGrow={1}
+      >
+        <SaveButton />
+        <DownloadButton />
+      </Stack>
+
     </DialogActions>
   );
 };
---
+++ packages/kokoas-client/src/pages/order/orderRequestForm/orderDialogActions/returnButton.tsx/ReturnButton.tsx
@@ -0,0 +1,18 @@
+import { Button } from '@mui/material';
+
+/**
+ * 差し戻しボタン
+ */
+export const ReturnButton = () => {
+  
+  return (
+    <Button
+      variant="contained"
+      color="info"
+      //onClick={handleReturn}
+    >
+      差し戻し
+    </Button>
+  );
+
+};

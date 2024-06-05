a38c8ea9ea858840cac8b35eb26ca07caaffea98
Lorenz Ras <lenzras@gmail.com>
2024-06-05 21:08:20 +0900

    Update DeleteButton.tsx

--- packages/kokoas-client/src/pages/order/orderRequestForm/orderDialogActions/deleteButton/DeleteButton.tsx
+++ packages/kokoas-client/src/pages/order/orderRequestForm/orderDialogActions/deleteButton/DeleteButton.tsx
@@ -1,13 +1,23 @@
 import { Button } from '@mui/material';
+import { useConfirmDialog } from 'kokoas-client/src/hooks';
 
 export const DeleteButton = () => {
+
+  const { setDialogState } = useConfirmDialog();
 
   return (
     <Button
       variant="contained"
       color="error"
       onClick={() => {
-        alert('開発中です。');
+        setDialogState({
+          open: true,
+          title: '削除確認',
+          content: '削除するとデータは完全に消え復元できません。削除しますか？',
+          handleYes: () => {
+            alert('開発中です。');
+          },
+        });
       }}
     >
       削除

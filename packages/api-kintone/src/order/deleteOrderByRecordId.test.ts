e8af7b7d028f8e26d6218e9e0f900f21afb93678
Lorenz Ras <lenzras@gmail.com>
2024-06-08 15:23:32 +0900

    implement delete order button

--- packages/api-kintone/src/order/deleteOrderByRecordId.test.ts
+++
@@ -1,9 +0,0 @@
-import { describe } from '@jest/globals';
-
-describe('deleteOrderByRecordId', () => {
-  it('should delete order by record id', async () => {
-
-    // TODO: implement test
-
-  });
-});

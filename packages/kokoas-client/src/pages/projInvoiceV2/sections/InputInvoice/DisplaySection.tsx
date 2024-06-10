edf898337d2ca57d9944efce56d3291973ed1762
YokoHayakawa <kaba3kobu@gmail.com>
2024-06-10 13:13:50 +0900

    金額表示の修正

--- packages/kokoas-client/src/pages/projInvoiceV2/sections/InputInvoice/DisplaySection.tsx
+++ packages/kokoas-client/src/pages/projInvoiceV2/sections/InputInvoice/DisplaySection.tsx
@@ -1,48 +1,8 @@
 import { useTypedWatch } from '../../hooks/useTypedRHF';
-import { Stack, StackProps, Typography } from '@mui/material';
+import { Stack } from '@mui/material';
 import { Big } from 'big.js';
+import { AmountInfo } from '../../parts/AmountInfo';
 
-
-
-const BillingInfo = ({
-  label,
-  value,
-  justifyContent,
-}: {
-  label: string,
-  value: string,
-  justifyContent?: StackProps['justifyContent']
-}) => (
-  <Stack
-    direction={'row'}
-    justifyContent={justifyContent}
-  >
-    <Typography variant='body2' width={'100Px'}>
-      {label}
-    </Typography>
-    <Typography
-      variant='body1'
-      component={'span'}
-      width={'15%'}
-      align={'right'}
-      style={{
-        position: 'relative',
-        display: 'inline-block',
-      }}
-    >
-      {value}
-      <span
-        style={{
-          position: 'absolute',
-          bottom: '-2px', // Adjust this value as needed
-          left: '0',
-          width: '100%',
-          borderBottom: '1px solid black',
-        }}
-      />
-    </Typography>
-  </Stack>
-);
 
 
 export const DisplaySection = () => {
@@ -64,8 +24,16 @@ export const DisplaySection = () => {
 
   return (
     <Stack spacing={1}>
-      <BillingInfo key={'billingTotalAmount'} label={'請求済み金額'} value={`${billingTotalAmount.toLocaleString()} 円`} />
-      <BillingInfo key={'billingBalance'} label={'請求残額'} value={`${billingBalance.toLocaleString()} 円`} />
+      <AmountInfo
+        key={'billingTotalAmount'}
+        label={'請求済み金額'}
+        value={`${billingTotalAmount.toLocaleString()} 円`}
+      />
+      <AmountInfo
+        key={'billingBalance'}
+        label={'請求残額'}
+        value={`${billingBalance.toLocaleString()} 円`}
+      />
     </Stack>
   );
 };

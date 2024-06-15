c3dd126b44b88fd5879d264dc68a0254362a0200
Lorenz Ras <lenzras@gmail.com>
2024-06-15 09:12:03 +0900

    refactor copy dialog

---
+++ packages/kokoas-client/src/pages/projEstimate/formActions/copy/CopyConfirm.tsx
@@ -0,0 +1,92 @@
+import { Button, Checkbox, Dialog, DialogActions, DialogContent, DialogTitle, FormControlLabel } from '@mui/material';
+import { generateParams } from 'kokoas-client/src/helpers/url';
+import { useBackdrop, useSnackBar } from 'kokoas-client/src/hooks';
+import { pages } from 'kokoas-client/src/pages/Router';
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { KForm } from '../../schema';
+
+export const CopyConfirm = ({
+  open,
+  handleClose,
+}:{
+  open: boolean,
+  handleClose: () => void,
+}) => {
+
+  const [checked, setChecked] = useState<boolean>(false);
+  const { setSnackState } = useSnackBar();
+  const { setBackdropState } = useBackdrop();  
+  const navigate = useNavigate();
+
+  
+  const handleCopy = async () => {
+    try {
+      handleClose();
+      setSnackState({
+        open: true,
+        autoHideDuration: 2000,
+        message: 'コピーしました。',
+        severity: 'success',
+        handleClose: () => {
+          setBackdropState({ open: false });
+        },
+      });
+
+
+      const fieldToClear : KForm = 'estimateId'; 
+      navigate(`${pages.projEstimate}?${generateParams({
+        clearFields: checked ? fieldToClear : '0',
+  
+      })}`); 
+       
+    } catch (err) {
+      setSnackState({
+        open: true,
+        message: err.message,
+        severity: 'error',
+      });
+    }
+
+  }; 
+
+  return (
+    <Dialog
+      open={open}
+      maxWidth={'xs'}
+      onClose={handleClose}
+    >
+      <DialogTitle>
+        見積もりのコピーを作成
+      </DialogTitle>
+      <DialogContent>
+        <FormControlLabel 
+          control={
+            <Checkbox 
+              checked={checked} 
+              onChange={(_, newValue) => setChecked(newValue)}
+            />
+          } 
+          label="同じ工事"
+        />
+      </DialogContent>
+      <DialogActions>
+
+        <Button
+          onClick={handleClose}
+        >
+          いいえ
+        </Button>
+        <Button
+          variant='contained'
+          color='info'
+          onClick={handleCopy}
+        >
+          はい
+        </Button>
+
+      </DialogActions>
+    
+    </Dialog>
+  );
+};
--- packages/kokoas-client/src/pages/projEstimate/formActions/copy/CopyForm.tsx
+++ packages/kokoas-client/src/pages/projEstimate/formActions/copy/CopyForm.tsx
@@ -1,14 +1,7 @@
 import { Button, Tooltip } from '@mui/material';
 import ContentCopyIcon from '@mui/icons-material/ContentCopy';
-
-
-import { useRef } from 'react';
-import { useNavigate } from 'react-router-dom';
-import { generateParams } from 'kokoas-client/src/helpers/url';
-import { pages } from '../../../Router';
-import { useBackdrop, useConfirmDialog, useSnackBar } from 'kokoas-client/src/hooks';
-import { CopyDialogContent } from './CopyDialogContent';
-import { KForm } from '../../schema';
+import { useState } from 'react';
+import { CopyConfirm } from './CopyConfirm';
 
 
 
@@ -16,76 +9,26 @@ import { KForm } from '../../schema';
 
 export const CopyForm = () => {
 
-  const { setSnackState } = useSnackBar();
-  const { setDialogState, handleClose } = useConfirmDialog();
-  const { setBackdropState } = useBackdrop();
-  const navigate = useNavigate();
-  const isSameProj = useRef(false);
 
-
-  const handleCopy = async () => {
-    try {
-      handleClose();
-      setSnackState({
-        open: true,
-        autoHideDuration: 2000,
-        message: 'コピーしました。',
-        severity: 'success',
-        handleClose: () => {
-          setBackdropState({ open: false });
-        },
-      });
-
-      /**
-       * if same project, use the current state but clear estimate id,
-       * otherwise, use initialValue and only copy items.
-       */
-      const fieldToClear : KForm = 'estimateId'; 
-      
-      navigate(`${pages.projEstimate}?${generateParams({
-        clearFields: isSameProj.current ? fieldToClear : '0',
-      })}`); 
- 
-    } catch (err: any) {
-      setSnackState({
-        open: true,
-        message: err.message,
-        severity: 'error',
-      });
-    }
-
-  };
-
-
-
-
-  const handleClickCopy = () => {
-    setDialogState({
-      open: true,
-      title: '見積もりのコピーを作成',
-      content: (
-        <CopyDialogContent
-          handleChangeIsSameProj={(checked)=>{
-            isSameProj.current = checked;
-          }}
-          defaultChecked={isSameProj.current}
-        />),
-      handleYes: handleCopy,
-      cancellable: true,
-    });
-  };
-
-
+  const [open, setOpen] = useState(false);
 
   return (
-    <Tooltip title={'当レコードをコピーし、新なレコードを作成します'}>
-      <Button
-        variant="outlined"
-        size="large"
-        onClick={handleClickCopy}
-      >
-        <ContentCopyIcon />
-      </Button>
-    </Tooltip>
+    <>
+      <Tooltip title={'当レコードをコピーし、新なレコードを作成します'}>
+        <Button
+          variant="outlined"
+          size="large"
+          onClick={() => setOpen(true)}
+        >
+          <ContentCopyIcon />
+        </Button>
+
+      </Tooltip>
+      <CopyConfirm 
+        open={open}
+        handleClose={() => setOpen(false)}
+      />
+    </>
+
   );
 };

dae0b80103019cc061b1c02df0e483cd0e8275f8
Lorenz Ras <lenzras@gmail.com>
2024-07-17 20:55:57 +0900

    add delete functionality

--- packages/kokoas-client/src/pages/projInvoiceV2/sections/actions/DeleteInvoice.tsx
+++ packages/kokoas-client/src/pages/projInvoiceV2/sections/actions/DeleteInvoice.tsx
@@ -1,19 +1,36 @@
 import { Button } from '@mui/material';
 import { useDeleteInvoiceB2CBy$Id } from 'kokoas-client/src/hooksQuery';
+import { useTypedWatch } from '../../hooks/useTypedRHF';
+import { useConfirmDialog, useNavigateWithQuery } from 'kokoas-client/src/hooks';
 
 
 
 export const DeleteInvoice = () => {
+  const [invoice$Id, projId] = useTypedWatch({
+    name: ['invoice$Id', 'projId'],
+  }) as [number, string];
   const {
     mutateAsync: deleteInvoice,
   } = useDeleteInvoiceB2CBy$Id();
+  const { setDialogState } = useConfirmDialog();
+  const navigate = useNavigateWithQuery();
 
   return (
     <Button
       variant={'outlined'}
       color='primary'
+      disabled={!invoice$Id}
       onClick={() => {
-        deleteInvoice('id');
+        setDialogState({
+          title: '請求書削除',
+          content: '請求書を削除しますか？',
+          handleYes: async () => {
+            await deleteInvoice(String(invoice$Id));
+            navigate('projInvoiceV2', { projId });
+          },
+
+        });
+
       }}
     >
       削除
